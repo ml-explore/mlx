c6af74f07d7005aec94dd5bec2d26ddcf3036ef3
Awni Hannun <awni@apple.com>
2023-12-05 12:10:03 -0800

    docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -7,7 +7,7 @@ var DOCUMENTATION_OPTIONS = {
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: '',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: false,
