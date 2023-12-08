6d27e3863b0b96afb9c56d8d8810c1327465f1f8
Kai Ma <k78ma@uwaterloo.ca>
2023-12-08 17:33:33 -0500

    MLE and L1 loss functions

--- python/mlx/nn/losses.py
+++ python/mlx/nn/losses.py
@@ -6,3 +6,12 @@ import mlx.core as mx
 def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1):
     score = mx.take_along_axis(logits, targets[..., None], axis).squeeze(-1)
     return mx.logsumexp(logits, axis=axis) - score
+
+def MLE_loss(logits: mx.array, targets: mx.array, axis: int = -1):
+    log_probs = mx.log_softmax(logits, axis=axis)
+    score = mx.take_along_axis(log_probs, targets[..., None], axis).squeeze(-1)
+    return -mx.mean(score)
+
+def L1_loss(predictions: mx.array, targets: mx.array):
+    return mx.mean(mx.abs(predictions - targets))
+
