33d34d88982b785b734276fc7b9a8ae295639339
Kai Ma <k78ma@uwaterloo.ca>
2023-12-08 18:29:17 -0500

    subtract max logit for numerical stability

--- python/mlx/nn/losses.py
+++ python/mlx/nn/losses.py
@@ -7,6 +7,7 @@ def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1):
     return mx.logsumexp(logits, axis=axis) - score
 
 def MLE_loss(logits: mx.array, targets: mx.array, axis: int = -1):
+    logits = logits- mx.max(logits, axis=axis, keepdims=True)
     log_probs = mx.log(mx.softmax(logits, axis=axis))
     score = mx.take_along_axis(log_probs, targets[..., None], axis).squeeze(-1)
     return -mx.mean(score)
