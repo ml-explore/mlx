b073656f4106175743daf77afe058a61adb7ff2b
Kai Ma <k78ma@uwaterloo.ca>
2023-12-08 22:44:28 -0500

    cross entropy reduction + unit tests

--- python/mlx/nn/losses.py
+++ python/mlx/nn/losses.py
@@ -2,15 +2,19 @@
 
 import mlx.core as mx
 
-def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1):
-    score = mx.take_along_axis(logits, targets[..., None], axis).squeeze(-1)
-    return mx.logsumexp(logits, axis=axis) - score
-
-def MLE_loss(logits: mx.array, targets: mx.array, axis: int = -1):
-    logits = logits- mx.max(logits, axis=axis, keepdims=True)
+def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1, reduction: str = 'mean'):
+    logits = logits - mx.max(logits, axis=axis, keepdims=True)
     log_probs = mx.log(mx.softmax(logits, axis=axis))
     score = mx.take_along_axis(log_probs, targets[..., None], axis).squeeze(-1)
-    return -mx.mean(score)
+    
+    if reduction == 'mean':
+        return -mx.mean(score)
+    elif reduction == 'sum':
+        return -mx.sum(score)
+    elif reduction == 'none':
+        return -score
+    else:
+        raise ValueError("Invalid reduction. Must be 'none', 'mean', or 'sum'.")
 
 def l1_loss(predictions: mx.array, targets: mx.array):
     return mx.mean(mx.abs(predictions - targets))
--- python/tests/test_nn.py
+++ python/tests/test_nn.py
@@ -20,15 +20,21 @@ class TestNN(mlx_tests.MLXTestCase):
     def test_cross_entropy(self):
         logits = mx.array([[0.0, -float("inf")], [-float("inf"), 0.0]])
         targets = mx.array([0, 1])
-        losses = nn.losses.cross_entropy(logits, targets)
-        self.assertTrue(mx.array_equal(losses, mx.zeros((2,))))
 
-    def test_MLE_loss(self):
-        logits = mx.array([[0.0, -float("inf")], [-float("inf"), 0.0]])
-        targets = mx.array([0, 1])
-        losses = nn.losses.MLE_loss(logits, targets)
-        print(losses)
-        self.assertTrue(mx.allclose(losses, mx.zeros((2,))))
+        # Test with reduction 'none'
+        losses_none = nn.losses.cross_entropy(logits, targets, reduction='none')
+        expected_none = mx.array([0.0, 0.0])
+        self.assertTrue(mx.array_equal(losses_none, expected_none))
+
+        # Test with reduction 'mean'
+        losses_mean = nn.losses.cross_entropy(logits, targets, reduction='mean')
+        expected_mean = mx.mean(expected_none)
+        self.assertEqual(losses_mean, expected_mean)
+
+        # Test with reduction 'sum'
+        losses_sum = nn.losses.cross_entropy(logits, targets, reduction='sum')
+        expected_sum = mx.sum(expected_none)
+        self.assertEqual(losses_sum, expected_sum)
 
     def test_L1_loss(self):
         predictions = mx.array([0.5, 0.2, 0.9, 0.0])
