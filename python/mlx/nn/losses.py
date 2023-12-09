c113bfa125ee6ed9e4b9ceb22099c11586f987a2
Kai Ma <k78ma@uwaterloo.ca>
2023-12-08 23:43:44 -0500

    style

--- python/mlx/nn/losses.py
+++ python/mlx/nn/losses.py
@@ -2,7 +2,10 @@
 
 import mlx.core as mx
 
-def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1, reduction: str = 'none'):
+
+def cross_entropy(
+    logits: mx.array, targets: mx.array, axis: int = -1, reduction: str = "none"
+):
     """
     Computes the cross entropy loss between logits and targets.
 
@@ -10,10 +13,10 @@ def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1, reduction
         logits (mx.array): The predicted logits.
         targets (mx.array): The target values.
         axis (int, optional): The axis over which to compute softmax. Defaults to -1.
-        reduction (str, optional): Specifies the reduction to apply to the output: 'none' | 'mean' | 'sum'. 
-                                    'none': no reduction will be applied. 
+        reduction (str, optional): Specifies the reduction to apply to the output: 'none' | 'mean' | 'sum'.
+                                    'none': no reduction will be applied.
                                     'mean': the sum of the output will be divided by the number of elements in the output.
-                                    'sum': the output will be summed. 
+                                    'sum': the output will be summed.
                                     Defaults to 'none'.
 
     Returns:
@@ -22,14 +25,15 @@ def cross_entropy(logits: mx.array, targets: mx.array, axis: int = -1, reduction
     score = mx.take_along_axis(logits, targets[..., None], axis).squeeze(-1)
     loss = mx.logsumexp(logits, axis=axis) - score
 
-    if reduction == 'mean':
+    if reduction == "mean":
         return mx.mean(loss)
-    elif reduction == 'sum':
+    elif reduction == "sum":
         return mx.sum(loss)
-    elif reduction == 'none':
+    elif reduction == "none":
         return loss
     else:
         raise ValueError("Invalid reduction. Must be 'none', 'mean', or 'sum'.")
+
 
 def l1_loss(predictions: mx.array, targets: mx.array):
     """
@@ -43,4 +47,3 @@ def l1_loss(predictions: mx.array, targets: mx.array):
         mx.array: The computed L1 loss.
     """
     return mx.mean(mx.abs(predictions - targets))
-
