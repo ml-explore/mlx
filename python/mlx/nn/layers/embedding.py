5bc974173da31e992cc382ffbcf0b527b5276a84
Henry Ansah <32216113+henrhie@users.noreply.github.com>
2023-12-13 19:41:41 +0000

    remove padding index

--- python/mlx/nn/layers/embedding.py
+++ python/mlx/nn/layers/embedding.py
@@ -1,10 +1,10 @@
 # Copyright © 2023 Apple Inc.
 
 import math
+from typing import Optional
 
 import mlx.core as mx
 from mlx.nn.layers.base import Module
-from typing import Optional
 
 
 class Embedding(Module):
@@ -19,19 +19,29 @@ class Embedding(Module):
         dims (int): The dimensionality of the embeddings.
     """
 
-    def __init__(self, num_embeddings: int, dims: int, _weight: Optional[mx.array], padding_idx: Optional[int], freeze: Optional[bool]):
+    def __init__(
+        self,
+        num_embeddings: int,
+        dims: int,
+        _weight: Optional[mx.array],
+        padding_idx: Optional[int],
+        freeze: Optional[bool],
+    ):
         super().__init__()
         scale = math.sqrt(1 / dims)
-        if _weight is  None:
+        if _weight is None:
             self.weight = mx.random.normal((num_embeddings, dims)) * scale
         else:
-            assert _weight.shape == [num_embeddings, dims], 'Shape of weight does not match num_embeddings and dims'
+            assert _weight.shape == [
+                num_embeddings,
+                dims,
+            ], "Shape of weight does not match num_embeddings and dims"
             self.weight = _weight
 
-        if(freeze):
+        if freeze:
             self.weight.freeze()
 
-        if(padding_idx is not None):
+        if padding_idx is not None:
             self.weight[padding_idx] = 0
 
     def _extra_repr(self):
