74e6b75a871ba5a0a0baf5567ecd4d1214635cf4
Justin Deschenaux <justin.deschenaux@gmail.com>
2023-12-19 09:26:03 +0100

    Review

--- python/mlx/optimizers.py
+++ python/mlx/optimizers.py
@@ -446,6 +446,7 @@ class Adamax(Adam):
 
 class Lion(Optimizer):
     r"""Implementation of the Lion optimizer [1]. 
+
     Since updates are computed through the sign operation, they tend to 
     have larger norm than for other optimizers such as SGD and Adam. 
     We recommend a learning rate that is 3-10x smaller than AdamW and a 
@@ -485,8 +486,8 @@ class Lion(Optimizer):
     def apply_single(
         self, gradient: mx.array, parameter: mx.array, state: OptimizerState
     ):
-        """Performs the Adam parameter update and stores :math:`v` and
-        :math:`m` in the optimizer state."""
+        """Performs the Lion parameter update and stores :math:`m` 
+        in the optimizer state."""
         lr = self.learning_rate
         b1, b2 = self.betas
         weight_decay = self.weight_decay
