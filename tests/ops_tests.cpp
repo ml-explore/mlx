50b476bbc8682cf92ae08f01cfaa99d0ced6b490
dc-dc-dc <dgcruz983@gmail.com>
2023-12-20 23:15:06 -0500

    fix comma break

--- tests/ops_tests.cpp
+++ tests/ops_tests.cpp
@@ -2504,3 +2504,14 @@ TEST_CASE("test divmod") {
   eval(out_holder);
   CHECK_EQ(out_holder[0].item<float>(), 1.0);
 }
+
+TEST_CASE("test einsum") {
+  auto x = einsum("jki", {zeros({2, 3, 4}, float32)});
+  CHECK_EQ(x.shape(), std::vector<int>{4, 2, 3});
+
+  x = einsum("ij,jk->ik", {full({2, 2}, 2.0f), full({2, 2}, 3.0f)});
+  CHECK_EQ(x.shape(), std::vector<int>{2, 2});
+  CHECK_EQ(x.dtype(), float32);
+  auto expected = array({12.0f, 12.0f, 12.0f, 12.0f}, {2, 2});
+  CHECK_EQ(array_equal(x, expected).item<bool>(), true);
+}
