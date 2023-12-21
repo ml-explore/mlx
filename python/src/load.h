48cf56d666ba782d495d56ab1d9de83bbd82b3ce
Angelos Katharopoulos <a_katharopoulos@apple.com>
2023-12-21 14:08:24 -0800

    Make sure that arrays are freed when saving (#247)

--- python/src/load.h
+++ python/src/load.h
@@ -13,7 +13,10 @@ using namespace mlx::core;
 using DictOrArray = std::variant<array, std::unordered_map<std::string, array>>;
 
 DictOrArray mlx_load_helper(py::object file, StreamOrDevice s);
-void mlx_save_helper(py::object file, array a, bool retain_graph = true);
+void mlx_save_helper(
+    py::object file,
+    array a,
+    std::optional<bool> retain_graph = std::nullopt);
 void mlx_savez_helper(
     py::object file,
     py::args args,
