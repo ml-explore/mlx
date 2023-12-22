0e5ae1d1bfaef8eed3a55896c7430dfbf85664b3
Hazem <hazemessam199@gmail.com>
2023-12-23 00:20:28 +0200

    Added RoPE scaling test

--- python/tests/test_nn.py
+++ python/tests/test_nn.py
@@ -556,7 +556,7 @@ class TestNN(mlx_tests.MLXTestCase):
         )
 
     def test_rope(self):
-        for kwargs in [{}, {"traditional": False}, {"base": 10000}]:
+        for kwargs in [{}, {"traditional": False}, {"base": 10000}, {"scale": 0.25}]:
             rope = nn.RoPE(4, **kwargs)
             shape = (1, 3, 4)
             x = mx.random.uniform(shape=shape)
