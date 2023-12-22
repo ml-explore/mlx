145a4d143d08458be33ca24958d41b57f219af6b
Gabrijel Boduljak <gboduljak@outlook.com>
2023-12-22 04:35:02 +0100

    refactored mlx.linalg.norm bindings

---
+++ python/src/overloaded.h
@@ -0,0 +1,8 @@
+// Copyright © 2023 Apple Inc.
+
+template <typename... Ts>
+struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
