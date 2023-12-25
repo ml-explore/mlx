42a5a512760b7437c5ae0f391bdedc4309a5d163
__mo_san__ <50895527+m0saan@users.noreply.github.com>
2023-12-25 16:32:53 +0100

    implement-batch-norm-layer (#217)
    - Add batch normalization layer
    
    ---------
    
    Co-authored-by: Robert McCraith <mccraithrobert@gmail.com>
    Co-authored-by: Awni Hannun <awni@apple.com>

--- python/mlx/nn/losses.py
+++ python/mlx/nn/losses.py
@@ -6,17 +6,6 @@ import mlx.core as mx
 from mlx.nn.layers.base import Module
 
 
-<<<<<<< HEAD
-def _make_loss_module(f):
-    def decorator(klass):
-        klass.__call__ = lambda self, inputs, targets: f(
-            inputs, targets, self.reduction
-        )
-        return klass
-
-    return decorator
-
-
 def _reduce(loss: mx.array, reduction: str = "none"):
     if reduction == "mean":
         return mx.mean(loss)
@@ -28,8 +17,6 @@ def _reduce(loss: mx.array, reduction: str = "none"):
         raise ValueError("Invalid reduction. Must be 'none', 'mean', or 'sum'.")
 
 
-=======
->>>>>>> 18cca64 (Add smoothed L1 loss and enhancements to cross entropy loss  (#166))
 def cross_entropy(
     logits: mx.array,
     targets: mx.array,
@@ -209,162 +196,6 @@ def kl_div_loss(
     return _reduce(loss, reduction)
 
 
-<<<<<<< HEAD
-def hinge_loss(
-    predictions: mx.array, targets: mx.array, reduction: str = "none"
-) -> mx.array:
-    """
-    Computes the hinge loss between predictions and targets for binary classification tasks.
-
-    Args:
-        predictions (mx.array): The predicted values.
-        targets (mx.array): The target values (-1 or 1).
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed hinge loss.
-    """
-    loss = mx.maximum(0, 1 - targets * predictions)
-    return _reduce(loss, reduction)
-
-
-def huber_loss(
-    predictions: mx.array,
-    targets: mx.array,
-    delta: float = 1.0,
-    reduction: str = "none",
-) -> mx.array:
-    """
-    Computes the Huber loss, a robust loss function for regression tasks.
-
-    Args:
-        predictions (mx.array): The predicted values.
-        targets (mx.array): The target values.
-        delta (float, optional): Threshold for switching between quadratic and linear losses. Default: ``1.0``.
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed Huber loss.
-    """
-    error = mx.abs(predictions - targets)
-    is_small_error = error < delta
-    squared_loss = 0.5 * mx.square(error)
-    linear_loss = delta * error - 0.5 * (delta**2)
-    loss = mx.where(is_small_error, squared_loss, linear_loss)
-    return _reduce(loss, reduction)
-
-
-def dice_loss(
-    inputs: mx.array, targets: mx.array, eps: float = 1e-6, reduction: str = "none"
-) -> mx.array:
-    """
-    Computes the Dice loss, useful for binary segmentation tasks.
-
-    Args:
-        inputs (mx.array): Predicted probabilities for each pixel.
-        targets (mx.array): The target values (binary labels for each pixel).
-        eps (float, optional): Small constant for numerical stability. Default: ``1e-6``.
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed Dice loss.
-    """
-    intersection = mx.sum(inputs * targets, axis=1)  # Sum over the feature dimension
-    union = mx.sum(inputs, axis=1) + mx.sum(targets, axis=1)
-    dice_score = (2.0 * intersection + eps) / (union + eps)
-    loss = 1 - dice_score
-    return _reduce(loss, reduction)
-
-
-def focal_loss(
-    inputs: mx.array,
-    targets: mx.array,
-    alpha: float = 0.25,
-    gamma: float = 2.0,
-    reduction: str = "none",
-) -> mx.array:
-    """
-    Computes the Focal loss, useful for handling class imbalance in binary classification tasks.
-
-    Args:
-        inputs (mx.array): Predicted probabilities for the positive class.
-        targets (mx.array): The target values (binary).
-        alpha (float, optional): Weighting factor for positive examples. Default: ``0.25``.
-        gamma (float, optional): Modulating factor for hard examples. Default: ``2.0``.
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed Focal loss.
-    """
-    BCE_loss = binary_cross_entropy(inputs, targets)
-    pt = mx.exp(-BCE_loss)
-    loss = alpha * (1 - pt) ** gamma * BCE_loss
-    return _reduce(loss, reduction)
-
-
-def contrastive_loss(
-    embeddings1: mx.array,
-    embeddings2: mx.array,
-    targets: mx.array,
-    margin: float = 1.0,
-    reduction: str = "none",
-) -> mx.array:
-    """
-    Computes the Contrastive loss, useful for learning embeddings.
-
-    Args:
-        embeddings1 (mx.array): Embeddings for the first set of samples.
-        embeddings2 (mx.array): Embeddings for the second set of samples.
-        targets (mx.array): The target values (binary labels indicating if pairs are similar or dissimilar).
-        margin (float, optional): Margin for dissimilar pairs. Default: ``1.0``.
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed Contrastive loss.
-    """
-    distances = mx.sqrt(mx.sum(mx.square(embeddings1 - embeddings2), axis=1))
-    loss = targets * distances + (1 - targets) * mx.maximum(0, margin - distances)
-    return _reduce(loss, reduction)
-
-
-def cosine_similarity_loss(
-    embeddings1: mx.array,
-    embeddings2: mx.array,
-    targets: mx.array,
-    eps: float = 1e-8,
-    margin: float = 0.0,
-    reduction: str = "none",
-) -> mx.array:
-    """
-    Computes the Cosine Similarity loss, useful for tasks where the angle between embeddings is important.
-
-    Args:
-        embeddings1 (mx.array): Embeddings for the first set of samples.
-        embeddings2 (mx.array): Embeddings for the second set of samples.
-        targets (mx.array): The target values (cosine similarity between embeddings).
-        margin (float, optional): Margin for dissimilar pairs. Default: ``0.0``.
-        reduction (str, optional): Specifies the reduction to apply to the output:
-          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
-
-    Returns:
-        mx.array: The computed Cosine Similarity loss.
-    """
-    embeddings1_norm = mx.sqrt(mx.sum(mx.square(embeddings1), axis=1) + eps)
-    embeddings2_norm = mx.sqrt(mx.sum(mx.square(embeddings2), axis=1) + eps)
-
-    cos_similarity = mx.sum(embeddings1 * embeddings2, axis=1) / (
-        embeddings1_norm * embeddings2_norm
-    )
-    loss = mx.where(
-        targets == 1, 1 - cos_similarity, mx.maximum(0, cos_similarity - margin)
-    )
-    return _reduce(loss, reduction)
-=======
 def smooth_l1_loss(
     predictions: mx.array, targets: mx.array, beta: float = 1.0, reduction: str = "mean"
 ) -> mx.array:
@@ -461,15 +292,12 @@ def _reduce(loss: mx.array, reduction: str = "none"):
         return loss
     else:
         raise ValueError("Invalid reduction. Must be 'none', 'mean', or 'sum'.")
-<<<<<<< HEAD
->>>>>>> 18cca64 (Add smoothed L1 loss and enhancements to cross entropy loss  (#166))
-=======
 
 
 def hinge_loss(
     inputs: mx.array, targets: mx.array, reduction: str = "none"
 ) -> mx.array:
-    """
+    r"""
     Computes the hinge loss between inputs and targets.
 
     .. math::
@@ -494,7 +322,7 @@ def hinge_loss(
 def huber_loss(
     inputs: mx.array, targets: mx.array, delta: float = 1.0, reduction: str = "none"
 ) -> mx.array:
-    """
+    r"""
     Computes the Huber loss between inputs and targets.
 
     .. math::
@@ -528,7 +356,7 @@ def huber_loss(
 def log_cosh_loss(
     inputs: mx.array, targets: mx.array, reduction: str = "none"
 ) -> mx.array:
-    """
+    r"""
     Computes the log cosh loss between inputs and targets.
 
     Logcosh acts like L2 loss for small errors, ensuring stable gradients,
@@ -555,4 +383,37 @@ def log_cosh_loss(
     loss = mx.logaddexp(errors, -errors) - math.log(2)
 
     return _reduce(loss, reduction)
->>>>>>> d35fa1d (Add Hinge, Huber and LogCosh losses (#199))
+
+
+def cosine_similarity_loss(
+    embeddings1: mx.array,
+    embeddings2: mx.array,
+    targets: mx.array,
+    eps: float = 1e-8,
+    margin: float = 0.0,
+    reduction: str = "none",
+) -> mx.array:
+    """
+    Computes the Cosine Similarity loss, useful for tasks where the angle between embeddings is important.
+
+    Args:
+        embeddings1 (mx.array): Embeddings for the first set of samples.
+        embeddings2 (mx.array): Embeddings for the second set of samples.
+        targets (mx.array): The target values (cosine similarity between embeddings).
+        margin (float, optional): Margin for dissimilar pairs. Default: ``0.0``.
+        reduction (str, optional): Specifies the reduction to apply to the output:
+          ``'none'`` | ``'mean'`` | ``'sum'``. Default: ``'none'``.
+
+    Returns:
+        mx.array: The computed Cosine Similarity loss.
+    """
+    embeddings1_norm = mx.sqrt(mx.sum(mx.square(embeddings1), axis=1) + eps)
+    embeddings2_norm = mx.sqrt(mx.sum(mx.square(embeddings2), axis=1) + eps)
+
+    cos_similarity = mx.sum(embeddings1 * embeddings2, axis=1) / (
+        embeddings1_norm * embeddings2_norm
+    )
+    loss = mx.where(
+        targets == 1, 1 - cos_similarity, mx.maximum(0, cos_similarity - margin)
+    )
+    return _reduce(loss, reduction)
