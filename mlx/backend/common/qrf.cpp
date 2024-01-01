32d0224e913d3e406750af5ea000eada1ab02b16
Taher <8665427+nullhook@users.noreply.github.com>
2024-01-01 12:30:33 -0800

    use xorgqr

--- mlx/backend/common/qrf.cpp
+++ mlx/backend/common/qrf.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include <iostream>
+#include "mlx/allocator.h"
 #include "mlx/backend/common/copy.h"
 #include "mlx/device.h"
 #include "mlx/dtype.h"
@@ -33,21 +34,17 @@ struct lpack<float> {
       __LAPACK_int* _Nonnull info) {
     sgeqrf_(m, n, a, lda, tau, work, lwork, info);
   }
-  static void xormqr(
-      const char* _Nonnull side,
-      const char* _Nonnull trans,
-      const __LAPACK_int* _Nonnull m,
-      const __LAPACK_int* _Nonnull n,
-      const __LAPACK_int* _Nonnull k,
-      float* _Nullable a,
-      const __LAPACK_int* _Nonnull lda,
-      const float* _Nullable tau,
-      float* _Nullable c,
-      const __LAPACK_int* _Nonnull ldc,
-      float* _Nonnull work,
-      const __LAPACK_int* _Nonnull lwork,
-      __LAPACK_int* _Nonnull info) {
-    sormqr_(side, trans, m, n, k, a, lda, tau, c, ldc, work, lwork, info);
+  static void xorgqr(
+      const __LAPACK_int * _Nonnull m,
+  const __LAPACK_int * _Nonnull n,
+  const __LAPACK_int * _Nonnull k,
+  float * _Nullable a,
+  const __LAPACK_int * _Nonnull lda,
+  const float * _Nullable tau,
+  float * _Nonnull work,
+  const __LAPACK_int * _Nonnull lwork,
+  __LAPACK_int * _Nonnull info) {
+    sorgqr_(m, n, k, a, lda, tau, work, lwork, info);
   }
 };
 
@@ -77,41 +74,40 @@ void qrf_impl(array& A, array& Q, array& R) {
 
   // For m ≥ n, R is an upper triangular matrix.
   // For m < n, R is an upper trapezoidal matrix.
-  R = triu(A, 0);
+  array R_ = triu(A, 0);
+  R_.eval();
 
-  int ldc = std::max(1, M);
-  array C = identity(ldc, default_device());
-  C.eval();
-  const char side = 'L';
-  const char trans = 'N';
-  const int K = tau.size(); // no. of elementary reflectors
+  R.set_data(
+    allocator::malloc_or_wait(R_.nbytes()),
+    R_.data_size(),
+    R_.strides(),
+    R_.flags());
 
-  assert(side == 'L' && M >= K && K >= 0);
+  copy_inplace(R_, R, CopyType::Vector);
+
+  // No. of elementary reflectors
+  const int K = tau.size();
 
   // retrieve Q from the elementary reflectors
   // uses the same worksize as before
-  lpack<T>::xormqr(
-      &side,
-      &trans,
+  lpack<T>::xorgqr(
       &M,
       &N,
       &K,
       A.data<T>(),
       &lda,
       tau.data<T>(),
-      C.data<float>(),
-      &ldc,
       work.data<T>(),
       &lwork,
       &info);
 
   Q.set_data(
-      allocator::malloc_or_wait(C.nbytes()),
-      C.data_size(),
-      C.strides(),
-      C.flags());
+      allocator::malloc_or_wait(A.nbytes()),
+      A.data_size(),
+      A.strides(),
+      A.flags());
 
-  copy_inplace(C, Q, CopyType::Vector);
+  copy_inplace(A, Q, CopyType::Vector);
 }
 
 void QRF::eval(const std::vector<array>& inputs, array& out) {
@@ -119,27 +115,18 @@ void QRF::eval(const std::vector<array>& inputs, array& out) {
 
   array A = inputs[0];
 
-  if (!is_floating_point(A.dtype())) {
+  if (!(A.dtype() == Dtype::Val::float32)) {
     throw std::runtime_error(
-        "QR factorization is only supported for floating point types.");
+        "QR factorization is only supported for floating point 32bit type.");
   }
 
-  std::cout << "A:" << A << "\n";
-  auto fnormA = sqrt(sum(square(A)));
-  std::cout << "fnorm A: " << fnormA << "\n";
-
   array Q = out;
-  array R = out;
+  array R = zeros(out.shape());
+  R.eval();
   qrf_impl<float>(A, Q, R);
 
-  std::cout << "Q:" << Q << "\n";
-  std::cout << "R:" << R << "\n";
-  auto qr = matmul(Q, R);
-  std::cout << "QR: " << qr << "\n";
-  auto diff = subtract(A, qr);
-  std::cout << "A - QR: " << diff << "\n";
-  auto fnorm = sqrt(sum(square(diff)));
-  std::cout << "fnorm: " << fnorm << "\n";
+  std::cout << "Q: " <<  Q << "\n";
+  std::cout << "R: " <<  R << "\n";
 }
 
 } // namespace mlx::core
