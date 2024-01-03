1720703528be43ad5e6dbc89330fc7d7b593997f
Juarez Bochi <juarez.bochi@grammarly.com>
2024-01-03 13:48:14 -0500

    Only convert to float if there's no equivalent type

--- mlx/io/gguf.cpp
+++ mlx/io/gguf.cpp
@@ -2,6 +2,61 @@
 
 namespace mlx::core {
 
+std::optional<uint32_t> dtype_to_gguf_type(const Dtype& dtype) {
+  switch (dtype) {
+    case float32:
+      return GGUF_TYPE_F32;
+    case float16:
+      return GGUF_TYPE_F16;
+    case int8:
+      return GGUF_TYPE_I8;
+    case int16:
+      return GGUF_TYPE_I16;
+    case int32:
+      return GGUF_TYPE_I32;
+    default:
+      return {};
+  }
+}
+
+std::optional<Dtype> gguf_type_to_dtype(const uint32_t& gguf_type) {
+  switch (gguf_type) {
+    case GGUF_TYPE_F32:
+      return float32;
+    case GGUF_TYPE_F16:
+      return float16;
+    case GGUF_TYPE_I8:
+      return int8;
+    case GGUF_TYPE_I16:
+      return int16;
+    case GGUF_TYPE_I32:
+      return int32;
+    default:
+      return {};
+  }
+}
+
+std::tuple<allocator::Buffer, Dtype> extract_tensor_data(gguf_tensor* tensor) {
+  std::optional<Dtype> equivalent_dtype = gguf_type_to_dtype(tensor->type);
+  // If there's an equivalent type, we can simply copy.
+  if (equivalent_dtype.has_value()) {
+    allocator::Buffer buffer = allocator::malloc(tensor->bsize);
+    memcpy(
+        buffer.raw_ptr(),
+        tensor->weights_data,
+        tensor->num_weights * equivalent_dtype.value().size);
+    return {buffer, equivalent_dtype.value()};
+  }
+  // Otherwise, we need to convert to float32.
+  const float* data = gguf_tensor_to_float(tensor);
+  if (data == NULL) {
+    throw std::runtime_error("[load_gguf] gguf_tensor_to_float failed");
+  }
+  allocator::Buffer buffer = allocator::malloc(tensor->bsize);
+  std::copy(data, data + tensor->num_weights, (float*)buffer.raw_ptr());
+  return {buffer, float32};
+}
+
 std::unordered_map<std::string, array> load_gguf(
     const std::string& file,
     StreamOrDevice s) {
@@ -15,19 +70,12 @@ std::unordered_map<std::string, array> load_gguf(
   uint64_t params = 0;
   while (gguf_get_tensor(ctx, &tensor)) {
     params += tensor.num_weights;
-
     std::vector<int> shape;
     for (int i = 0; i < tensor.ndim; i++) {
       shape.push_back(tensor.dim[i]);
     }
-    const float* data = gguf_tensor_to_float(&tensor);
-    if (data == NULL) {
-      throw std::runtime_error("[load_gguf] gguf_tensor_to_float failed");
-    }
-    allocator::Buffer buffer = allocator::malloc(tensor.bsize);
-    std::copy(data, data + tensor.num_weights, (float*)buffer.raw_ptr());
-    array loaded_array = array(data, shape, float32);
-
+    const auto& [data, dtype] = extract_tensor_data(&tensor);
+    array loaded_array = array(data, shape, dtype);
     std::string name = std::string(tensor.name, tensor.namelen);
     result.insert({name, loaded_array});
   }
@@ -58,10 +106,11 @@ void save_gguf(
     arr.eval(retain_graph.value_or(arr.is_tracer()));
 
     tensor_offset += gguf_get_alignment_padding(ctx->alignment, tensor_offset);
-    if (arr.dtype() != float32) {
-      throw std::runtime_error("[save_gguf] only float32 supported");
+    const std::optional<uint32_t> gguf_type = dtype_to_gguf_type(arr.dtype());
+
+    if (!gguf_type.has_value()) {
+      throw std::runtime_error("[save_gguf] dtype is not supported");
     }
-    const uint32_t type = GGUF_TYPE_F32;
     const char* tensorname = key.c_str();
     const uint64_t namelen = key.length();
     const uint32_t num_dim = arr.shape().size();
@@ -70,7 +119,13 @@ void save_gguf(
       dim[i] = arr.shape()[i];
     }
     if (!gguf_append_tensor_info(
-            ctx, tensorname, namelen, num_dim, dim, type, tensor_offset)) {
+            ctx,
+            tensorname,
+            namelen,
+            num_dim,
+            dim,
+            gguf_type.value(),
+            tensor_offset)) {
       throw std::runtime_error("[save_gguf] gguf_append_tensor_info failed");
     }
     tensor_offset += arr.nbytes();
@@ -78,7 +133,7 @@ void save_gguf(
 
   // Then, append the tensor weights
   for (const auto& [key, arr] : a) {
-    if (!gguf_append_tensor_data(ctx, (void*)arr.data<float>(), arr.nbytes())) {
+    if (!gguf_append_tensor_data(ctx, (void*)arr.data<void>(), arr.nbytes())) {
       throw std::runtime_error("[save_gguf] gguf_append_tensor_data failed");
     }
   }
