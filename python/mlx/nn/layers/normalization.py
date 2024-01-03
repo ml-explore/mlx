e47eab6d243ec274eb2113161fe95b09175c163d
Gabrijel Boduljak <gboduljak@outlook.com>
2024-01-03 02:40:09 +0100

    addressed PR review comments

--- python/mlx/nn/layers/normalization.py
+++ python/mlx/nn/layers/normalization.py
@@ -16,68 +16,55 @@ class InstanceNorm(Module):
         y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta,
 
     where :math:`\gamma` and :math:`\beta` are learned per feature dimension
-    parameters initialized at 1 and 0 respectively. Both are of size num_features,
+    parameters initialized at 1 and 0 respectively. Both are of size dims,
     if :attr:`affine` is ``True``.
 
     [1]: https://arxiv.org/abs/1607.08022
 
     Args:
-        num_features: number of features of the input
-        eps: a value added to the denominator for numerical stability. Default: 1e-5
-        momentum: the value used for the running_mean and running_var computation. Default: 0.1
-        affine: a boolean value that when set to ``True``, this module has
-            learnable affine parameters, initialized the same way as done for batch normalization.
+        dims (int): The number of features of the input.
+        eps (float): A value added to the denominator for numerical stability. Default: ``1e-5``.
+        affine (bool): A boolean value that indicates whether this module has learnable affine parameters,
+            initialized the same way as done for batch normalization.
             Default: ``False``.
     Shape:
-        - Input: :math:`(N, C, L)` or :math:`(N, C, H, W)` or :math:`(N, C, D, H, W)`
-        - Output: :math:`(N, C, L)` or :math:`(N, C, H, W)` or :math:`(N, C, D, H, W)` (same shape as input)
+        - Input: :math:`(N, L, C)` or :math:`(N, H, W, C)` or :math:`(N, D, H, W, C)`
+        - Output: :math:`(N, L, C)` or :math:`(N, H, W, C)` or :math:`(N, D, H, W, C)` (same shape as input)
+
+    Examples:
+        >>> import mlx.core as mx
+        >>> import mlx.nn as nn
+        >>> x = mx.random.normal((8, 4, 4, 16))
+        >>> inorm = nn.InstanceNorm(dims=16, affine=True)
+        >>> output = inorm(x)
     """
 
     def __init__(
         self,
-        num_features: int,
+        dims: int,
         eps: float = 1e-5,
         affine: bool = False,
     ):
         super().__init__()
         if affine:
-            self.weight = mx.ones((num_features,))
-            self.bias = mx.zeros((num_features,))
+            self.weight = mx.ones((dims,))
+            self.bias = mx.zeros((dims,))
+        self.dims = dims
         self.eps = eps
         self.affine = affine
-        self.param_shapes = {
-            3: (1, num_features, 1),  # input shape: (B, C, L)
-            4: (1, num_features, 1, 1),  # input shape: (B, C, H, W)
-            5: (1, num_features, 1, 1, 1),  # input shape: (B, C, D, H, W)
-        }
-        self.reduction_axes = {
-            3: [2],  # input shape: (B, C, L)
-            4: [2, 3],  # input shape: (B, C, H, W)
-            5: [2, 3, 4],  # input shape: (B, C, D, H, W)
-        }
 
-    def extra_repr(self):
-        return "{num_features}, eps={eps}, momentum={momentum}, affine={affine}".format(
-            **self.__dict__
-        )
+    def _extra_repr(self):
+        return f"{self.dims}, eps={self.eps}, affine={'weight' in self}"
 
     def __call__(self, x: mx.array) -> mx.array:
-        if self.affine and self.weight.ndim != x.ndim:
-            if x.ndim not in self.reduction_axes:
-                raise ValueError("Unsupported number shape")
-            # Ensure parameters are reshaped for correct broadcasting
-            self.weight = mx.reshape(self.weight, self.param_shapes[x.ndim])
-            self.bias = mx.reshape(self.bias, self.param_shapes[x.ndim])
+        reduction_axes = tuple(range(1, x.ndim - 1))
         # Compute stats
-        mean = mx.mean(x, axis=self.reduction_axes[x.ndim], keepdims=True)
-        var = mx.var(x, axis=self.reduction_axes[x.ndim], keepdims=True)
+        mean = mx.mean(x, axis=reduction_axes, keepdims=True)
+        var = mx.var(x, axis=reduction_axes, keepdims=True)
         # Normalize
         normalized = (x - mean) * mx.rsqrt(var + self.eps)
         # Scale and shift if necessary
-        if self.affine:
-            return self.weight * normalized + self.bias
-        else:
-            return normalized
+        return self.weight * normalized + self.bias if "weight" in self else normalized
 
 
 class LayerNorm(Module):
