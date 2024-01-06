a6a2012d3a8be7cb2002aa7d23c45ba72a3d34ed
Gabrijel Boduljak <gboduljak@outlook.com>
2024-01-06 19:19:05 +0100

    refactored pooling

--- python/mlx/nn/layers/pooling.py
+++ python/mlx/nn/layers/pooling.py
@@ -33,7 +33,7 @@ class Pooling(Module):
             raise ValueError(
                 "the number of provided padding values must match the number of feature axes"
             )
-        return [(0, 0)] + self.padding + [(0, 0)]
+        return [(0, 0)] + [(p, p) for p in self.padding] + [(0, 0)]
 
     def _get_strides(self, features_sizes: List[int]) -> List[int]:
         if isinstance(self.stride, int):
@@ -54,28 +54,20 @@ class Pooling(Module):
     def _pad(self, a: mx.array) -> mx.array:
         feature_size = a.shape[1:-1]
         padding_value = {"max": float("-inf"), "mean": 0}[self.mode]
-        return mx.pad(
-            a, pad_width=self._get_padding(feature_size), constant_values=padding_value
-        )
+        return mx.pad(a, self._get_padding(feature_size), padding_value)
 
     def __call__(self, a: mx.array) -> mx.array:
         if a.ndim < 2:
             raise ValueError("the input must be at least two-dimensional.")
-
         # Select pooling operator
         pool = {"max": mx.max, "mean": mx.mean}[self.mode]
         # Pad if necessary
         a = self._pad(a)
-
-        # a : [B, ..., C]
-        batch_size = a.shape[0]
-        feature_size = a.shape[1:-1]
-        channels = a.shape[-1]
-
-        batch_stride = a.strides[0]
-        feature_strides = a.strides[1:-1]
-        channels_stride = a.strides[-1]
-
+        # Assumes a.shape = (batch_size, ..., num_channels)
+        batch_size, batch_stride = a.shape[0], a.strides[0]
+        feature_size, feature_strides = a.shape[1:-1], a.strides[1:-1]
+        num_channels, channels_stride = a.shape[-1], a.strides[-1]
+        # Get kernel size and strides
         kernel_size = self._get_kernel_size(feature_size)
         strides = self._get_strides(feature_size)
         # Compute windows
@@ -86,7 +78,7 @@ class Pooling(Module):
                 for (f, k, s) in zip(feature_size, kernel_size, strides)
             ]
             + kernel_size
-            + [channels]
+            + [num_channels]
         )
         windows_strides = tuple(
             [batch_stride]
@@ -97,6 +89,6 @@ class Pooling(Module):
         windows = mx.as_strided(a, windows_shape, windows_strides)
         # Reduce over windows
         reduction_axes = (
-            -1 * mx.arange(start=2, stop=2 + len(kernel_size), step=1) + windows.ndim
+            -1 * mx.arange(start=2, stop=2 + len(kernel_size), step=1)
         ).tolist()
         return pool(windows, reduction_axes)
