4124f02c48222db05bd7c281d6154f7a5b54d3e5
dc-dc-dc <dgcruz983@gmail.com>
2024-01-06 12:12:27 -0500

    moved einsum naive

--- mlx/einsum.h
+++ mlx/einsum.h
@@ -1,9 +1,12 @@
+// Copyright © 2023 Apple Inc.
+
+#include <map>
 #include <set>
 #include <string>
 #include <tuple>
 #include <vector>
 
-#include "array.h"
+#include "mlx/array.h"
 
 namespace mlx::core {
 std::vector<std::tuple<
@@ -13,4 +16,9 @@ std::vector<std::tuple<
     std::vector<std::string>,
     bool>>
 einsum_path(const std::string& equation, const std::vector<array>& operands);
-}
+
+std::pair<std::vector<std::string>, std::string> einsum_parse(
+    const std::string& equation);
+
+std::map<char, int> str_idx_map(const std::string inp);
+} // namespace mlx::core
