b1d6a1e40d5520799d5cf8c6b90a975a887b836b
Gabrijel Boduljak <gboduljak@outlook.com>
2024-01-06 18:57:37 +0100

    removed pooling demo

--- python/pooling_demo.py
+++
@@ -1,9 +0,0 @@
-import mlx.core as mx
-from mlx.nn.layers import MaxPooling2d
-
-a = mx.arange(16).reshape((4, 4))
-pool_no_padding = MaxPooling2d(kernel_size=2, stride=2, padding=0)
-print(pool_no_padding(a))
-print("--")
-pool_with_padding = MaxPooling2d(kernel_size=2, stride=2, padding=1)
-print(pool_with_padding(a))
