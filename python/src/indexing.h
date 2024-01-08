cfdd6ddcd3db211cada9186abcf2f003af0960d9
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-01-07 23:26:36 -0800

    Add array.at to implement user friendly scatters

--- python/src/indexing.h
+++ python/src/indexing.h
@@ -12,3 +12,11 @@ using namespace mlx::core;
 
 array mlx_get_item(const array& src, const py::object& obj);
 void mlx_set_item(array& src, const py::object& obj, const ScalarOrArray& v);
+array mlx_add_item(
+    const array& src,
+    const py::object& obj,
+    const ScalarOrArray& v);
+array mlx_subtract_item(
+    const array& src,
+    const py::object& obj,
+    const ScalarOrArray& v);
