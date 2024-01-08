7cca49951e374d312cacabc8a06dac3d2f8dc552
Awni Hannun <awni@apple.com>
2024-01-07 20:00:01 -0800

    format

--- mlx/backend/metal/metal.cpp
+++ mlx/backend/metal/metal.cpp
@@ -46,41 +46,39 @@ std::function<void()> make_task(
     array& arr,
     std::vector<std::shared_future<void>> deps,
     std::shared_ptr<std::promise<void>> p) {
-  auto task =
-      [arr, deps = std::move(deps), p = std::move(p)]() mutable {
-        auto pool = new_scoped_memory_pool();
-        for (auto& d : deps) {
-          d.wait();
-        }
-        auto s = arr.primitive().stream();
-        auto command_buffer = increment_command_buffer(s);
-        auto outputs = arr.outputs();
-        arr.primitive().eval_gpu(arr.inputs(), outputs);
-        if (p) {
-          metal::device(s.device).end_encoding(s.index);
-          scheduler::notify_new_task(s);
-          command_buffer->addCompletedHandler(
-              [s, arr, p = std::move(p)](
-                  MTL::CommandBuffer*) mutable {
-                if (!arr.is_tracer()) {
-                  arr.detach();
-                  for (auto s : arr.siblings()) {
-                    s.detach();
-                  }
-                }
-                p->set_value();
-                scheduler::notify_task_completion(s);
-              });
-          metal::device(s.device).commit_command_buffer(s.index);
-        } else {
-          command_buffer->addCompletedHandler(
-              [s, arr](MTL::CommandBuffer*) mutable {
-                if (!arr.is_tracer()) {
-                  arr.detach();
-                }
-              });
-        }
-      };
+  auto task = [arr, deps = std::move(deps), p = std::move(p)]() mutable {
+    auto pool = new_scoped_memory_pool();
+    for (auto& d : deps) {
+      d.wait();
+    }
+    auto s = arr.primitive().stream();
+    auto command_buffer = increment_command_buffer(s);
+    auto outputs = arr.outputs();
+    arr.primitive().eval_gpu(arr.inputs(), outputs);
+    if (p) {
+      metal::device(s.device).end_encoding(s.index);
+      scheduler::notify_new_task(s);
+      command_buffer->addCompletedHandler(
+          [s, arr, p = std::move(p)](MTL::CommandBuffer*) mutable {
+            if (!arr.is_tracer()) {
+              arr.detach();
+              for (auto s : arr.siblings()) {
+                s.detach();
+              }
+            }
+            p->set_value();
+            scheduler::notify_task_completion(s);
+          });
+      metal::device(s.device).commit_command_buffer(s.index);
+    } else {
+      command_buffer->addCompletedHandler(
+          [s, arr](MTL::CommandBuffer*) mutable {
+            if (!arr.is_tracer()) {
+              arr.detach();
+            }
+          });
+    }
+  };
   return task;
 }
 
