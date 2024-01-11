ef5b83333e8d557ec635dac42d853dc965ccdf6c
Avikant Srivastava <asdavikant@gmail.com>
2024-01-11 19:58:51 +0530

    add test for newaxis transformation

--- python/tests/test_constants.py
+++ python/tests/test_constants.py
@@ -48,6 +48,13 @@ class TestConstants(mlx_tests.MLXTestCase):
         self.assertTrue(hasattr(mx, "newaxis"))
         self.assertTrue(hasattr(mx, "pi"))
 
+    def test_newaxis_for_reshaping_arrays(self):
+        arr_1d = mx.array([1, 2, 3, 4, 5])
+        arr_2d_column = arr_1d[:, mx.newaxis]
+        expected_result = mx.array([[1], [2], [3], [4], [5]])
+
+        self.assertTrue(mx.array_equal(arr_2d_column, expected_result))
+
 
 if __name__ == "__main__":
     unittest.main()
