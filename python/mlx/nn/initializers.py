111f6c1add4d0423b5643fec5a2dba0e0b5ad77c
Leon Ericsson <leon.ericsson@icloud.com>
2024-01-17 18:10:25 +0100

    nits

--- python/mlx/nn/initializers.py
+++ python/mlx/nn/initializers.py
@@ -1,9 +1,13 @@
 # Copyright © 2023 Apple Inc.
 
-import mlx.core as mx
 from typing import Callable
 
-def constant(value: float, dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
+import mlx.core as mx
+
+
+def constant(
+    value: float, dtype: mx.Dtype = mx.float32
+) -> Callable[[mx.array], mx.array]:
     r"""Build an initializer that returns a array filled with 'value'.
 
     Args:
@@ -21,7 +25,9 @@ def constant(value: float, dtype: mx.Dtype = mx.float32) -> Callable[[mx.array],
     return initializer
 
 
-def normal(mean: float = 0.0, std: float = 1.0, dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
+def normal(
+    mean: float = 0.0, std: float = 1.0, dtype: mx.Dtype = mx.float32
+) -> Callable[[mx.array], mx.array]:
     r"""Build an initializer that returns random values from a normal distribution.
 
     Args:
@@ -42,7 +48,9 @@ def normal(mean: float = 0.0, std: float = 1.0, dtype: mx.Dtype = mx.float32) ->
     return initializer
 
 
-def uniform(low: float = 0.0, high: float = 1.0, dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
+def uniform(
+    low: float = 0.0, high: float = 1.0, dtype: mx.Dtype = mx.float32
+) -> Callable[[mx.array], mx.array]:
     r"""Build an initializer that returns random values from a uniform distribution.
 
     Args:
@@ -69,13 +77,15 @@ def identity(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
         dtype (Dtype, optional): The data type of the array. Defaults to mx.float32.
 
     Returns:
-        Callable[[mx.array], mx.array]: An initializer that returns an identity array, of 
+        Callable[[mx.array], mx.array]: An initializer that returns an identity array, of
                                     the same shape as the input array.
     """
 
     def initializer(arr: mx.array) -> mx.array:
         if arr.ndim != 2 or arr.shape[0] != arr.shape[1]:
-            raise ValueError("The input array must be square (same number of rows and columns).")
+            raise ValueError(
+                "The input array must be square (same number of rows and columns)."
+            )
         return mx.core.eye(n=arr.shape[0], dtype=dtype)
 
     return initializer
@@ -99,77 +109,84 @@ def _calculate_fan_in_fan_out(x):
     return fan_in, fan_out
 
 
-def glorot_normal(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
+def glorot_normal(
+    dtype: mx.Dtype = mx.float32,
+) -> Callable[[mx.array, mx.float32], mx.array]:
     r"""Build a Xavier Glorot normal initializer.
 
-    This initializer generates values from a normal distribution centered around 0. 
-    The standard deviation is calculated based on the number of input (`fan_in`) and 
-    output (`fan_out`) units in the weight tensor. The method is described in 
-    `Understanding the difficulty of training deep feedforward neural networks` 
+    This initializer generates values from a normal distribution centered around 0.
+    The standard deviation is calculated based on the number of input (`fan_in`) and
+    output (`fan_out`) units in the weight tensor. The method is described in
+    `Understanding the difficulty of training deep feedforward neural networks`
     - Glorot, X. & Bengio, Y. (2010).
 
     Args:
         dtype (mx.Dtype, optional): The data type of the array. Defaults to mx.float32.
 
     Returns:
-        Callable[[mx.array], mx.array]: An initializer that returns an array, of the same
-                                    shape as the input array, filled with random values from 
+        Callable[[mx.array, mx.float32], mx.array]: An initializer that returns an array, of the same
+                                    shape as the input array, filled with random values from
                                     the Glorot normal distribution.
     """
 
-    def initializer(a: mx.array) -> mx.array:
+    def initializer(a: mx.array, gain: mx.float32 = 1.0) -> mx.array:
         fan_in, fan_out = _calculate_fan_in_fan_out(a)
-        gain = 1.0
         std = gain * mx.sqrt(2.0 / (fan_in + fan_out))
         return mx.random.normal(0.0, std, a.shape, dtype=dtype)
 
     return initializer
 
 
-def glorot_uniform(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array], mx.array]:
+def glorot_uniform(
+    dtype: mx.Dtype = mx.float32,
+) -> Callable[[mx.array, mx.float32], mx.array]:
     r"""Build a Xavier Glorot uniform initializer.
 
-    This initializer generates values from a uniform distribution within a range 
-    determined by the number of input (`fan_in`) and output (`fan_out`) units in the 
-    weight tensor. The method is described in  `Understanding the difficulty of training 
+    This initializer generates values from a uniform distribution within a range
+    determined by the number of input (`fan_in`) and output (`fan_out`) units in the
+    weight tensor. The method is described in  `Understanding the difficulty of training
     deep feedforward neural networks` - Glorot, X. & Bengio, Y. (2010).
 
     Args:
         dtype (mx.Dtype, optional): The data type of the array. Defaults to mx.float32.
 
     Returns:
-        Callable[[mx.array], mx.array]: An initializer that returns an array, of the same
-                                    shape as the input array, filled with random values from 
+        Callable[[mx.array, mx.array], mx.array]: An initializer that returns an array, of the same
+                                    shape as the input array, filled with random values from
                                     the Glorot uniform distribution.
     """
 
-    def initializer(a: mx.array) -> mx.array:
+    def initializer(a: mx.array, gain: mx.float32 = 1.0) -> mx.array:
         fan_in, fan_out = _calculate_fan_in_fan_out(a)
-        gain = 1.0 
         limit = gain * mx.sqrt(6.0 / (fan_in + fan_out))
         return mx.random.uniform(-limit, limit, a.shape, dtype=dtype)
 
     return initializer
 
-def he_normal(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array, str], mx.array]:
-    r"""Build a Kaming He normal initializer. 
 
-    This initializer generates values from a normal distribution centered around 0, 
-    with a standard deviation calculated based on the number of input (`fan_in`) or 
-    output (`fan_out`) units in the weight tensor. The method is described in `Delving 
-    deep into rectifiers: Surpassing human-level performance on ImageNet classification` 
-    - He, K. et al. (2015). 
+def he_normal(
+    dtype: mx.Dtype = mx.float32,
+) -> Callable[[mx.array, str, mx.float32], mx.array]:
+    r"""Build a Kaming He normal initializer.
+
+    This initializer generates values from a normal distribution centered around 0,
+    with a standard deviation calculated based on the number of input (`fan_in`) or
+    output (`fan_out`) units in the weight tensor. The method is described in `Delving
+    deep into rectifiers: Surpassing human-level performance on ImageNet classification`
+    - He, K. et al. (2015).
 
     Args:
         dtype (mx.Dtype, optional): The data type of the array. Defaults to mx.float32.
 
     Returns:
-        Callable[[mx.array, str], mx.array]: An initializer that returns an array, of the same
-                                        shape as the input array, filled with random values from 
+        Callable[[mx.array, str, mx.float32], mx.array]: An initializer that returns an array, of the same
+                                        shape as the input array, filled with random values from
                                         the He normal distribution.
     """
-    
-    def initializer(a: mx.array, mode: str = "fan_in") -> mx.array:
+
+    def initializer(
+        a: mx.array, mode: str = "fan_in", gain: mx.float32 = 1.0
+    ) -> mx.array:
         fan_in, fan_out = _calculate_fan_in_fan_out(a)
         if mode == "fan_in":
             fan = fan_in
@@ -178,31 +195,34 @@ def he_normal(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array, str], mx.arra
         else:
             raise ValueError(f"Invalid mode: {mode}. Valid modes are: fan_in, fan_out")
 
-        gain = 2.0
         std = gain / mx.sqrt(fan)
         return mx.random.normal(0, std, a.shape, dtype=dtype)
 
     return initializer
 
 
-def he_uniform(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array, str], mx.array]:
+def he_uniform(
+    dtype: mx.Dtype = mx.float32,
+) -> Callable[[mx.array, str, mx.float32], mx.array]:
     r"""Create a He uniform (Kaiming uniform) initializer.
 
-    This initializer generates values from a uniform distribution within a range 
-    determined by the number of input (`fan_in`) or output (`fan_out`) units in the 
-    weight tensor. The method is described in `Delving deep into rectifiers: Surpassing 
-    human-level performance on ImageNet classification` - He, K. et al. (2015). 
+    This initializer generates values from a uniform distribution within a range
+    determined by the number of input (`fan_in`) or output (`fan_out`) units in the
+    weight tensor. The method is described in `Delving deep into rectifiers: Surpassing
+    human-level performance on ImageNet classification` - He, K. et al. (2015).
 
     Args:
         dtype (mx.Dtype, optional): The data type of the array. Defaults to mx.float32.
 
     Returns:
-        Callable[[mx.array, str], mx.array]: An initializer that returns an array, of the same
-                                        shape as the input array, filled with random values from 
+        Callable[[mx.array, str, mx.float32], mx.array]: An initializer that returns an array, of the same
+                                        shape as the input array, filled with random values from
                                         the He uniform distribution.
     """
-    
-    def initializer(a: mx.array, mode: str = "fan_in") -> mx.array:
+
+    def initializer(
+        a: mx.array, mode: str = "fan_in", gain: mx.float32 = 1.0
+    ) -> mx.array:
         fan_in, fan_out = _calculate_fan_in_fan_out(a)
         if mode == "fan_in":
             fan = fan_in
@@ -211,7 +231,6 @@ def he_uniform(dtype: mx.Dtype = mx.float32) -> Callable[[mx.array, str], mx.arr
         else:
             raise ValueError(f"Invalid mode: {mode}. Valid modes are: fan_in, fan_out")
 
-        gain = 2.0
         limit = gain * mx.sqrt(3.0 / fan)
         return mx.random.uniform(-limit, limit, a.shape, dtype=dtype)
 
