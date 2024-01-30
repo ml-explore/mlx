46dbed77fc3d99491c331a097c1ba5143dd00633
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-01-29 21:10:47 -0800

    Fix c++ tests for custom vjp and checkpointing

--- tests/custom_vjp_tests.cpp
+++ tests/custom_vjp_tests.cpp
@@ -14,16 +14,13 @@ TEST_CASE("test simple custom vjp") {
   auto fn = [](const std::vector<array>& inputs) {
     return std::vector<array>{inputs[0] * inputs[1], inputs[0] + inputs[1]};
   };
-  auto wrapped = [&fn, &one](const std::vector<array>& inputs) {
-    return custom_vjp(
-        fn,
-        [&](const std::vector<array>&,
-            const std::vector<array>&,
-            const std::vector<array>&) {
-          return std::vector<array>{one, one};
-        },
-        inputs);
-  };
+  auto transformed_fn = custom_vjp(
+      fn,
+      [&](const std::vector<array>&,
+          const std::vector<array>&,
+          const std::vector<array>&) {
+        return std::vector<array>{one, one};
+      });
 
   auto [z, g] = vjp(fn, {x, y}, {one, one});
   CHECK_EQ(z[0].item<float>(), 6.0f);
@@ -31,7 +28,7 @@ TEST_CASE("test simple custom vjp") {
   CHECK_EQ(g[0].item<float>(), 4.0f);
   CHECK_EQ(g[1].item<float>(), 3.0f);
 
-  std::tie(z, g) = vjp(wrapped, {x, y}, {one, one});
+  std::tie(z, g) = vjp(transformed_fn, {x, y}, {one, one});
   CHECK_EQ(z[0].item<float>(), 6.0f);
   CHECK_EQ(z[1].item<float>(), 5.0f);
   CHECK_EQ(g[0].item<float>(), 1.0f);
@@ -50,11 +47,9 @@ TEST_CASE("test checkpointing") {
     auto y = inputs[0] + inputs[1];
     return std::vector<array>{square(x + y)};
   };
-  auto wrapped = [&fn, &one](const std::vector<array>& inputs) {
-    return checkpoint(fn, inputs);
-  };
+  auto checkpointed_fn = checkpoint(fn);
 
-  auto [z, g] = vjp(wrapped, {x, y}, {one});
+  auto [z, g] = vjp(checkpointed_fn, {x, y}, {one});
   CHECK_EQ(z[0].item<float>(), 121.0f);
   CHECK_EQ(g[0].item<float>(), 88.0f);
   CHECK_EQ(g[1].item<float>(), 66.0f);
