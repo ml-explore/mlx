221f8d3fc2b0cebfee7fb8aa157c67e3947012a0
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-02-08 11:27:12 -0800

    Bump the version to 0.2 (#656)

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
 
     setup(
         name="mlx",
-        version=get_version("0.1.0"),
+        version=get_version("0.2.0"),
         author="MLX Contributors",
         author_email="mlx@group.apple.com",
         description="A framework for machine learning on Apple silicon.",
