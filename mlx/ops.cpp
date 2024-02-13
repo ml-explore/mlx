e45470f81d3b0881d21da1e01313ec4de3d10bb2
Noah Farr <login@noahfarr.com>
2024-02-13 23:10:19 +0100

    Return empty array when repeats is 0

--- mlx/ops.cpp
+++ mlx/ops.cpp
@@ -774,7 +774,7 @@ array repeat(const array& arr, int repeats, int axis, StreamOrDevice s) {
   }
 
   if (repeats == 0) {
-    return array({}, arr.dtype());
+    return array(std::initializer_list<int>{}, arr.dtype());
   }
 
   if (repeats == 1) {
