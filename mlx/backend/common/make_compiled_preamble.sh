38abbd2a72989fffbb25a23441c00f12ed86887d
Awni Hannun <awni@apple.com>
2024-02-18 21:25:35 -0800

    try to fix includes

--- mlx/backend/common/make_compiled_preamble.sh
+++ mlx/backend/common/make_compiled_preamble.sh
@@ -9,13 +9,24 @@
 OUTPUT_FILE=$1
 GCC=$2
 SRCDIR=$3
+CLANG=$4
 
-CONTENT=$($CC -I $SRCDIR -E $SRCDIR/mlx/backend/common/compiled_preamble.h 2>/dev/null)
+if [ $CLANG = "TRUE" ]; then
+  read -r -d '' INCLUDES <<- EOM
+  #include <cmath>
+  #include <complex>
+  #include <cstdint>
+  #include <vector>
+EOM
 
-echo $OUTPUT_FILE
+fi
+
+CONTENT=$($GCC -I $SRCDIR -E $SRCDIR/mlx/backend/common/compiled_preamble.h) # 2>/dev/null)
+
 cat << EOF > "$OUTPUT_FILE"
 const char* get_kernel_preamble() {
 return R"preamble(
+$INCLUDES
 $CONTENT
 using namespace mlx::core::detail;
 )preamble";
