c82b570b4e8ffd769e66688d0a5b8c3e43b24f07
Gleb Pobudzey <gleb.pobudzey@gmail.com>
2024-02-22 12:31:36 -0500

    Added ternary op

--- mlx/backend/common/select.cpp
+++ mlx/backend/common/select.cpp
@@ -6,73 +6,59 @@
 #include <sstream>
 
 #include "mlx/allocator.h"
-#include "mlx/backend/common/binary.h"
-#include "mlx/backend/common/select.h"
+#include "mlx/backend/common/ternary.h"
 #include "mlx/primitives.h"
 
 namespace mlx::core {
 
 namespace {
 
-template <typename T>
-void select_binary_op(
+template <typename Op>
+void select_op(
     const array& a,
     const array& b,
+    const array& c,
     array& out,
-    BinaryOpType bopt,
-    bool invert_predicate) {
-  SelectScalarScalar<T> opss(invert_predicate);
-  SelectScalarVector<T> opsv(invert_predicate);
-  SelectVectorScalar<T> opvs(invert_predicate);
-  SelectVectorVector<T> opvv(invert_predicate);
-  binary_op<bool, T, T>(a, b, out, bopt, opss, opsv, opvs, opvv);
-}
-
-void select_with_predicate(
-    const array& a,
-    const array& b,
-    array& out,
-    BinaryOpType bopt,
-    bool invert_predicate = false) {
+    Op op) {
   switch (out.dtype()) {
     case bool_:
-      select_binary_op<bool>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, bool, bool, bool>(a, b, c, out, op);
       break;
     case uint8:
-      select_binary_op<uint8_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, uint8_t, uint8_t, uint8_t>(a, b, c, out, op);
       break;
     case uint16:
-      select_binary_op<uint16_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, uint16_t, uint16_t, uint16_t>(a, b, c, out, op);
       break;
     case uint32:
-      select_binary_op<uint32_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, uint32_t, uint32_t, uint32_t>(a, b, c, out, op);
       break;
     case uint64:
-      select_binary_op<uint64_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, uint64_t, uint64_t, uint64_t>(a, b, c, out, op);
       break;
     case int8:
-      select_binary_op<int8_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, int8_t, int8_t, int8_t>(a, b, c, out, op);
       break;
     case int16:
-      select_binary_op<int16_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, int16_t, int16_t, int16_t>(a, b, c, out, op);
       break;
     case int32:
-      select_binary_op<int32_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, int32_t, int32_t, int32_t>(a, b, c, out, op);
       break;
     case int64:
-      select_binary_op<int64_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, int64_t, int64_t, int64_t>(a, b, c, out, op);
       break;
     case float16:
-      select_binary_op<float16_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, float16_t, float16_t, float16_t>(a, b, c, out, op);
       break;
     case float32:
-      select_binary_op<float>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, float, float, float>(a, b, c, out, op);
       break;
     case bfloat16:
-      select_binary_op<bfloat16_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, bfloat16_t, bfloat16_t, bfloat16_t>(a, b, c, out, op);
       break;
     case complex64:
-      select_binary_op<complex64_t>(a, b, out, bopt, invert_predicate);
+      ternary_op<bool, complex64_t, complex64_t, complex64_t>(a, b, c, out, op);
       break;
   }
 }
@@ -84,12 +70,7 @@ void Select::eval(const std::vector<array>& inputs, array& out) {
   const auto& condition = inputs[0];
   const auto& a = inputs[1];
   const auto& b = inputs[2];
-
-  BinaryOpType bopt = get_select_binary_op_type(condition, a, b);
-  set_select_binary_op_output_data(condition, a, b, out, bopt);
-
-  select_with_predicate(condition, a, out, bopt);
-  select_with_predicate(condition, b, out, bopt, true /* invert_predicate */);
+  select_op(condition, a, b, out, detail::Select());
 }
 
 } // namespace mlx::core
