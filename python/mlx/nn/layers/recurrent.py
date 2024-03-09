06f93d13238df9aac0ee138c326620d0d527d2f2
Awni Hannun <awni@apple.com>
2024-03-09 07:30:30 -0800

    nits in docs

--- python/mlx/nn/layers/recurrent.py
+++ python/mlx/nn/layers/recurrent.py
@@ -1,46 +1,52 @@
 import math
+from typing import Callable, Optional
 
 import mlx.core as mx
+from mlx.nn.layers.activations import tanh
 from mlx.nn.layers.base import Module
 
 
 class RNN(Module):
-    r"""Apply one Elman recurrent units to a sequence of
-    shape ``NLD`` or ``LD``, where:
-        - ``N`` is the optional batch dimension
-        - ``L`` is the sequence length
-        - ``D`` is the input's feature dimension
+    r"""An Elman recurrent layer.
 
-    Concretely, for each element along the sequence length axis, compute:
+    The input is a sequence of shape ``NLD`` or ``LD`` where:
+
+    * ``N`` is the optional batch dimension
+    * ``L`` is the sequence length
+    * ``D`` is the input's feature dimension
+
+    Concretely, for each element along the sequence length axis, this
+    layer applies the function:
 
     .. math::
 
-        h_{t + 1} = \text{tanh} (W_{ih}x_t + b_{ih} + W_{hh}h_t + b_{hh})
+        h_{t + 1} = \text{tanh} (W_{ih}x_t + W_{hh}h_t + b)
 
-    The hidden state :math:`h` has shape ``NH`` or ``H``, depending
+    The hidden state :math:`h` has shape ``NH`` or ``H``, depending on
     whether the input is batched or not. Returns the hidden state at each
     time step, of shape ``NLH`` or ``LH``.
 
-
     Args:
-        input_size (int): Dimension of the input :math:`x`
-        hidden_size (int): Dimension of the hidden state :math:`h`
-        nonlinearity (callable): Non-linearity to use. Default: `mx.tanh`.
-        bias (bool): Whether to use biases or not. Default: `True`.
+        input_size (int): Dimension of the input, ``D``.
+        hidden_size (int): Dimension of the hidden state, ``H``.
+        bias (bool, optional): Whether to use a bias. Default: ``True``.
+        nonlinearity (callable, optional): Non-linearity to use. If ``None``,
+            then func:`tanh` is used. Default: ``None``.
     """
 
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
-        nonlinearity: callable = mx.tanh,
         bias: bool = True,
+        nonlinearity: Optional[Callable] = None,
     ):
         super().__init__()
 
-        if not callable(nonlinearity):
+        self.nonlinearity = nonlinearity or tanh
+        if not callable(self.nonlinearity):
             raise ValueError(
-                f"Nonlinearity must be callable. Current value: {nonlinearity}"
+                f"Nonlinearity must be callable. Current value: {nonlinearity}."
             )
 
         scale = 1.0 / math.sqrt(hidden_size)
@@ -51,28 +57,31 @@ class RNN(Module):
         self.Whh = mx.random.uniform(
             low=-scale, high=scale, shape=(hidden_size, hidden_size)
         )
-        self.bh = (
+        self.bias = (
             mx.random.uniform(low=-scale, high=scale, shape=(hidden_size,))
             if bias
             else None
         )
-        self.nonlinearity = nonlinearity
 
     def _extra_repr(self):
-        return f"input_dims={self.Wxh.shape[0]}, hidden_size={self.hidden_size}, nonlinearity={self.nonlinearity}, bias={self.bh is not None}"
+        return (
+            f"input_dims={self.Wxh.shape[0]}, "
+            f"hidden_size={self.hidden_size}, "
+            f"nonlinearity={self.nonlinearity}, bias={self.bias is not None}"
+        )
 
     def __call__(self, x, hidden=None):
-        x_proj = x @ self.Wxh
+        if self.bias is not None:
+            x = mx.addmm(self.bias, x, self.Wxh)
+        else:
+            x = x @ self.Wxh
 
-        if self.bh is not None:
-            x_proj += x_proj
-
-        if hidden is None:
-            hidden = mx.zeros(shape=(self.hidden_size,))
         all_hidden = []
-
         for idx in range(x.shape[-2]):
-            hidden = x_proj[..., idx, :] + hidden @ self.Whh
+            if hidden is not None:
+                hidden = x[..., idx, :] + hidden @ self.Whh
+            else:
+                hidden = x[..., idx, :]
             hidden = self.nonlinearity(hidden)
             all_hidden.append(hidden)
 
@@ -80,29 +89,33 @@ class RNN(Module):
 
 
 class GRU(Module):
-    r"""Apply one GRU recurrent unit to a sequence of shape
-    ``NLD`` or ``LD``, where:
-        - ``N`` is the optional batch dimension
-        - ``L`` is the sequence length
-        - ``D`` is the input's feature dimension
+    r"""A gated recurrent unit (GRU) RNN layer.
 
-    Concretely, for each element of the sequence, computes:
+    The input has shape ``NLD`` or ``LD`` where:
+
+    * ``N`` is the optional batch dimension
+    * ``L`` is the sequence length
+    * ``D`` is the input's feature dimension
+
+    Concretely, for each element of the sequence, this layer computes:
 
     .. math::
 
-        r_t = \sigma (W_{xr}x_t + b_{xr} + W_{hr}h_t + b_{hr})
-        z_t = \sigma (W_{xz}x_t + b_{xz} + W_{hz}h_t + b_{hz})
-        n_t = \text{tanh}(W_{xn}x_t + b_{xn} + r_t \odot (W_{hn}h_t + b_{hn}))
-        h_{t + 1} = (1 - z_t) \odot n_t + z_t \odot h_t
+        \begin{align*}
+        r_t &= \sigma (W_{xr}x_t + W_{hr}h_t + b_{r}) \\
+        z_t &= \sigma (W_{xz}x_t + W_{hz}h_t + b_{z}) \\
+        n_t &= \text{tanh}(W_{xn}x_t + b_{n} + r_t \odot (W_{hn}h_t + b_{hn})) \\
+        h_{t + 1} &= (1 - z_t) \odot n_t + z_t \odot h_t
+        \end{align*}
 
-    The hidden state :math:`h` has shape ``NH`` or ``H``, depending
+    The hidden state :math:`h` has shape ``NH`` or ``H`` depending on
     whether the input is batched or not. Returns the hidden state at each
-    time step, of shape ``NLH`` or ``LH``.
+    time step of shape ``NLH`` or ``LH``.
 
     Args:
-        input_size (int): Dimension of the input :math:`x`
-        hidden_size (int): Dimension of the hidden state :math:`h`
-        bias (bool): Whether to use biases or not. Default: `True`.
+        input_size (int): Dimension of the input, ``D``.
+        hidden_size (int): Dimension of the hidden state, ``H``.
+        bias (bool): Whether to use biases or not. Default: ``True``.
     """
 
     def __init__(
@@ -133,16 +146,19 @@ class GRU(Module):
         )
 
     def _extra_repr(self):
-        return f"input_dims={self.Wx.shape[0]}, hidden_size={self.hidden_size}, bias={self.b is not None}"
+        return (
+            f"input_dims={self.Wx.shape[0]}, "
+            f"hidden_size={self.hidden_size}, bias={self.b is not None}"
+        )
 
     def __call__(self, x, hidden=None):
-        x_proj = x @ self.Wx
-
         if self.b is not None:
-            x_proj += self.b
+            x = mx.addmm(self.b, x, self.Wx)
+        else:
+            x = x @ self.Wx
 
-        x_proj_rz = x_proj[..., : -self.hidden_size]
-        x_proj_n = x_proj[..., -self.hidden_size :]
+        x_rz = x[..., : -self.hidden_size]
+        x_n = x[..., -self.hidden_size :]
 
         all_hidden = []
 
@@ -158,12 +174,12 @@ class GRU(Module):
                 h_proj_n += self.bhn
 
             # Note bias in r, z, n is added through x already
-            rz = x_proj_rz[..., idx, :] + h_proj_rz
+            rz = x_rz[..., idx, :] + h_proj_rz
             rz = mx.sigmoid(rz)
 
             r, z = mx.split(rz, 2, axis=-1)
 
-            n_x = x_proj_n[..., idx, :]
+            n_x = x_n[..., idx, :]
 
             n = n_x + r * h_proj_n
             n = mx.tanh(n)
@@ -175,31 +191,36 @@ class GRU(Module):
 
 
 class LSTM(Module):
-    r"""Apply one LSTM recurrent unit to a sequence of
-    shape ``NLD`` or ``LD``, where:
-        - ``N`` is the optional batch dimension
-        - ``L`` is the sequence length
-        - ``D`` is the input's feature dimension
+    r"""An LSTM recurrent layer.
 
-    Concretely, for each element of the sequence, computes:
+    The input has shape ``NLD`` or ``LD`` where:
+
+    * ``N`` is the optional batch dimension
+    * ``L`` is the sequence length
+    * ``D`` is the input's feature dimension
+
+    Concretely, for each element of the sequence, this layer computes:
 
     .. math::
-        i_t = \sigma (W_{xi}x_t + b_{xi} + W_{hi}h_t + b_{hi})
-        f_t = \sigma (W_{xf}x_t + b_{xf} + W_{hf}h_t + b_{hf})
-        g_t = \text{tanh} (W_{xg}x_t + b_{xg} + W_{hg}h_t + b_{hg})
-        o_t = \sigma (W_{xo}x_t + b_{xo} + W_{ho}h_t + b_{ho})
-        c_{t + 1} = f_t \odot c_t + i_t \odot g_t
-        h_{t + 1} = o_t \text{tanh}(c_{t + 1})
+        \begin{align*}
+        i_t &= \sigma (W_{xi}x_t + W_{hi}h_t + b_{i}) \\
+        f_t &= \sigma (W_{xf}x_t + W_{hf}h_t + b_{f}) \\
+        g_t &= \text{tanh} (W_{xg}x_t + W_{hg}h_t + b_{g}) \\
+        o_t &= \sigma (W_{xo}x_t + W_{ho}h_t + b_{o}) \\
+        c_{t + 1} &= f_t \odot c_t + i_t \odot g_t \\
+        h_{t + 1} &= o_t \text{tanh}(c_{t + 1})
+        \end{align*}
 
     The hidden state :math:`h` and cell state :math:`c` have shape ``NH``
-    or ``H``, depending whether the input is batched or not. Returns two
-    arrays: the hidden state and the cell state at each time step, both
-    of shape ``NLH`` or ``LH``.
+    or ``H``, depending on whether the input is batched or not.
+
+    The layer returns two arrays, the hidden state and the cell state at
+    each time step, both of shape ``NLH`` or ``LH``.
 
     Args:
-        input_size (int): Dimension of the input :math:`x`
-        hidden_size (int): Dimension of the hidden state :math:`h`
-        bias (bool): Whether to use biases or not. Default: `True`.
+        input_size (int): Dimension of the input, ``D``.
+        hidden_size (int): Dimension of the hidden state, ``H``.
+        bias (bool): Whether to use biases or not. Default: ``True``.
     """
 
     def __init__(
@@ -218,20 +239,23 @@ class LSTM(Module):
         self.Wh = mx.random.uniform(
             low=-scale, high=scale, shape=(hidden_size, 4 * hidden_size)
         )
-        self.b = (
+        self.bias = (
             mx.random.uniform(low=-scale, high=scale, shape=(4 * hidden_size,))
             if bias
             else None
         )
 
     def _extra_repr(self):
-        return f"input_dims={self.Wx.shape[0]}, hidden_size={self.hidden_size}, bias={self.b is not None}"
+        return (
+            f"input_dims={self.Wx.shape[0]}, "
+            f"hidden_size={self.hidden_size}, bias={self.bias is not None}"
+        )
 
     def __call__(self, x, hidden=None, cell=None):
-        x_proj = x @ self.Wx
-
-        if self.b is not None:
-            x_proj += self.b
+        if self.bias is not None:
+            x = mx.addmm(self.bias, x, self.Wx)
+        else:
+            x = x @ self.Wx
 
         if hidden is None:
             hidden = mx.zeros(shape=(self.hidden_size,))
@@ -245,7 +269,7 @@ class LSTM(Module):
         for idx in range(x.shape[-2]):
             h_proj = hidden @ self.Wh
 
-            ifgo = x_proj[..., idx, :] + h_proj
+            ifgo = x[..., idx, :] + h_proj
             i, f, g, o = mx.split(ifgo, 4, axis=-1)
 
             i = mx.sigmoid(i)
