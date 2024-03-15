a70e113579525f149e27c78555277a9476593211
Jagrit Digani <digani@apple.com>
2024-03-15 12:34:47 -0700

    Remove manual unrolling of elem to loc loop

--- mlx/backend/metal/kernels/utils.h
+++ mlx/backend/metal/kernels/utils.h
@@ -137,52 +137,15 @@ template <int NDIM>
 METAL_FUNC size_t elem_to_loc_nd(
     uint elem,
     device const int* shape,
-    device const size_t* strides);
-
-template <>
-METAL_FUNC size_t elem_to_loc_nd<1>(
-    uint elem,
-    device const int* shape,
     device const size_t* strides) {
-  return (elem % shape[0]) * strides[0];
-}
+  size_t loc = (elem % shape[NDIM - 1]) * strides[NDIM - 1];
 
-template <>
-METAL_FUNC size_t elem_to_loc_nd<2>(
-    uint elem,
-    device const int* shape,
-    device const size_t* strides) {
-  size_t loc = (elem % shape[1]) * strides[1];
-  elem /= shape[1];
-  loc += (elem % shape[0]) * strides[0];
-  return loc;
-}
+  MLX_MTL_PRAGMA_UNROLL
+  for (int d = NDIM - 2; d >= 0; --d) {
+    elem /= shape[d + 1];
+    loc += (elem % shape[d]) * strides[d];
+  }
 
-template <>
-METAL_FUNC size_t elem_to_loc_nd<3>(
-    uint elem,
-    device const int* shape,
-    device const size_t* strides) {
-  size_t loc = (elem % shape[2]) * strides[2];
-  elem /= shape[2];
-  loc += (elem % shape[1]) * strides[1];
-  elem /= shape[1];
-  loc += (elem % shape[0]) * strides[0];
-  return loc;
-}
-
-template <>
-METAL_FUNC size_t elem_to_loc_nd<4>(
-    uint elem,
-    device const int* shape,
-    device const size_t* strides) {
-  size_t loc = (elem % shape[3]) * strides[3];
-  elem /= shape[3];
-  loc += (elem % shape[2]) * strides[2];
-  elem /= shape[2];
-  loc += (elem % shape[1]) * strides[1];
-  elem /= shape[1];
-  loc += (elem % shape[0]) * strides[0];
   return loc;
 }
 
@@ -204,13 +167,14 @@ METAL_FUNC int64_t elem_to_loc_nd(
     uint elem,
     constant const int shape[NDIM],
     constant const int64_t strides[NDIM]) {
-  int64_t loc{0};
+  int64_t loc = (elem % shape[NDIM - 1]) * strides[NDIM - 1];
 
   MLX_MTL_PRAGMA_UNROLL
-  for (int d = NDIM - 1; d >= 0; --d) {
+  for (int d = NDIM - 2; d >= 0; --d) {
+    elem /= shape[d + 1];
     loc += (elem % shape[d]) * strides[d];
-    elem /= shape[d];
   }
+
   return loc;
 }
 
