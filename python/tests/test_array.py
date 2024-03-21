b49fc10fcd52588bd3d6f73087bb9116c42f4930
AmirHossein-nr <arazlighi@gmail.com>
2024-03-21 21:39:40 +0330

    add tests for array equality

--- python/tests/test_array.py
+++ python/tests/test_array.py
@@ -96,7 +96,6 @@ class TestEquality(mlx_tests.MLXTestCase):
         c = mx.array([1, 2, 4])
         self.assertTrue(mx.all(a == b))
         self.assertFalse(mx.all(a == c))
-
     def test_array_eq_scalar(self):
         a = mx.array([1, 2, 3])
         b = 1
