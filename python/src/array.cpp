66c20c7a90a6c9189c57b0cc89d386b22a1b5aa6
AmirHossein-nr <arazlighi@gmail.com>
2024-03-24 14:33:13 +0330

    debug minor issue regarding detecting mlx array

--- python/src/array.cpp
+++ python/src/array.cpp
@@ -80,16 +80,6 @@ auto to_scalar(array& a) {
   }
 }
 
-bool isMlxArray(const ScalarOrArray& v) {
-    auto check_for_obj = std::get_if<nb::object>(&v);
-    if (check_for_obj) {
-        if(isMlxCoreArray(*check_for_obj)) {
-            return true;
-        }
-    }
-    return false;
-}
-
 nb::object tolist(array& a) {
   if (a.ndim() == 0) {
     return to_scalar(a);
@@ -921,13 +911,19 @@ void init_array(nb::module_& m) {
           [](const array& a, const ScalarOrArray v) -> array {
             if (!is_comparable_with_array(v)) {
               throw_invalid_operation("greater than", v);
+            }
             return greater(a, to_array(v, a.dtype()));
+          },
+          "other"_a)
       .def(
           "__ge__",
           [](const array& a, const ScalarOrArray v) -> array {
             if (!is_comparable_with_array(v)) {
               throw_invalid_operation("greater than or equal", v);
+            }
             return greater_equal(a, to_array(v, a.dtype()));
+          },
+          "other"_a)
       .def(
           "__ne__",
           [](const array& a,
