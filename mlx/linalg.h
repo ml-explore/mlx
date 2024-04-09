a078cb8db4fa1ec406125f9bd017bc5364686d14
Awni Hannun <awni@apple.com>
2024-04-09 11:10:52 -0700

    use string

--- mlx/linalg.h
+++ mlx/linalg.h
@@ -38,13 +38,13 @@ inline array norm(
 }
 array norm(
     const array& a,
-    std::string_view ord,
+    const std::string& ord,
     const std::optional<std::vector<int>>& axis = std::nullopt,
     bool keepdims = false,
     StreamOrDevice s = {});
 inline array norm(
     const array& a,
-    std::string_view ord,
+    const std::string& ord,
     int axis,
     bool keepdims = false,
     StreamOrDevice s = {}) {
