835a41a1da014f281659293b6c5e89e006ed9f54
Awni Hannun <awni@apple.com>
2024-04-10 17:36:40 -0700

    rebase

--- mlx/backend/metal/device.h
+++ mlx/backend/metal/device.h
@@ -83,7 +83,7 @@ struct CommandEncoder {
     enc->setBuffer(a_buf, base_offset, idx);
 
     // Rough estimate of the amount of memory used
-    enc.cb_info.bytes += a.nbytes();
+    cb_info.bytes += a.nbytes();
   }
 
   void set_output_array(array& a, int idx, int offset = 0) {
