e7164170dddfb0ba8c2f86793b365f189a8e40cc
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-05-10 21:28:42 -0700

    Make MPI optional and add a no distributed implementation

---
+++ mlx/distributed/no_distributed.cpp
@@ -0,0 +1,49 @@
+// Copyright © 2024 Apple Inc.
+
+#include "mlx/distributed/distributed.h"
+
+namespace mlx::core::distributed {
+
+namespace {
+
+struct DummyGroup : public Group {
+  virtual int rank() override {
+    return 0;
+  }
+  virtual int size() override {
+    return 1;
+  }
+  virtual std::shared_ptr<Group> split(int n) override {
+    throw std::runtime_error("Cannot split the distributed group further");
+  }
+};
+
+} // namespace
+
+namespace detail {
+
+Stream communication_stream() {
+  static Stream comm_stream = new_stream(Device::cpu);
+  return comm_stream;
+}
+
+} // namespace detail
+
+bool is_available() {
+  return false;
+}
+
+std::shared_ptr<Group> init() {
+  return std::make_shared<DummyGroup>();
+}
+
+void all_reduce_sum(
+    std::shared_ptr<Group> group,
+    const array& input,
+    array& output) {}
+void all_gather(
+    std::shared_ptr<Group> group,
+    const array& input,
+    array& output) {}
+
+} // namespace mlx::core::distributed
