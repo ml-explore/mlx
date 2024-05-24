2f44273687ad4469460fa4392928075a16a7e692
Awni Hannun <awni@apple.com>
2024-05-23 21:11:24 -0700

    docs

--- docs/build/html/search/classes_4.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['data_0',['Data',['../structmlx_1_1core_1_1array_1_1_data.html',1,'mlx::core::array']]],
-  ['depends_1',['Depends',['../classmlx_1_1core_1_1_depends.html',1,'mlx::core']]],
-  ['device_2',['Device',['../structmlx_1_1core_1_1_device.html',1,'mlx::core::Device'],['../classmlx_1_1core_1_1metal_1_1_device.html',1,'mlx::core::metal::Device']]],
-  ['divide_3',['Divide',['../struct_divide.html',1,'Divide'],['../structmlx_1_1core_1_1detail_1_1_divide.html',1,'mlx::core::detail::Divide'],['../classmlx_1_1core_1_1_divide.html',1,'mlx::core::Divide']]],
-  ['divmod_4',['DivMod',['../classmlx_1_1core_1_1_div_mod.html',1,'mlx::core']]],
-  ['dtype_5',['Dtype',['../structmlx_1_1core_1_1_dtype.html',1,'mlx::core']]]
-];
--- docs/build/html/search/enumvalues_e.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['scalar_0',['Scalar',['../namespacemlx_1_1core.html#abd84ff6c5245e4e170b2ef5247594337af60357a8d17e45793298323f1b372a74',1,'mlx::core']]],
-  ['scheduled_1',['scheduled',['../classmlx_1_1core_1_1array.html#a199726612fa8a4bcd5c2d05eadad7078af8a6f8eed2395ab89a758dec434393ae',1,'mlx::core::array']]],
-  ['signedinteger_2',['signedinteger',['../structmlx_1_1core_1_1_dtype.html#ac091c39cbd6686ef69aa1e5a2425aa2daed58b4631ff157bec9e35ed1182d2c10',1,'mlx::core::Dtype']]],
-  ['sum_3',['Sum',['../classmlx_1_1core_1_1_reduce.html#a0848518b16ae6d4043d6be247bdf31c9a8582875544f1d3d396a1a376473ef1dd',1,'mlx::core::Reduce::Sum'],['../classmlx_1_1core_1_1_scan.html#a47bf2ec54ead4b8f00f9f188518630f1ade23893033e4849f5596e7ce76a5fc36',1,'mlx::core::Scan::Sum'],['../classmlx_1_1core_1_1_scatter.html#a614d19af11dc30644b2b4941033b613ca14abe2d8818efa71726be4e156813d6f',1,'mlx::core::Scatter::Sum']]]
-];
