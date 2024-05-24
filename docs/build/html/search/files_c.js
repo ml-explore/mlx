405a155f42d8ace5145e466ba5b3eca70eefb93b
Awni Hannun <awni@apple.com>
2024-05-23 21:11:24 -0700

    docs

--- docs/build/html/search/files_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['params_2eh_0',['params.h',['../conv_2params_8h.html',1,'(Global Namespace)'],['../gemm_2params_8h.html',1,'(Global Namespace)']]],
-  ['pocketfft_2eh_1',['pocketfft.h',['../pocketfft_8h.html',1,'']]],
-  ['primitives_2eh_2',['primitives.h',['../primitives_8h.html',1,'']]]
-];
