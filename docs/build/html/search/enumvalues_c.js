04e1a874a4a07a93d136b494e57e5e4a720f7e50
Awni Hannun <awni@apple.com>
2024-05-23 21:11:24 -0700

    docs

--- docs/build/html/search/enumvalues_c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['prod_0',['Prod',['../classmlx_1_1core_1_1_reduce.html#a0848518b16ae6d4043d6be247bdf31c9ac5b077bfec55fe2b141b197dfa00ecf7',1,'mlx::core::Reduce::Prod'],['../classmlx_1_1core_1_1_scan.html#a47bf2ec54ead4b8f00f9f188518630f1a33edce755ed1a74632c302ad93a14789',1,'mlx::core::Scan::Prod'],['../classmlx_1_1core_1_1_scatter.html#a614d19af11dc30644b2b4941033b613ca5e43e8ffd1f5ba49826e2e7ac3450466',1,'mlx::core::Scatter::Prod']]]
-];
--- docs/build/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['array_20operations_0',['Core array operations',['../group__ops.html',1,'']]]
-];
