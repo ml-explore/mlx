850e5163214d1ebf1d6fcf23b6aad80217a530df
Awni Hannun <awni@apple.com>
2024-05-23 21:11:24 -0700

    docs

--- docs/build/html/search/enumvalues_3.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['datatypealternateencodingbit_0',['DataTypeAlternateEncodingBit',['../namespace_m_p_s.html#a89deea53e8654b369b5cde55bdcd1288a9681e9100f89bbc0484c4195ff1cef03',1,'MPS']]],
-  ['datatypebfloat16_1',['DataTypeBFloat16',['../namespace_m_p_s.html#a89deea53e8654b369b5cde55bdcd1288a39c004722f824f460d83cca680b242ca',1,'MPS']]],
-  ['datatypefloat16_2',['DataTypeFloat16',['../namespace_m_p_s.html#a89deea53e8654b369b5cde55bdcd1288a4fb70692e99c4ff2dc6028275e5a3868',1,'MPS']]],
-  ['datatypefloat32_3',['DataTypeFloat32',['../namespace_m_p_s.html#a89deea53e8654b369b5cde55bdcd1288a2156c04e979b8d8bd39d4ae17d53ded8',1,'MPS']]],
-  ['datatypefloatbit_4',['DataTypeFloatBit',['../namespace_m_p_s.html#a89deea53e8654b369b5cde55bdcd1288a330c9014eb076d89dae3d69f3b49c04a',1,'MPS']]],
-  ['disabled_5',['disabled',['../namespacemlx_1_1core.html#adb15ff2b1ca5207fd4f6e631e2c3bcb4a075ae3d2fc31640504f814f60e5ef713',1,'mlx::core']]]
-];
--- docs/build/html/search/files_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['bf16_2eh_0',['bf16.h',['../backend_2metal_2kernels_2bf16_8h.html',1,'(Global Namespace)'],['../types_2bf16_8h.html',1,'(Global Namespace)']]],
-  ['bf16_5fmath_2eh_1',['bf16_math.h',['../bf16__math_8h.html',1,'']]],
-  ['binary_2eh_2',['binary.h',['../common_2binary_8h.html',1,'(Global Namespace)'],['../metal_2kernels_2binary_8h.html',1,'(Global Namespace)']]],
-  ['binary_5ftwo_2eh_3',['binary_two.h',['../binary__two_8h.html',1,'']]]
-];
