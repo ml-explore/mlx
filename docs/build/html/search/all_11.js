6e4877a86ac6c4f554bc0e0541c596a226fc4102
Awni Hannun <awni@apple.com>
2024-06-06 20:28:06 -0700

    docs update

---
+++ docs/build/html/search/all_11.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['q_0',['q',['../structmlx_1_1core_1_1scheduler_1_1_stream_thread.html#adf608e22d0c0397217472408aab52631',1,'mlx::core::scheduler::StreamThread']]],
+  ['qr_1',['qr',['../namespacemlx_1_1core_1_1linalg.html#ae6d97829459353fe3b31c8a0867c0ca2',1,'mlx::core::linalg']]],
+  ['qrf_2',['QRF',['../classmlx_1_1core_1_1_q_r_f.html',1,'mlx::core::QRF'],['../classmlx_1_1core_1_1_q_r_f.html#a44ed2924dc574c4aeb79b1188b5c3983',1,'mlx::core::QRF::QRF()']]],
+  ['quantize_3',['quantize',['../group__ops.html#gab43cc28690da7cdd43b43065adbd31da',1,'mlx::core']]],
+  ['quantized_5fmatmul_4',['quantized_matmul',['../group__ops.html#ga6381c7062b232ac21f3fdc3136aaf86e',1,'mlx::core']]],
+  ['quantizedmatmul_5',['QuantizedMatmul',['../classmlx_1_1core_1_1_quantized_matmul.html',1,'mlx::core::QuantizedMatmul'],['../classmlx_1_1core_1_1_quantized_matmul.html#a5bd164d038d9dc21919f7e0bfdeaa25c',1,'mlx::core::QuantizedMatmul::QuantizedMatmul()']]],
+  ['query_5fsequence_5flength_6',['QUERY_SEQUENCE_LENGTH',['../struct_m_l_x_scaled_dot_product_attention_params.html#a46cc2da6a069d822f36983ee18467e5c',1,'MLXScaledDotProductAttentionParams']]],
+  ['quiet_5fnan_7',['quiet_NaN',['../structmetal_1_1__numeric__limits__impl_3_01bfloat16__t_01_4.html#aebeb07c01984be246bc2d1b8f8e4ac7b',1,'metal::_numeric_limits_impl&lt; bfloat16_t &gt;']]]
+];
