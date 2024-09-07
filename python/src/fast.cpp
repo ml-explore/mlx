21c2c2ca2b7341e2b722b2830dbf7462d2608028
Awni Hannun <awni@apple.com>
2024-09-06 17:52:08 -0700

    update examples

--- python/src/fast.cpp
+++ python/src/fast.cpp
@@ -333,18 +333,20 @@ void init_fast(nb::module_& parent_module) {
 
               kernel = mx.fast.metal_kernel(
                   name="myexp",
+                  input_names=["inp"],
+                  output_names=["out"],
                   source=source
               )
               outputs = kernel(
-                  inputs={"inp": a},
-                  template={"T": mx.float32},
+                  inputs=[a],
+                  template=[("T", mx.float32)],
                   grid=(a.size, 1, 1),
                   threadgroup=(256, 1, 1),
-                  output_shapes={"out": a.shape},
-                  output_dtypes={"out": a.dtype},
+                  output_shapes=[a.shape],
+                  output_dtypes=[a.dtype],
                   verbose=True,
               )
-              return outputs["out"]
+              return outputs[0]
 
           a = mx.random.normal(shape=(4, 16)).astype(mx.float16)
           b = exp_elementwise(a)
