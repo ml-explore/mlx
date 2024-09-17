bf752c1a87ac916a70e956b110bcae0670426fdf
Awni Hannun <awni@apple.com>
2024-09-17 12:06:14 -0700

    docs up

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.17.0',
+    VERSION: '0.17.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/build/html/search/files_f.js
+++ docs/build/html/search/files_f.js
@@ -3,7 +3,7 @@ var searchData=
   ['radix_2eh_0',['radix.h',['../radix_8h.html',1,'']]],
   ['random_2eh_1',['random.h',['../random_8h.html',1,'']]],
   ['readwrite_2eh_2',['readwrite.h',['../readwrite_8h.html',1,'']]],
-  ['reduce_2eh_3',['reduce.h',['../common_2reduce_8h.html',1,'(Global Namespace)'],['../metal_2jit_2reduce_8h.html',1,'(Global Namespace)'],['../metal_2kernels_2reduce_8h.html',1,'(Global Namespace)'],['../metal_2reduce_8h.html',1,'(Global Namespace)']]],
+  ['reduce_2eh_3',['reduce.h',['../common_2reduce_8h.html',1,'(Global Namespace)'],['../metal_2kernels_2reduce_8h.html',1,'(Global Namespace)'],['../metal_2reduce_8h.html',1,'(Global Namespace)']]],
   ['reduce_5fall_2eh_4',['reduce_all.h',['../reduce__all_8h.html',1,'']]],
   ['reduce_5fcol_2eh_5',['reduce_col.h',['../reduce__col_8h.html',1,'']]],
   ['reduce_5finit_2eh_6',['reduce_init.h',['../reduce__init_8h.html',1,'']]],
