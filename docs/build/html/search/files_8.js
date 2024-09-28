b7d2d1e08c89d7cfd07c31f00a15ad27ab8d1829
Awni Hannun <awni@apple.com>
2024-09-28 11:04:59 -0700

    docs update

--- docs/build/html/search/files_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['includes_2eh_0',['includes.h',['../includes_8h.html',1,'']]],
-  ['indexing_2eh_1',['indexing.h',['../jit_2indexing_8h.html',1,'(Global Namespace)'],['../kernels_2indexing_8h.html',1,'(Global Namespace)']]],
-  ['io_2eh_2',['io.h',['../io_8h.html',1,'']]]
-];
--- docs/build/html/search/typedefs_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['enable_5ffor_5farrays_5ft_0',['enable_for_arrays_t',['../namespacemlx_1_1core.html#af89751d79339f3e4d9318ea97d64d114',1,'mlx::core']]]
-];
