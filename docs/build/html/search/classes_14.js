a8db79c9047cf064ab0fdc4eb97044f3c9495ae0
Awni Hannun <awni@apple.com>
2024-09-28 11:04:59 -0700

    docs update

--- docs/build/html/search/classes_14.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['unaryprimitive_0',['UnaryPrimitive',['../classmlx_1_1core_1_1_unary_primitive.html',1,'mlx::core']]],
-  ['uniform_1',['Uniform',['../classmlx_1_1core_1_1_uniform.html',1,'mlx::core']]],
-  ['util_2',['util',['../structpocketfft_1_1detail_1_1util.html',1,'pocketfft::detail']]]
-];
