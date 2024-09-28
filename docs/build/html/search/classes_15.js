151da89a192182986817899d1a177f9455a2678a
Awni Hannun <awni@apple.com>
2024-09-28 11:04:59 -0700

    docs update

--- docs/build/html/search/classes_15.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['view_0',['View',['../classmlx_1_1core_1_1_view.html',1,'mlx::core']]],
-  ['vlen_1',['VLEN',['../structpocketfft_1_1detail_1_1_v_l_e_n.html',1,'pocketfft::detail']]],
-  ['vtype_2',['VTYPE',['../structpocketfft_1_1detail_1_1_v_t_y_p_e.html',1,'pocketfft::detail']]]
-];
