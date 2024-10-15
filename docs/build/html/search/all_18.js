453a1ac3936b2eb0165ebc818afe05163797c126
CircleCI Docs <mlx@group.apple.com>
2024-10-15 15:12:17 +0000

    rebase

---
+++ docs/build/html/search/all_18.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['x_0',['x',['../struct___no_mask.html#a0c4a4557d5c97ceafe3a2c4e521cdf7e',1,'_NoMask']]],
+  ['xor_1',['Xor',['../classmlx_1_1core_1_1_bitwise_binary.html#a6f8b5d455d0c1770428a6bef1608f23dac95e7d8e6205449a70c8134e7dae3bd1',1,'mlx::core::BitwiseBinary']]]
+];
---
+++ docs/build/html/search/enumvalues_6.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['general_0',['General',['../namespacemlx_1_1core.html#abd84ff6c5245e4e170b2ef5247594337a0db377921f4ce762c62526131097968f',1,'mlx::core']]],
+  ['generalcontiguousreduce_1',['GeneralContiguousReduce',['../namespacemlx_1_1core.html#a12412984a1cabfe1189942c898f8fe65a540cf31fe6858115a02e789938297cdb',1,'mlx::core']]],
+  ['generalgeneral_2',['GeneralGeneral',['../namespacemlx_1_1core.html#abd84ff6c5245e4e170b2ef5247594337a6fe62e8ce1fae1e70cb9eeaa67d29dab',1,'mlx::core']]],
+  ['generalreduce_3',['GeneralReduce',['../namespacemlx_1_1core.html#a12412984a1cabfe1189942c898f8fe65a3622f95ed0ec99657f9ad8ef39ec2184',1,'mlx::core']]],
+  ['generalstridedreduce_4',['GeneralStridedReduce',['../namespacemlx_1_1core.html#a12412984a1cabfe1189942c898f8fe65ad999b1a8ae1d7436efb5ffdfafb1dd3d',1,'mlx::core']]],
+  ['generic_5',['generic',['../structmlx_1_1core_1_1_dtype.html#ac091c39cbd6686ef69aa1e5a2425aa2da3d517f8924ac7fd03699a29d97dc52d9',1,'mlx::core::Dtype']]],
+  ['gpu_6',['gpu',['../structmlx_1_1core_1_1_device.html#ac45b3de9b3458d8f31005136cde20fdba0aa0be2a866411d9ff03515227454947',1,'mlx::core::Device']]]
+];
