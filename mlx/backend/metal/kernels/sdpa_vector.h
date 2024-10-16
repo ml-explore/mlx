1d84541fd5b142c9880d3f5364092fb65da2249b
Angelos Katharopoulos <a_katharopoulos@apple.com>
2024-10-16 09:45:10 -0700

    Add a barrier to account for arbitrary lengths

--- mlx/backend/metal/kernels/sdpa_vector.h
+++ mlx/backend/metal/kernels/sdpa_vector.h
@@ -79,6 +79,7 @@ template <typename T, int D>
     keys += stride;
     values += stride;
   }
+  threadgroup_barrier(mem_flags::mem_threadgroup);
 
   // Each thread has a partial part of the output so we need to combine them.
 
