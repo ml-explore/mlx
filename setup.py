9df7995deccbe43769f854a9062fff20617248eb
Awni Hannun <awni@apple.com>
2024-10-30 16:46:24 -0700

    patch

--- setup.py
+++ setup.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
 
     setup(
         name="mlx",
-        version=get_version("0.19.1"),
+        version=get_version("0.19.2"),
         author="MLX Contributors",
         author_email="mlx@group.apple.com",
         description="A framework for machine learning on Apple silicon.",
