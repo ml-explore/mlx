f237d8df87fd934a57b91d78b0bbb738a33a8a03
CircleCI Docs <mlx@group.apple.com>
2024-10-31 03:00:19 +0000

    rebase

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.19.1',
+    VERSION: '0.19.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/build/html/search/classes_f.js
+++ docs/build/html/search/classes_f.js
@@ -5,8 +5,13 @@ var searchData=
   ['partition_2',['Partition',['../classmlx_1_1core_1_1_partition.html',1,'mlx::core']]],
   ['pocketfft_5fc_3',['pocketfft_c',['../classpocketfft_1_1detail_1_1pocketfft__c.html',1,'pocketfft::detail']]],
   ['pocketfft_5fr_4',['pocketfft_r',['../classpocketfft_1_1detail_1_1pocketfft__r.html',1,'pocketfft::detail']]],
-  ['power_5',['Power',['../structmlx_1_1core_1_1detail_1_1_power.html',1,'mlx::core::detail::Power'],['../classmlx_1_1core_1_1_power.html',1,'mlx::core::Power'],['../struct_power.html',1,'Power']]],
-  ['primitive_6',['Primitive',['../classmlx_1_1core_1_1_primitive.html',1,'mlx::core']]],
-  ['printformatter_7',['PrintFormatter',['../structmlx_1_1core_1_1_print_formatter.html',1,'mlx::core']]],
-  ['prod_8',['Prod',['../struct_prod.html',1,'']]]
+  ['pointer_5felement_5',['pointer_element',['../structmetal_1_1pointer__element.html',1,'metal']]],
+  ['pointer_5felement_3c_20constant_20t_20_2a_20_3e_6',['pointer_element&lt; constant T * &gt;',['../structmetal_1_1pointer__element_3_01constant_01_t_01_5_01_4.html',1,'metal']]],
+  ['pointer_5felement_3c_20device_20t_20_2a_20_3e_7',['pointer_element&lt; device T * &gt;',['../structmetal_1_1pointer__element_3_01device_01_t_01_5_01_4.html',1,'metal']]],
+  ['pointer_5felement_3c_20thread_20t_20_2a_20_3e_8',['pointer_element&lt; thread T * &gt;',['../structmetal_1_1pointer__element_3_01thread_01_t_01_5_01_4.html',1,'metal']]],
+  ['pointer_5felement_3c_20threadgroup_20t_20_2a_20_3e_9',['pointer_element&lt; threadgroup T * &gt;',['../structmetal_1_1pointer__element_3_01threadgroup_01_t_01_5_01_4.html',1,'metal']]],
+  ['power_10',['Power',['../structmlx_1_1core_1_1detail_1_1_power.html',1,'mlx::core::detail::Power'],['../classmlx_1_1core_1_1_power.html',1,'mlx::core::Power'],['../struct_power.html',1,'Power']]],
+  ['primitive_11',['Primitive',['../classmlx_1_1core_1_1_primitive.html',1,'mlx::core']]],
+  ['printformatter_12',['PrintFormatter',['../structmlx_1_1core_1_1_print_formatter.html',1,'mlx::core']]],
+  ['prod_13',['Prod',['../struct_prod.html',1,'']]]
 ];
