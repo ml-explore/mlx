379b7b40278dc8814e450c02e145c1732484ebdf
CircleCI Docs <mlx@group.apple.com>
2024-11-22 20:24:16 +0000

    rebase

--- docs/build/html/search/typedefs_e.js
+++ docs/build/html/search/typedefs_e.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['val_5ft_0',['val_t',['../struct_kernel_merge_sort.html#a4e3f09896275956fc4c23e1f157dca3b',1,'KernelMergeSort']]],
-  ['value_5ftype_1',['value_type',['../structpocketfft_1_1detail_1_1threading_1_1aligned__allocator.html#ab86a2740ed9ce3199135372ff1d88c76',1,'pocketfft::detail::threading::aligned_allocator::value_type'],['../structmlx_1_1core_1_1array_1_1_array_iterator.html#ae24fe304397e961687d0d4c7012b8ae4',1,'mlx::core::array::ArrayIterator::value_type'],['../structmlx_1_1steel_1_1integral__constant.html#a0569cc1334e0bc4f474304b33d365759',1,'mlx::steel::integral_constant::value_type']]],
-  ['valueandgradfn_2',['ValueAndGradFn',['../namespacemlx_1_1core.html#ab79d66ddf1ec38b2f2c01234892a2230',1,'mlx::core']]],
-  ['void_5ft_3',['void_t',['../namespacemetal.html#a192322c772aa8b168d59edc55fb806f1',1,'metal']]],
-  ['vtype_5ft_4',['vtype_t',['../namespacepocketfft_1_1detail.html#a3edfb93aeed2f8258183d463ea291d62',1,'pocketfft::detail']]]
+  ['templatearg_0',['TemplateArg',['../namespacemlx_1_1core_1_1fast.html#a9390693ff7be931f3ef3428e2ea4c3f9',1,'mlx::core::fast']]],
+  ['thread_5fsort_5ft_1',['thread_sort_t',['../struct_block_merge_sort.html#ad2474d16721f4ceb954125728a0e2ea2',1,'BlockMergeSort']]],
+  ['true_5ftype_2',['true_type',['../namespacemlx_1_1steel.html#a594a6ccb75b38b5ae4ddd0d9ad047b3a',1,'mlx::steel']]],
+  ['type_3',['type',['../structpocketfft_1_1detail_1_1add__vec.html#a7568dc83136c1b41eb71dcb78527227e',1,'pocketfft::detail::add_vec::type'],['../structpocketfft_1_1detail_1_1add__vec_3_01cmplx_3_01_t_01_4_01_4.html#a257b1c81fb9f559c48ee90497013494e',1,'pocketfft::detail::add_vec&lt; cmplx&lt; T &gt; &gt;::type'],['../structmlx_1_1steel_1_1integral__constant.html#a6492c15b37d160d3a33e1cbe770aa3f1',1,'mlx::steel::integral_constant::type'],['../structmetal_1_1make__void.html#aee74916713465374928c5379ab0d9b75',1,'metal::make_void::type'],['../structmetal_1_1pointer__element_3_01thread_01_t_01_5_01_4.html#a98fbc2aa99dd26bb35aa9cd1826318d8',1,'metal::pointer_element&lt; thread T * &gt;::type'],['../structmetal_1_1pointer__element_3_01device_01_t_01_5_01_4.html#ab36a7c5a64c0693dd3d8ccb322c163d4',1,'metal::pointer_element&lt; device T * &gt;::type'],['../structmetal_1_1pointer__element_3_01constant_01_t_01_5_01_4.html#ad154b55b9e450a6376016488c8e68c53',1,'metal::pointer_element&lt; constant T * &gt;::type'],['../structmetal_1_1pointer__element_3_01threadgroup_01_t_01_5_01_4.html#a78c718d6da9d393c139a385f42472362',1,'metal::pointer_element&lt; threadgroup T * &gt;::type']]]
 ];
