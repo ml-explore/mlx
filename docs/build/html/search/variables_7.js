8a70e9e8cb0edacbf7820ac1b2882ee33ba5d1c4
CircleCI Docs <mlx@group.apple.com>
2024-11-22 20:24:16 +0000

    rebase

--- docs/build/html/search/variables_7.js
+++ docs/build/html/search/variables_7.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['h12_0',['h12',['../namespacemlx_1_1core.html#a4beeeec4413be7adcfb14feaa9cf0e2e',1,'mlx::core']]],
-  ['h20_1',['h20',['../namespacemlx_1_1core.html#a862c6b94fec384c34a699ced64d01404',1,'mlx::core']]],
-  ['h28_2',['h28',['../namespacemlx_1_1core.html#ac447ad59592dd06435adca7df37e33ad',1,'mlx::core']]],
-  ['has_5fbatch_3',['has_batch',['../steel__gemm__fused_8h.html#adffcdc900c19ff97f1523e43f1a5a6cc',1,'steel_gemm_fused.h']]],
-  ['has_5fmul_5foperand_5fmask_4',['has_mul_operand_mask',['../struct_g_e_m_v_kernel.html#ad47223ee49b3cb7bf3746a2cec45f883',1,'GEMVKernel::has_mul_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a8db6f01f96a36b216acd801c34a96ef5',1,'GEMVTKernel::has_mul_operand_mask']]],
-  ['has_5fmul_5foutput_5fmask_5',['has_mul_output_mask',['../struct_g_e_m_v_kernel.html#a0edbf2dd6a6563e7afa6dab6b670615c',1,'GEMVKernel::has_mul_output_mask'],['../struct_g_e_m_v_t_kernel.html#a8eb06f6569e4042e24fee220b11fa10d',1,'GEMVTKernel::has_mul_output_mask']]],
-  ['has_5foperand_5fmask_6',['has_operand_mask',['../struct_g_e_m_v_kernel.html#ab00784dff1512a7b0919fcb4cfa5d50e',1,'GEMVKernel::has_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a6729d6e63e76a1e9c7c8e78d9aac4869',1,'GEMVTKernel::has_operand_mask']]],
-  ['has_5foutput_5fmask_7',['has_output_mask',['../struct_g_e_m_v_kernel.html#ab8b64c94f4c8f6f09c0777415589b487',1,'GEMVKernel::has_output_mask'],['../struct_g_e_m_v_t_kernel.html#aaefdf8f023da255bbb70a0c3e3408626',1,'GEMVTKernel::has_output_mask']]]
+  ['h_0',['H',['../structmlx_1_1steel_1_1_attn_params.html#a3d286a0c27bace6016ed7a87f43291b7',1,'mlx::steel::AttnParams']]],
+  ['h12_1',['h12',['../namespacemlx_1_1core.html#a4beeeec4413be7adcfb14feaa9cf0e2e',1,'mlx::core']]],
+  ['h20_2',['h20',['../namespacemlx_1_1core.html#a862c6b94fec384c34a699ced64d01404',1,'mlx::core']]],
+  ['h28_3',['h28',['../namespacemlx_1_1core.html#ac447ad59592dd06435adca7df37e33ad',1,'mlx::core']]],
+  ['has_5fbatch_4',['has_batch',['../steel__gemm__fused_8h.html#adffcdc900c19ff97f1523e43f1a5a6cc',1,'steel_gemm_fused.h']]],
+  ['has_5fmul_5foperand_5fmask_5',['has_mul_operand_mask',['../struct_g_e_m_v_kernel.html#ad47223ee49b3cb7bf3746a2cec45f883',1,'GEMVKernel::has_mul_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a8db6f01f96a36b216acd801c34a96ef5',1,'GEMVTKernel::has_mul_operand_mask']]],
+  ['has_5fmul_5foutput_5fmask_6',['has_mul_output_mask',['../struct_g_e_m_v_kernel.html#a0edbf2dd6a6563e7afa6dab6b670615c',1,'GEMVKernel::has_mul_output_mask'],['../struct_g_e_m_v_t_kernel.html#a8eb06f6569e4042e24fee220b11fa10d',1,'GEMVTKernel::has_mul_output_mask']]],
+  ['has_5foperand_5fmask_7',['has_operand_mask',['../struct_g_e_m_v_kernel.html#ab00784dff1512a7b0919fcb4cfa5d50e',1,'GEMVKernel::has_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a6729d6e63e76a1e9c7c8e78d9aac4869',1,'GEMVTKernel::has_operand_mask']]],
+  ['has_5foutput_5fmask_8',['has_output_mask',['../struct_g_e_m_v_kernel.html#ab8b64c94f4c8f6f09c0777415589b487',1,'GEMVKernel::has_output_mask'],['../struct_g_e_m_v_t_kernel.html#aaefdf8f023da255bbb70a0c3e3408626',1,'GEMVTKernel::has_output_mask']]]
 ];
