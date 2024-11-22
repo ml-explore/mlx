e3b2f750f6f24dc0b7de9bfc28dfdd3c30d6885c
CircleCI Docs <mlx@group.apple.com>
2024-11-22 20:24:16 +0000

    rebase

--- docs/build/html/search/typedefs_c.js
+++ docs/build/html/search/typedefs_c.js
@@ -1,8 +1,6 @@
 var searchData=
 [
-  ['safetensorsload_0',['SafetensorsLoad',['../namespacemlx_1_1core.html#a688cd7917b1365065e8059e9964c3d45',1,'mlx::core']]],
-  ['shape_5ft_1',['shape_t',['../namespacepocketfft_1_1detail.html#a885ee37fcf564a268a5c8ca9ea8603e1',1,'pocketfft::detail']]],
-  ['simplevalueandgradfn_2',['SimpleValueAndGradFn',['../namespacemlx_1_1core.html#a2689b8f1181648cb1685204fea9f3066',1,'mlx::core']]],
-  ['streamordevice_3',['StreamOrDevice',['../namespacemlx_1_1core.html#a95fc1013cc48fbfee0c54310711a5e58',1,'mlx::core']]],
-  ['stride_5ft_4',['stride_t',['../namespacepocketfft_1_1detail.html#afb987c919e9424a996d0fc8b3c23cc84',1,'pocketfft::detail']]]
+  ['radixfunc_0',['RadixFunc',['../backend_2metal_2kernels_2fft_8h.html#a6ba62eabfd5428644aabf89ddaa0128d',1,'fft.h']]],
+  ['reference_1',['reference',['../structmlx_1_1core_1_1array_1_1_array_iterator.html#a44e2e1f29191c20ec4390de4fa0bd59f',1,'mlx::core::array::ArrayIterator']]],
+  ['row_5ffrag_5ftype_2',['row_frag_type',['../structmlx_1_1steel_1_1_base_m_m_a_frag_3_01_t_00_018_00_018_01_4.html#a5ec2e40a8f5ad98c71b825544cdd878b',1,'mlx::steel::BaseMMAFrag&lt; T, 8, 8 &gt;']]]
 ];
