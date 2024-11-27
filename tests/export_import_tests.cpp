f9a86763a921e538c0cb0d22d6464dc6591e73c7
Awni Hannun <awni@apple.com>
2024-11-27 15:24:27 -0800

    refactor + works for few primitives

--- tests/export_import_tests.cpp
+++ tests/export_import_tests.cpp
@@ -6,7 +6,7 @@
 
 #include "doctest/doctest.h"
 
-#include "mlx/compile.h"
+#include "mlx/export.h"
 #include "mlx/mlx.h"
 
 using namespace mlx::core;
@@ -21,10 +21,24 @@ TEST_CASE("test export basic functions") {
   std::string file_path = "model.mlxfn"; // get_temp_file("model.mlxfn");
 
   auto fun = [](std::vector<array> x) -> std::vector<array> {
-    return {abs(negative(exp(x[0])))};
+    return {negative(exp(x[0]))};
   };
 
   export_function(file_path, fun, {array({1.0, 2.0})});
 
   auto imported_fun = import_function(file_path);
+
+  // Check num inputs mismatch throws
+  CHECK_THROWS_AS(
+      imported_fun({array({1.0}), array({2.0})}), std::invalid_argument);
+
+  // Check shape mismatch throws
+  CHECK_THROWS_AS(imported_fun({array({1.0})}), std::invalid_argument);
+
+  // Check type mismatch throws
+  CHECK_THROWS_AS(imported_fun({array({1.0}, float16)}), std::invalid_argument);
+
+  auto expected = fun({array({1.0, -1.0})});
+  auto out = imported_fun({array({1.0, -1.0})});
+  CHECK(allclose(expected[0], out[0]).item<bool>());
 }
