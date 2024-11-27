6ebb391e815dc3c38929bf803f5aecfa1d08fc35
Awni Hannun <awni@apple.com>
2024-11-27 15:24:27 -0800

    refactor + works for few primitives

---
+++ mlx/export.h
@@ -0,0 +1,25 @@
+// Copyright © 2024 Apple Inc.
+
+#pragma once
+
+#include <fstream>
+#include "mlx/array.h"
+
+namespace mlx::core {
+
+/**
+ * Export a function to a file.
+ */
+void export_function(
+    std::string path,
+    const std::function<std::vector<array>(const std::vector<array>&)>& fun,
+    const std::vector<array>& inputs,
+    bool shapeless = false);
+
+/**
+ * Import a function from a file.
+ */
+std::function<std::vector<array>(const std::vector<array>&)> import_function(
+    std::string path);
+
+} // namespace mlx::core
