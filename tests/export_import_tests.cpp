28ba75d80a480f0a714cde28a30de53928cdcb57
Awni Hannun <awni@apple.com>
2024-11-28 11:11:31 -0800

    allow primitives with state

--- tests/export_import_tests.cpp
+++ tests/export_import_tests.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2023 Apple Inc.
+// Copyright © 2024 Apple Inc.
 
 #include <filesystem>
 #include <stdexcept>
@@ -18,7 +18,7 @@ std::string get_temp_file(const std::string& name) {
 } // namespace
 
 TEST_CASE("test export basic functions") {
-  std::string file_path = "model.mlxfn"; // get_temp_file("model.mlxfn");
+  std::string file_path = get_temp_file("model.mlxfn");
 
   auto fun = [](std::vector<array> x) -> std::vector<array> {
     return {negative(exp(x[0]))};
@@ -40,5 +40,40 @@ TEST_CASE("test export basic functions") {
 
   auto expected = fun({array({1.0, -1.0})});
   auto out = imported_fun({array({1.0, -1.0})});
+  CHECK(allclose(expected[0], out[0]).item<bool>());
+}
+
+TEST_CASE("test export multi output primitives") {
+  std::string file_path = get_temp_file("model.mlxfn");
+
+  auto fun = [](std::vector<array> x) -> std::vector<array> {
+    return {divmod(x[0], x[1])};
+  };
+
+  auto inputs = std::vector<array>{array({5.0, -10.0}), array({3.0, -2.0})};
+  export_function(file_path, fun, inputs);
+
+  auto imported_fun = import_function(file_path);
+
+  auto expected = fun(inputs);
+  auto out = imported_fun(inputs);
+  CHECK(allclose(expected[0], out[0]).item<bool>());
+  CHECK(allclose(expected[1], out[1]).item<bool>());
+}
+
+TEST_CASE("test export primitives with state") {
+  std::string file_path = get_temp_file("model.mlxfn");
+
+  auto fun = [](std::vector<array> x) -> std::vector<array> {
+    return {argpartition(x[0], 2, 0)};
+  };
+
+  auto x = array({1, 3, 2, 4, 5, 7, 6, 8}, {4, 2});
+  export_function(file_path, fun, {x});
+
+  auto imported_fun = import_function(file_path);
+
+  auto expected = fun({x});
+  auto out = imported_fun({x});
   CHECK(allclose(expected[0], out[0]).item<bool>());
 }
