6530c8d36e87b7135588258db3443a5bd3ced43c
CircleCI Docs <mlx@group.apple.com>
2024-12-06 21:22:39 +0000

    rebase

--- docs/build/html/search/all_19.js
+++ docs/build/html/search/all_19.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['zero_5fhelper_0',['zero_helper',['../sort_8h.html#aca8b6f36c9024b8406fe545765316dc0',1,'sort.h']]],
-  ['zeros_1',['zeros',['../group__ops.html#ga34573eb2ead179839579142f3aedb3b8',1,'mlx::core::zeros(const std::vector&lt; int &gt; &amp;shape, Dtype dtype, StreamOrDevice s={})'],['../group__ops.html#ga7c1487f8a830c9df75a961b3a24527ee',1,'mlx::core::zeros(const std::vector&lt; int &gt; &amp;shape, StreamOrDevice s={})']]],
+  ['zeros_1',['zeros',['../group__ops.html#gae2cace3b388cec4e520659a91879e1c1',1,'mlx::core::zeros(const Shape &amp;shape, Dtype dtype, StreamOrDevice s={})'],['../group__ops.html#gac8aa722f5e798819b7091693173f1f36',1,'mlx::core::zeros(const Shape &amp;shape, StreamOrDevice s={})']]],
   ['zeros_5flike_2',['zeros_like',['../group__ops.html#gafbb857094d784b38c78683a091ffdbde',1,'mlx::core']]]
 ];
