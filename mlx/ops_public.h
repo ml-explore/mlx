8e410df5db7cd6c527b2f203285d1d62680aacf6
Cheng <git@zcbenz.com>
2024-12-09 16:40:57 +0900

    Add comment

--- mlx/ops_public.h
+++ mlx/ops_public.h
@@ -6,8 +6,13 @@
 
 namespace mlx::core {
 
+// The "std" function has a name collision with "namespace std" in MSVC after
+// "using namespace mlx::core", to work around it in our python bindings code
+// we use "std_dev" as name instead and only expose the "std" function in
+// public header.
 template <typename... Args>
 inline array std(Args&&... args) {
   return std_dev(std::forward<Args>(args)...);
 }
+
 } // namespace mlx::core
