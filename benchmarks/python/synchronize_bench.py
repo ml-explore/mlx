2f40d9535e8fbffa2480a2891fa155f6eff56741
Awni Hannun <awni@apple.com>
2025-01-17 17:03:38 -0800

    update benchmarks

--- benchmarks/python/synchronize_bench.py
+++ benchmarks/python/synchronize_bench.py
@@ -2,31 +2,54 @@ import time
 
 import mlx.core as mx
 
-world = mx.distributed.init()
-
-a = mx.ones((5, 5), mx.int32)
-its = 10
-its_per_eval = 100
+rank = mx.distributed.init().rank()
 
 
-def fn(x):
-    for _ in range(its_per_eval):
-        x = mx.distributed.all_sum(x)
-        x = x - 1
-    return x
+def timeit(fn, a):
+
+    # warmup
+    for _ in range(5):
+        mx.eval(fn(a))
+
+    its = 10
+    tic = time.perf_counter()
+    for _ in range(its):
+        mx.eval(fn(a))
+    toc = time.perf_counter()
+    ms = 1000 * (toc - tic) / its
+    return ms
 
 
-# warmup
-for _ in range(5):
-    x = fn(a)
-    assert mx.array_equal(x, mx.ones_like(x))
+def all_reduce_benchmark():
+    a = mx.ones((5, 5), mx.int32)
 
-tic = time.perf_counter()
+    its_per_eval = 100
 
-for _ in range(its):
-    x = fn(a)
-    mx.eval(x)
+    def fn(x):
+        for _ in range(its_per_eval):
+            x = mx.distributed.all_sum(x)
+            x = x - 1
+        return x
 
-toc = time.perf_counter()
-ms = 1000 * (toc - tic) / (its * its_per_eval)
-print(f"Time per iteration {ms:.6f} (ms)")
+    ms = timeit(fn, a) / its_per_eval
+    if rank == 0:
+        print(f"All Reduce: time per iteration {ms:.6f} (ms)")
+
+
+def all_gather_benchmark():
+    a = mx.ones((5, 5), mx.int32)
+    its_per_eval = 100
+
+    def fn(x):
+        for _ in range(its_per_eval):
+            x = mx.distributed.all_gather(x)[0]
+        return x
+
+    ms = timeit(fn, a) / its_per_eval
+    if rank == 0:
+        print(f"All gather: time per iteration {ms:.6f} (ms)")
+
+
+if __name__ == "__main__":
+    all_reduce_benchmark()
+    all_gather_benchmark()
