aca0721de07ca1b52f195cfc9c42f9b380ed4e9e
Awni Hannun <awni@apple.com>
2025-01-17 17:08:35 -0800

    doc string in metal fence

--- mlx/backend/metal/fence.h
+++ mlx/backend/metal/fence.h
@@ -4,6 +4,20 @@
 
 namespace mlx::core {
 
+/* A fence to be used for synchronizing work between the CPU and GPU
+ *
+ * Calls to `update_gpu` should be paired with calls to `wait`. This ensures
+ * that the array passed to `update_gpu` is computed and visible to the CPU
+ * after the call to `wait` returns.
+ *
+ * Calls to `update` should be paired with calls to `wait_gpu`. This ensures
+ * that the array passed to `wait_gpu` will not be read by the GPU until the CPU
+ * has called `update`.
+ *
+ * The fence supports slow (default) and fast mode. Fast mode requires setting
+ * the environment variable `MLX_METAL_FAST_SYNCH=1`. Fast mode also requires
+ * Metal 3.2+ (macOS 15+, iOS 18+).
+ */
 class Fence {
  public:
   Fence(const Stream& stream);
