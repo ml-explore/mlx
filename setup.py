25ef4f00074600cf4bc1ac38e11749b6aeea28d0
Awni Hannun <awni@apple.com>
2025-02-06 10:50:13 -0800

    bump

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
 
     setup(
         name="mlx",
-        version=get_version("0.22.0"),
+        version=get_version("0.22.1"),
         author="MLX Contributors",
         author_email="mlx@group.apple.com",
         description="A framework for machine learning on Apple silicon.",
