f066e9bc9a22324507ee4b25d8909ffa7602f35d
Jagrit Digani <digani@apple.com>
2025-02-25 13:29:22 -0800

    Remove commented out function

--- mlx/backend/metal/kernels/steel/attn/mma.h
+++ mlx/backend/metal/kernels/steel/attn/mma.h
@@ -417,29 +417,6 @@ struct MMATile {
   }
 };
 
-// template <typename T, typename U, int M, int N, int K>
-// METAL_FUNC void tile_matmad(
-//     thread MMATile<T, M, N>& D,
-//     thread MMATile<U, M, K>& A,
-//     thread MMATile<U, K, N>& B,
-//     thread MMATile<T, M, N>& C) {
-//   STEEL_PRAGMA_UNROLL
-//   for (short k = 0; k < K; ++k) {
-//     STEEL_PRAGMA_UNROLL
-//     for (short m = 0; m < M; ++m) {
-//       STEEL_PRAGMA_UNROLL
-//       for (short n = 0; n < N; ++n) {
-//         short n_serp = (m % 2) ? (N - 1 - n) : n;
-//         MMATile<T, M, N>::MMAFrag_t::mma(
-//             D.frag_at(m, n_serp),
-//             A.frag_at(m, k),
-//             B.frag_at(k, n_serp),
-//             C.frag_at(m, n_serp));
-//       }
-//     }
-//   }
-// }
-
 template <
     typename Dtype,
     typename Atype,
