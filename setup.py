ebc6804b68ff0cebc722935ad75aa2c623587788
Angelos Katharopoulos <a_katharopoulos@apple.com>
2025-02-25 00:27:45 -0800

    Add the prepare tb ring script

--- setup.py
+++ setup.py
@@ -194,7 +194,12 @@ if __name__ == "__main__":
                 "typing_extensions",
             ],
         },
-        entry_points={"console_scripts": ["mlx.launch = mlx.distributed_run:main"]},
+        entry_points={
+            "console_scripts": [
+                "mlx.launch = mlx.distributed_run:main",
+                "mlx.prepare_tb_ring = mlx.distributed_run:prepare_tb_ring",
+            ]
+        },
         ext_modules=[CMakeExtension("mlx.core")],
         cmdclass={"build_ext": CMakeBuild, "generate_stubs": GenerateStubs},
         zip_safe=False,
