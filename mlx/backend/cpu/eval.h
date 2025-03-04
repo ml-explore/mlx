f140792f1cc740fcb16819188b6e33bc0b4c1a00
Awni Hannun <awni@apple.com>
2025-03-04 12:50:46 -0800

    more donation take 2

--- mlx/backend/cpu/eval.h
+++ mlx/backend/cpu/eval.h
@@ -2,11 +2,16 @@
 
 #pragma once
 
+#include <unordered_set>
+
 #include "mlx/array.h"
 #include "mlx/stream.h"
 
 namespace mlx::core::cpu {
 
 void eval(array& arr);
+void finalize(
+    Stream s,
+    std::unordered_set<std::shared_ptr<array::Data>> retain_buffers);
 
 } // namespace mlx::core::cpu
