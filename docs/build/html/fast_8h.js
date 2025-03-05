6d3c96dc5ca3b766da21b041a6b9579d9840c6a6
CircleCI Docs <mlx@group.apple.com>
2025-03-05 21:30:09 +0000

    rebase

--- docs/build/html/fast_8h.js
+++ docs/build/html/fast_8h.js
@@ -6,7 +6,7 @@ var fast_8h =
     [ "mlx::core::fast::affine_quantize", "namespacemlx_1_1core_1_1fast.html#aa4b5f6886b2288cb6dfdd8598579f080", null ],
     [ "mlx::core::fast::layer_norm", "namespacemlx_1_1core_1_1fast.html#a01bd533ebd0e2415c4ee30032d51d7bf", null ],
     [ "mlx::core::fast::metal_kernel", "namespacemlx_1_1core_1_1fast.html#ab16436b465dc10ce472193d541d8426e", null ],
-    [ "mlx::core::fast::rms_norm", "namespacemlx_1_1core_1_1fast.html#ac7b620275c6386f822b7aacc6b312e62", null ],
+    [ "mlx::core::fast::rms_norm", "namespacemlx_1_1core_1_1fast.html#a85ec3abc6b9d968c58275f5eef916f01", null ],
     [ "mlx::core::fast::rope", "namespacemlx_1_1core_1_1fast.html#a1632b78950f0c8c31b24be7d80faeb39", null ],
     [ "mlx::core::fast::rope", "namespacemlx_1_1core_1_1fast.html#a534ef357eae24892684a6ecd866d3fab", null ],
     [ "mlx::core::fast::scaled_dot_product_attention", "namespacemlx_1_1core_1_1fast.html#a3663b50265b0a9c0cca2b5376852e059", null ]
--- docs/build/html/sdpa__vector_8h.js
+++ docs/build/html/sdpa__vector_8h.js
@@ -1,7 +1,8 @@
 var sdpa__vector_8h =
 [
-    [ "sdpa_vector", "sdpa__vector_8h.html#a826f7a3c7ab843abc0842241db3e57b3", null ],
-    [ "sdpa_vector_2pass_1", "sdpa__vector_8h.html#aae1a2f23b03e24734805b08ebc5c1a59", null ],
-    [ "sdpa_vector_2pass_2", "sdpa__vector_8h.html#a1368cf3618a4e03dbf743b3463205efe", null ],
-    [ "has_mask", "sdpa__vector_8h.html#a6ed0dd113fe7d471fc0b869b8c028c81", null ]
+    [ "sdpa_vector", "sdpa__vector_8h.html#aa83885125881230b6c4657dd3d0eba18", null ],
+    [ "sdpa_vector_2pass_1", "sdpa__vector_8h.html#ae2a4a8d17e571578ed529f4d4afe93ac", null ],
+    [ "sdpa_vector_2pass_2", "sdpa__vector_8h.html#ae1be83816bf9332277dab185aa1b58c2", null ],
+    [ "has_mask", "sdpa__vector_8h.html#a6ed0dd113fe7d471fc0b869b8c028c81", null ],
+    [ "query_transposed", "sdpa__vector_8h.html#a0c2c54bcc20cc4783a5040d47fa3ba81", null ]
 ];
