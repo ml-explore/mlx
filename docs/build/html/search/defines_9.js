93748c5cca65d7a10e857c30424436d699d3aad0
CircleCI Docs <mlx@group.apple.com>
2025-03-05 21:30:09 +0000

    rebase

--- docs/build/html/search/defines_9.js
+++ docs/build/html/search/defines_9.js
@@ -7,5 +7,9 @@ var searchData=
   ['mlx_5fmtl_5floop_5funroll_4',['MLX_MTL_LOOP_UNROLL',['../sort_8h.html#ad34b622323cebef136669fedd7229515',1,'sort.h']]],
   ['mlx_5fmtl_5fpragma_5funroll_5',['MLX_MTL_PRAGMA_UNROLL',['../gemv__masked_8h.html#a069b682d7d21827461544817d722bfd3',1,'MLX_MTL_PRAGMA_UNROLL:&#160;gemv_masked.h'],['../backend_2metal_2kernels_2utils_8h.html#a069b682d7d21827461544817d722bfd3',1,'MLX_MTL_PRAGMA_UNROLL:&#160;utils.h']]],
   ['mlx_5fsimd_5flibrary_5fversion_6',['MLX_SIMD_LIBRARY_VERSION',['../accelerate__simd_8h.html#a628420ebe2f37d352c8c4b0dd0db59d0',1,'accelerate_simd.h']]],
-  ['mtl_5fconst_7',['MTL_CONST',['../defines_8h.html#a767ed9f2604de22b259cee02c4ce1d22',1,'defines.h']]]
+  ['mlx_5fversion_5fmajor_7',['MLX_VERSION_MAJOR',['../version_8h.html#a11113ca6d778b3970362ab4bdce9f199',1,'version.h']]],
+  ['mlx_5fversion_5fminor_8',['MLX_VERSION_MINOR',['../version_8h.html#a33b774f54c2725d743e28dd9bf89969e',1,'version.h']]],
+  ['mlx_5fversion_5fnumeric_9',['MLX_VERSION_NUMERIC',['../version_8h.html#a28be6f5338015802ef5f1ad6a4c98750',1,'version.h']]],
+  ['mlx_5fversion_5fpatch_10',['MLX_VERSION_PATCH',['../version_8h.html#ab25bf5456c6cb58d66fc90e143a26530',1,'version.h']]],
+  ['mtl_5fconst_11',['MTL_CONST',['../defines_8h.html#a767ed9f2604de22b259cee02c4ce1d22',1,'defines.h']]]
 ];
