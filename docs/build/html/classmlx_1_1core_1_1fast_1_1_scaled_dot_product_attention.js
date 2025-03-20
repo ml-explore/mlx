148bb432af190084307638508b9fab6e4b33c67d
CircleCI Docs <mlx@group.apple.com>
2025-03-20 22:37:22 +0000

    rebase

--- docs/build/html/classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.js
+++ docs/build/html/classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.js
@@ -1,6 +1,6 @@
 var classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention =
 [
-    [ "ScaledDotProductAttention", "classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.html#ab3f78d30e5bb3e76cfe701f2358e4748", null ],
+    [ "ScaledDotProductAttention", "classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.html#a09c99b460cca606b2ebb22f90b3d13a2", null ],
     [ "DEFINE_INPUT_OUTPUT_SHAPE", "classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.html#a64d2ce4b46b529a6a9ef068947bc623e", null ],
     [ "DEFINE_PRINT", "classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.html#a6cc2092fa5b8e7585921b8e0f3ec3db7", null ],
     [ "eval_cpu", "classmlx_1_1core_1_1fast_1_1_scaled_dot_product_attention.html#ae20851e002f7fcb6d4f97817596f6328", null ],
--- docs/build/html/search/all_8.js
+++ docs/build/html/search/all_8.js
@@ -20,10 +20,10 @@ var searchData=
   ['half_5finplace_5fop_17',['half_inplace_op',['../fp16_8h.html#a6348c00d31a50b2df1b47d18af49c4b8',1,'fp16.h']]],
   ['half_5ftypes_2eh_18',['half_types.h',['../half__types_8h.html',1,'']]],
   ['has_5fbatch_19',['has_batch',['../steel__gemm__fused_8h.html#adffcdc900c19ff97f1523e43f1a5a6cc',1,'steel_gemm_fused.h']]],
-  ['has_5fmask_20',['has_mask',['../sdpa__vector_8h.html#a6ed0dd113fe7d471fc0b869b8c028c81',1,'sdpa_vector.h']]],
-  ['has_5fmul_5foperand_5fmask_21',['has_mul_operand_mask',['../struct_g_e_m_v_kernel.html#ad47223ee49b3cb7bf3746a2cec45f883',1,'GEMVKernel::has_mul_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a8db6f01f96a36b216acd801c34a96ef5',1,'GEMVTKernel::has_mul_operand_mask']]],
-  ['has_5fmul_5foutput_5fmask_22',['has_mul_output_mask',['../struct_g_e_m_v_kernel.html#a0edbf2dd6a6563e7afa6dab6b670615c',1,'GEMVKernel::has_mul_output_mask'],['../struct_g_e_m_v_t_kernel.html#a8eb06f6569e4042e24fee220b11fa10d',1,'GEMVTKernel::has_mul_output_mask']]],
-  ['has_5foperand_5fmask_23',['has_operand_mask',['../struct_g_e_m_v_kernel.html#ab00784dff1512a7b0919fcb4cfa5d50e',1,'GEMVKernel::has_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a6729d6e63e76a1e9c7c8e78d9aac4869',1,'GEMVTKernel::has_operand_mask']]],
-  ['has_5foutput_5fmask_24',['has_output_mask',['../struct_g_e_m_v_kernel.html#ab8b64c94f4c8f6f09c0777415589b487',1,'GEMVKernel::has_output_mask'],['../struct_g_e_m_v_t_kernel.html#aaefdf8f023da255bbb70a0c3e3408626',1,'GEMVTKernel::has_output_mask']]],
+  ['has_5fmask_20',['has_mask',['../sdpa__vector_8h.html#a6ed0dd113fe7d471fc0b869b8c028c81',1,'has_mask:&#160;sdpa_vector.h'],['../steel__attention_8h.html#a6ed0dd113fe7d471fc0b869b8c028c81',1,'has_mask:&#160;steel_attention.h']]],
+  ['has_5fmul_5foperand_5fmask_21',['has_mul_operand_mask',['../struct_g_e_m_v_kernel.html#a09f0e646822d45dc2543e31509552258',1,'GEMVKernel::has_mul_operand_mask'],['../struct_g_e_m_v_t_kernel.html#a94f56f0ecf1f148f0513312325f33653',1,'GEMVTKernel::has_mul_operand_mask']]],
+  ['has_5fmul_5foutput_5fmask_22',['has_mul_output_mask',['../struct_g_e_m_v_kernel.html#ad54fb5c6f0f9a820365b638542693291',1,'GEMVKernel::has_mul_output_mask'],['../struct_g_e_m_v_t_kernel.html#a19df54577e341044dc8e57cc5b6147f3',1,'GEMVTKernel::has_mul_output_mask']]],
+  ['has_5foperand_5fmask_23',['has_operand_mask',['../struct_g_e_m_v_kernel.html#a68bad880d1e689228ae4d3c6958cc6c1',1,'GEMVKernel::has_operand_mask'],['../struct_g_e_m_v_t_kernel.html#aef8622b2f76d1ff272ced396c89e829d',1,'GEMVTKernel::has_operand_mask']]],
+  ['has_5foutput_5fmask_24',['has_output_mask',['../struct_g_e_m_v_kernel.html#a3b0b9ccf11bd5d7de50b9626fa9a22cc',1,'GEMVKernel::has_output_mask'],['../struct_g_e_m_v_t_kernel.html#a19bcb2d9377493a81da072f33711de33',1,'GEMVTKernel::has_output_mask']]],
   ['has_5fprimitive_25',['has_primitive',['../classmlx_1_1core_1_1array.html#aa5aceab15241e7826cbaf8b8a41440c1',1,'mlx::core::array']]]
 ];
