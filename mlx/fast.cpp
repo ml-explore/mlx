8ed25e7f2a9a3a4a75f262f4c0194dc38650f432
Jagrit Digani <digani@apple.com>
2025-03-20 12:33:37 -0700

    Update attention tests to show diff, disable array masks

--- mlx/fast.cpp
+++ mlx/fast.cpp
@@ -748,8 +748,8 @@ array scaled_dot_product_attention(
       (query_head_dim == 64 || query_head_dim == 80 || query_head_dim == 128);
 
   const bool sdpa_vector_supported_mask = (!has_mask || has_bool_mask);
-  const bool sdpa_full_supported_mask = !has_mask || has_arr_mask ||
-      (query_sequence_length <= key_sequence_length && do_causal);
+  const bool sdpa_full_supported_mask =
+      !has_mask || (query_sequence_length <= key_sequence_length && do_causal);
 
   const bool supports_sdpa_full = query_sequence_length >= threshold &&
       sdpa_full_supported_mask && sdpa_full_supported_head_dim &&
