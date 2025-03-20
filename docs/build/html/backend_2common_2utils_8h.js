24db04673c14e33d7ded9b31196fc7763d153ea3
CircleCI Docs <mlx@group.apple.com>
2025-03-20 22:37:22 +0000

    rebase

--- docs/build/html/backend_2common_2utils_8h.js
+++ docs/build/html/backend_2common_2utils_8h.js
@@ -11,8 +11,6 @@ var backend_2common_2utils_8h =
     [ "mlx::core::elem_to_loc", "namespacemlx_1_1core.html#a59c0af06c5325c04ad8d69563c1c6b0a", null ],
     [ "mlx::core::is_donatable", "namespacemlx_1_1core.html#af650e831ce21759da1ac103037d08d84", null ],
     [ "mlx::core::make_contiguous_strides", "namespacemlx_1_1core.html#a449ef1148816a37bbc7ffd43d3c586a0", null ],
-    [ "mlx::core::move_or_copy", "namespacemlx_1_1core.html#a830a47d8a317dffb0c88e5a7afe6aee2", null ],
-    [ "mlx::core::move_or_copy", "namespacemlx_1_1core.html#a9fcb3711b150cb65c7778a35c51284b2", null ],
     [ "mlx::core::prepare_reshape", "namespacemlx_1_1core.html#a6783cfc7dbe1a116ba84a3904a37145f", null ],
     [ "mlx::core::shared_buffer_reshape", "namespacemlx_1_1core.html#a88d88987bd8bf3ca46bf3b5e8aacce9d", null ]
 ];
--- docs/build/html/compiled_8h.js
+++ docs/build/html/compiled_8h.js
@@ -1,7 +1,7 @@
 var compiled_8h =
 [
     [ "mlx::core::build_lib_name", "namespacemlx_1_1core.html#a3ef23f334cb9f68a2c50524bc67c913b", null ],
-    [ "mlx::core::compiled_allocate_outputs", "namespacemlx_1_1core.html#ab8c3c4fc05745f586de922c8266f4fce", null ],
+    [ "mlx::core::compiled_allocate_outputs", "namespacemlx_1_1core.html#a8ed5ff0d69f6c7d2e092fe811e40d564", null ],
     [ "mlx::core::compiled_check_contiguity", "namespacemlx_1_1core.html#a562040f4a03f2c0a5d50eb9c8f14a8be", null ],
     [ "mlx::core::get_type_string", "namespacemlx_1_1core.html#af776fd91dd60594dcfebbafd17f19068", null ],
     [ "mlx::core::is_scalar", "namespacemlx_1_1core.html#a985c60929757190e0b4ec51f57c767d0", null ],
