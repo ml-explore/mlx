a37f6fa30fa92f3a918205b33b576628c740da16
chunyang-wen <chunyang.wen@gmail.com>
2025-03-21 20:29:21 +0800

    Use __all__ to avoid import *

--- python/mlx/optimizers/__init__.py
+++ python/mlx/optimizers/__init__.py
@@ -1,4 +1,46 @@
 # Copyright © 2023-2024 Apple Inc.
 
-from mlx.optimizers.optimizers import *
-from mlx.optimizers.schedulers import *
+from mlx.optimizers.optimizers import (
+    SGD,
+    AdaDelta,
+    Adafactor,
+    Adagrad,
+    Adam,
+    Adamax,
+    AdamW,
+    Lion,
+    MultiOptimizer,
+    Optimizer,
+    RMSprop,
+    clip_grad_norm,
+)
+from mlx.optimizers.schedulers import (
+    cosine_decay,
+    exponential_decay,
+    join_schedules,
+    linear_schedule,
+    step_decay,
+)
+
+__all__ = [
+    # Schedulers
+    "cosine_decay",
+    "exponential_decay",
+    "join_schedules",
+    "linear_schedule",
+    "step_decay",
+    # Optimizers
+    "AdaDelta",
+    "Adafactor",
+    "Adagrad",
+    "Adam",
+    "AdamW",
+    "Adamax",
+    "Lion",
+    "MultiOptimizer",
+    "Optimizer",
+    "RMSprop",
+    "SGD",
+    # Functions
+    "clip_grad_norm",
+]
