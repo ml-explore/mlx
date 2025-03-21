6a79e8086a5f011311cf8f6e53301efc41fe0ed0
Angelos Katharopoulos <a_katharopoulos@apple.com>
2025-03-21 13:41:11 -0700

    Move memory stats calls to top-level namespace

--- python/tests/mlx_distributed_tests.py
+++ python/tests/mlx_distributed_tests.py
@@ -66,18 +66,18 @@ class MLXDistributedCommonTestCase(mlx_tests.MLXTestCase):
     def test_donation(self):
         x = mx.random.normal((1024,))
         mx.eval(x)
-        mx.synchronize(mx.default_stream(mx.default_device()))
+        mx.synchronize()
 
-        mx.metal.reset_peak_memory()
+        mx.reset_peak_memory()
         scale = mx.array(2.0)
         y = mx.distributed.all_sum(x)
         mx.eval(y)
-        mx.synchronize(mx.default_stream(mx.default_device()))
-        all_sum_only = mx.metal.get_peak_memory()
+        mx.synchronize()
+        all_sum_only = mx.get_peak_memory()
         y = mx.distributed.all_sum(x) * scale
         mx.eval(y)
-        mx.synchronize(mx.default_stream(mx.default_device()))
-        all_sum_with_binary = mx.metal.get_peak_memory()
+        mx.synchronize()
+        all_sum_with_binary = mx.get_peak_memory()
 
         self.assertEqual(all_sum_only, all_sum_with_binary)
 
