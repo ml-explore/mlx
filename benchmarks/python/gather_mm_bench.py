da345a803c1e041e0983c8a937e25fe379b16f10
Angelos Katharopoulos <a_katharopoulos@apple.com>
2025-04-04 21:33:01 -0700

    Generalize the benchmark

--- benchmarks/python/gather_mm_bench.py
+++ benchmarks/python/gather_mm_bench.py
@@ -4,7 +4,7 @@ import mlx.core as mx
 from time_utils import time_fn
 
 N = 1024
-K = 1024
+D = 1024
 M = 1024
 E = 32
 I = 4
@@ -35,39 +35,39 @@ def gather_mm_simulate(x, w, indices):
 
 
 def time_gather_mm():
-    x = mx.random.normal((N, 1, 1, K)) / 1024**0.5
-    w = mx.random.normal((E, M, K)) / 1024**0.5
+    x = mx.random.normal((N, 1, 1, D)) / 1024**0.5
+    w1 = mx.random.normal((E, M, D)) / 1024**0.5
+    w2 = mx.random.normal((E, D, M)) / 1024**0.5
     indices = (mx.random.uniform(shape=(N, I)) * E).astype(mx.uint32)
     sorted_indices = mx.sort(indices.flatten()).reshape(N, I)
-    mx.eval(x, w, indices, sorted_indices)
+    mx.eval(x, w1, w2, indices, sorted_indices)
 
-    def gather_mm(x, w, indices, sort):
+    def gather_mm(x, w1, w2, indices, sort):
         idx = indices
         inv_order = None
         if sort:
             x, idx, inv_order = gather_sort(x, indices)
-        for _ in range(2):
-            x = mx.gather_mm(
-                x, w.swapaxes(-1, -2), rhs_indices=idx, sorted_indices=sort
-            )
+        x = mx.gather_mm(x, w1.swapaxes(-1, -2), rhs_indices=idx, sorted_indices=sort)
+        x = mx.gather_mm(x, w2.swapaxes(-1, -2), rhs_indices=idx, sorted_indices=sort)
         if sort:
             x = scatter_unsort(x, inv_order, indices.shape)
         return x
 
-    time_fn(gather_mm, x, w, indices, False)
-    time_fn(gather_mm, x, w, sorted_indices, False)
-    time_fn(gather_mm, x, w, indices, True)
+    time_fn(gather_mm, x, w1, w2, indices, False)
+    time_fn(gather_mm, x, w1, w2, sorted_indices, False)
+    time_fn(gather_mm, x, w1, w2, indices, True)
 
-    x = mx.random.normal((N * I, K)) / 1024**0.5
-    w = mx.random.normal((M, K)) / 1024**0.5
-    mx.eval(x, w)
+    x = mx.random.normal((N * I, D)) / 1024**0.5
+    w1 = mx.random.normal((M, D)) / 1024**0.5
+    w2 = mx.random.normal((D, M)) / 1024**0.5
+    mx.eval(x, w1, w2)
 
-    def equivalent_matmul(x, w):
-        for _ in range(2):
-            x = x @ w.T
+    def equivalent_matmul(x, w1, w2):
+        x = x @ w1.T
+        x = x @ w2.T
         return x
 
-    time_fn(equivalent_matmul, x, w)
+    time_fn(equivalent_matmul, x, w1, w2)
 
 
 if __name__ == "__main__":
