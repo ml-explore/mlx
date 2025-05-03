d81c2ec3af9c07f0c45e7385458f436a0a6ab3eb
Awni Hannun <awni.hannun@gmail.com>
2025-05-02 17:05:04 -0700

    add compile

--- mlx/backend/metal/kernels/utils.h
+++ mlx/backend/metal/kernels/utils.h
@@ -15,7 +15,8 @@
 
 typedef half float16_t;
 
-// Work per thread values for different types
+// Work per thread values for different types. The values here are expected to
+// match get_work_per_thread in mlx/backend/metal/utils.h
 template <typename U>
 struct WorkPerThread {
   static_assert(sizeof(U) <= 8, "Type too large");
