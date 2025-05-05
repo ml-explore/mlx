09712b8dd090bde21fbd6cd87b35934e9ddf249b
Awni Hannun <awni.hannun@gmail.com>
2025-05-05 15:52:12 -0700

    fix input coherent kernel launch

--- mlx/backend/metal/fence.cpp
+++ mlx/backend/metal/fence.cpp
@@ -138,7 +138,7 @@ void Fence::update(Stream stream, const array& x) {
   compute_encoder.set_compute_pipeline_state(kernel);
   compute_encoder.set_input_array(x, 0);
   compute_encoder.set_bytes(nthreads, 1);
-  compute_encoder.dispatch_threadgroups(group_dims, grid_dims);
+  compute_encoder.dispatch_threadgroups(grid_dims, group_dims);
 
   // Barrier on previous kernels
   compute_encoder.barrier();
