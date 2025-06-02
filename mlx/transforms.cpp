95b7551d65c24d65e0a3de2a656f11577499fd9f
Cheng <git@zcbenz.com>
2025-06-03 05:23:34 +0900

    Do not check event.is_signaled() in eval_impl (#2230)

--- mlx/transforms.cpp
+++ mlx/transforms.cpp
@@ -208,9 +208,7 @@ array eval_impl(std::vector<array> outputs, bool async) {
         // output arrays stream
         fences[it->second].wait(stream, in);
       } else if (in.event().valid()) {
-        if (in.event().is_signaled()) {
-          in.detach_event();
-        } else if (in.event().stream() != stream) {
+        if (in.event().stream() != stream) {
           // Use event to wait across async eval
           in.event().wait(stream);
         }
