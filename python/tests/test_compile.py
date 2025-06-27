2c11d10f8d8e4d124cb447af731b9199374695bb
Angelos Katharopoulos <a_katharopoulos@apple.com>
2025-06-26 22:08:18 -0700

    Split broadcast so it is always fused in compile (#2318)

--- python/tests/test_compile.py
+++ python/tests/test_compile.py
@@ -5,6 +5,7 @@ import io
 import math
 import unittest
 from functools import partial
+from io import StringIO
 
 import mlx.core as mx
 import mlx_tests
@@ -990,6 +991,28 @@ class TestCompile(mlx_tests.MLXTestCase):
             y = fun(x).item()
             y_compiled = mx.compile(fun)(x).item()
             self.assertEqual(y, y_compiled)
+
+    def test_shared_broadcast(self):
+        def fun(x, y, z):
+            yy = mx.broadcast_to(y, z.shape)
+            return (x + yy * z), yy.sum()
+
+        a = mx.random.normal((10, 10))
+        b = mx.array(0.1)
+        c = mx.random.normal((10, 10))
+        mx.eval(a, b, c)
+        fc = mx.compile(fun)
+        d = fc(a, b, c)
+
+        s = StringIO()
+        mx.export_to_dot(s, a=a, b=b, c=c, d1=d[0], d2=d[1])
+        s.seek(0)
+        s = s.read()
+
+        self.assertTrue("CompiledBroadcastMultiplyAdd" in s)
+        d_hat = fun(a, b, c)
+        self.assertTrue(mx.allclose(d[0], d_hat[0]))
+        self.assertTrue(mx.allclose(d[1], d_hat[1]))
 
 
 if __name__ == "__main__":
