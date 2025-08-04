434ed933c537058e192e9145f16b49b3cedf1ca2
Awni Hannun <awni@apple.com>
2025-08-04 14:52:22 -0700

    default install cuda on linux

--- setup.py
+++ setup.py
@@ -274,11 +274,13 @@ if __name__ == "__main__":
     #  - Package name is back-end specific, e.g mlx-metal
     if build_stage != 2:
         if build_stage == 1:
-            install_requires.append(
-                f'mlx-metal=={version}; platform_system == "Darwin"'
-            )
-            extras["cuda"] = [f'mlx-cuda=={version}; platform_system == "Linux"']
-            extras["cpu"] = [f'mlx-cpu=={version}; platform_system == "Linux"']
+            install_requires += [
+                f'mlx-metal=={version}; platform_system == "Darwin"',
+                f'mlx-cuda=={version}; extra != "cpu" and platform_system == "linux"',
+            ]
+            extras["cpu"] = [
+                f'mlx-cpu=={version}; extra == "cpu" and platform_system == "linux"'
+            ]
 
         _setup(
             name="mlx",
