bbf14239538c18a9f8a3d61f52ab0f29589410f0
Awni Hannun <awni@apple.com>
2025-09-30 16:08:46 -0700

    wait for tasks in cuda (#2636)

--- mlx/backend/cuda/allocator.cpp
+++ mlx/backend/cuda/allocator.cpp
@@ -86,7 +86,7 @@ CudaAllocator::CudaAllocator()
   // TODO: Set memory limit for multi-device.
   size_t free, total;
   CHECK_CUDA_ERROR(cudaMemGetInfo(&free, &total));
-  memory_limit_ = total * 0.8;
+  memory_limit_ = total * 0.95;
   max_pool_size_ = memory_limit_;
 }
 
