b704e9e77a4f184edaf2bc64d62c3cda3134589a
Cheng <zcbenz@gmail.com>
2025-11-14 07:10:23 +0900

    [CUDA] Check CUDA error in synchronize (#2757)

--- mlx/backend/cuda/device.cpp
+++ mlx/backend/cuda/device.cpp
@@ -368,7 +368,7 @@ void CommandEncoder::commit() {
 }
 
 void CommandEncoder::synchronize() {
-  cudaStreamSynchronize(stream_);
+  CHECK_CUDA_ERROR(cudaStreamSynchronize(stream_));
   auto p = std::make_shared<std::promise<void>>();
   std::future<void> f = p->get_future();
   add_completed_handler([p = std::move(p)]() { p->set_value(); });
