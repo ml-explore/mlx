1bedcbd55616e4dd06b2acaccd6bc310c7e4b163
Cheng <zcbenz@gmail.com>
2025-11-16 07:12:47 +0900

    Fix warnings with cmake 4.1 (#2774)

---
+++ cmake/Findnvpl.cmake
@@ -0,0 +1,3 @@
+# This file does nothing but to suppress the cmake warning: "By not providing
+# Findnvpl.cmake in CMAKE_MODULE_PATH...", which is caused by the
+# find_package(nvpl) from cmake's builtin FindLAPACK.cmake module.
