bf8502c630d7383ba1024ac992252d56ad131407
Cheng <git@zcbenz.com>
2026-01-27 23:32:12 +0000

    Fallback to pinned host memory when managed memory is not supported

--- mlx/backend/cuda/fence.cpp
+++ mlx/backend/cuda/fence.cpp
@@ -29,15 +29,9 @@ void Fence::update(Stream s, const array& a, bool cross_device) {
     auto& cbuf =
         *static_cast<cu::CudaBuffer*>(const_cast<array&>(a).buffer().ptr());
     if (cbuf.device != -1) {
-      void* new_data;
-      CHECK_CUDA_ERROR(cudaMallocManaged(&new_data, cbuf.size));
-      cbuf.device = -1;
-      auto& encoder = cu::device(s.device).get_command_encoder(s);
+      auto& encoder = cu::get_command_encoder(s);
       encoder.commit();
-      CHECK_CUDA_ERROR(cudaMemcpyAsync(
-          new_data, cbuf.data, cbuf.size, cudaMemcpyDefault, encoder.stream()));
-      CHECK_CUDA_ERROR(cudaFreeAsync(cbuf.data, encoder.stream()));
-      cbuf.data = new_data;
+      cu::allocator().move_to_unified_memory(cbuf, encoder.stream());
     }
   }
   fence->count++;
