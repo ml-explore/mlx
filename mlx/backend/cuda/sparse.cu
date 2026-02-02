3a2028033d29fbe5512dde6ee851b7ad1d7f9829
mercush <barba@mit.edu>
2026-02-01 22:16:42 -0500

    fix cuda bug

--- mlx/backend/cuda/sparse.cu
+++ mlx/backend/cuda/sparse.cu
@@ -24,26 +24,52 @@ __global__ void sparse_matmul_csr_kernel(
     int n_rows,
     int n_cols,
     int dense_b_cols) {
-  int row = blockIdx.x;
+  constexpr int BM = 4;
 
-  if (row >= n_rows) {
+  int row = blockIdx.y;
+  int col_idx = (blockIdx.x * BLOCK_SIZE + threadIdx.x) * BM;
+
+  if (row >= n_rows || col_idx >= n_cols)
     return;
-  }
 
   int row_start = row_ptr[row];
   int row_end = row_ptr[row + 1];
 
-  for (int col = threadIdx.x; col < n_cols; col += BLOCK_SIZE) {
-    T sum = 0;
+  bool full_vector = (col_idx + BM <= n_cols);
 
+  float sum[BM] = {0.0f};
+
+  if (full_vector) {
     for (int idx = row_start; idx < row_end; idx++) {
       int k = col_indices[idx];
-      T a_val = values[idx];
-      T b_val = dense_b[k * dense_b_cols + col];
-      sum += a_val * b_val;
+      float val_a = static_cast<float>(values[idx]);
+
+      const T* src = dense_b + k * dense_b_cols + col_idx;
+#pragma unroll
+      for (int i = 0; i < BM; i++) {
+        sum[i] += val_a * static_cast<float>(src[i]);
+      }
     }
 
-    out[row * n_cols + col] = sum;
+    T* dst = out + row * n_cols + col_idx;
+#pragma unroll
+    for (int i = 0; i < BM; i++) {
+      dst[i] = static_cast<T>(sum[i]);
+    }
+  } else {
+    for (int idx = row_start; idx < row_end; idx++) {
+      int k = col_indices[idx];
+      float val_a = static_cast<float>(values[idx]);
+
+      for (int i = 0; i < n_cols - col_idx; i++) {
+        sum[i] +=
+            val_a * static_cast<float>(dense_b[k * dense_b_cols + col_idx + i]);
+      }
+    }
+
+    for (int i = 0; i < n_cols - col_idx; i++) {
+      out[row * n_cols + col_idx + i] = static_cast<T>(sum[i]);
+    }
   }
 }
 
@@ -61,7 +87,7 @@ void SparseMatmulCSR::eval_gpu(const std::vector<array>& inputs, array& out) {
   auto& s = stream();
   auto& encoder = cu::get_command_encoder(s);
 
-  out.set_data(allocator::malloc(out.nbytes()));
+  out.set_data(cu::malloc_async(out.nbytes(), encoder));
 
   encoder.set_input_array(row_ptr);
   encoder.set_input_array(col_indices);
@@ -75,8 +101,9 @@ void SparseMatmulCSR::eval_gpu(const std::vector<array>& inputs, array& out) {
     using DataType = cuda_type_t<MLX_GET_TYPE(type_tag)>;
 
     constexpr int BLOCK_SIZE = 256;
+    constexpr int BM = 4;
     dim3 block_dim(BLOCK_SIZE);
-    dim3 grid_dim(n_rows_);
+    dim3 grid_dim((n_cols_ + BM * BLOCK_SIZE - 1) / (BM * BLOCK_SIZE), n_rows_);
 
     auto kernel = cu::sparse_matmul_csr_kernel<DataType, BLOCK_SIZE>;
 
@@ -85,11 +112,11 @@ void SparseMatmulCSR::eval_gpu(const std::vector<array>& inputs, array& out) {
         grid_dim,
         block_dim,
         0,
-        row_ptr.data<int>(),
-        col_indices.data<int>(),
-        values.data<DataType>(),
-        dense_b.data<DataType>(),
-        out.data<DataType>(),
+        gpu_ptr<int>(row_ptr),
+        gpu_ptr<int>(col_indices),
+        gpu_ptr<DataType>(values),
+        gpu_ptr<DataType>(dense_b),
+        gpu_ptr<DataType>(out),
         n_rows_,
         n_cols_,
         dense_b_cols);
