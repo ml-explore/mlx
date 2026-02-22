49d11b64adda995571c8a7873d0d07ff4218bc44
Willem Adnet <willem.adnet@grenoble-inp.org>
2026-02-22 12:28:47 +0100

    feat: adding the bartlett function

--- python/src/ops.cpp
+++ python/src/ops.cpp
@@ -1429,6 +1429,28 @@ void init_ops(nb::module_& m) {
       nb::sig(
           "def arange(stop : Union[int, float], step : Union[None, int, float] = None, dtype: Optional[Dtype] = None, *, stream: Union[None, Stream, Device] = None) -> array"));
   m.def(
+      "bartlett",
+      &mlx::core::bartlett,
+      "M"_a,
+      nb::kw_only(),
+      "stream"_a = nb::none(),
+      R"pbdoc(
+        Return the Bartlett window.
+        
+        The Bartlett window is a taper formed by using a weighted cosine.
+
+        .. math::
+          w(n) = 1 - \frac{2|n - (M-1)/2|}{M-1}
+           \qquad 0 \le n \le M-1
+        
+        Args:
+            M (int): Number of points in the output window.
+            
+        Returns:
+            array: The window, with the maximum value normalized to one (the value one
+                   appears only if the number of samples is odd).
+    )pbdoc");
+  m.def(
       "hanning",
       &mlx::core::hanning,
       "M"_a,
