f1d123390ace520664d5d71b7edf436185e1dda0
Cheng <git@zcbenz.com>
2026-02-26 15:09:57 -0800

    [CUDA] Quantized GEMV

--- mlx/backend/cuda/quantized/qmm/qmm.h
+++ mlx/backend/cuda/quantized/qmm/qmm.h
@@ -3,10 +3,23 @@
 #pragma once
 
 #include "mlx/backend/cuda/device.h"
+#include "mlx/primitives.h"
 
 #include <optional>
 
 namespace mlx::core {
+
+bool supports_qmm_sm90(
+    const array& x,
+    const array& w,
+    const array& scales,
+    const std::optional<array>& biases,
+    const array& out,
+    bool transpose,
+    int bits,
+    int group_size,
+    QuantizationMode mode,
+    cu::Device& device);
 
 void qmm_sm90(
     const array& x,
@@ -18,5 +31,50 @@ void qmm_sm90(
     int group_size,
     cu::CommandEncoder& encoder,
     Stream s);
+
+bool supports_fp_qmv(
+    const array& x,
+    const array& w,
+    const array& scales,
+    const std::optional<array>& biases,
+    const array& out,
+    bool transpose,
+    int bits,
+    int group_size,
+    QuantizationMode mode,
+    cu::Device& device);
+
+void fp_qmv(
+    const array& x,
+    const array& w,
+    const array& scales,
+    array& out,
+    int bits,
+    int group_size,
+    cu::CommandEncoder& encoder,
+    Stream s);
+
+bool supports_qmv(
+    const array& x,
+    const array& w,
+    const array& scales,
+    const std::optional<array>& biases,
+    const array& out,
+    bool transpose,
+    int bits,
+    int group_size,
+    QuantizationMode mode,
+    cu::Device& device);
+
+void qmv(
+    const array& x,
+    const array& w,
+    const array& scales,
+    const std::optional<array>& biases,
+    array& out,
+    int bits,
+    int group_size,
+    QuantizationMode mode,
+    cu::CommandEncoder& encoder);
 
 } // namespace mlx::core
