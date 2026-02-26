d25b7a43cba855a1e50ae7233a2ad7540858de15
Angelos Katharopoulos <a_katharopoulos@apple.com>
2026-02-25 17:10:42 -0800

    Refactor the mesh algorithms outside the mesh group

--- mlx/distributed/jaccl/mesh.cpp
+++ mlx/distributed/jaccl/mesh.cpp
@@ -5,8 +5,6 @@
 #include "mlx/distributed/reduction_ops.h"
 #include "mlx/dtype_utils.h"
 
-constexpr int MAX_PEERS = 8;
-
 namespace mlx::core::distributed::jaccl {
 
 MeshGroup::MeshGroup(
@@ -17,9 +15,9 @@ MeshGroup::MeshGroup(
       size_(device_names.size()),
       side_channel_(rank_, size_, coordinator_addr),
       connections_(create_connections(device_names)) {
-  if (size_ > MAX_PEERS) {
+  if (size_ > MESH_MAX_PEERS) {
     std::ostringstream msg;
-    msg << "[jaccl] The JACCL mesh supports up to " << MAX_PEERS
+    msg << "[jaccl] The JACCL mesh supports up to " << MESH_MAX_PEERS
         << " peers but " << size_ << " were provided.";
     throw std::runtime_error(msg.str());
   }
@@ -29,6 +27,9 @@ MeshGroup::MeshGroup(
 
   // Make sure every node has reached here before continuing
   side_channel_.all_gather<int>(0);
+
+  // Create the mesh implementation object
+  mesh_ = MeshImpl(rank_, size_, connections_, buffers_);
 }
 
 void MeshGroup::initialize() {
@@ -139,83 +140,7 @@ void MeshGroup::all_gather(const array& input, array& output, Stream stream) {
   encoder.set_input_array(input);
   encoder.set_output_array(output);
   encoder.dispatch([in_ptr, out_ptr, n_bytes, this]() {
-    // Copy our data to the appropriate place
-    std::memcpy(out_ptr + rank_ * n_bytes, in_ptr, n_bytes);
-
-    // Fully connected all gather
-    char* data = out_ptr;
-    char* our_data = out_ptr + rank_ * n_bytes;
-    auto [sz, N] = buffer_size_from_message(n_bytes);
-    constexpr int PIPELINE = 2;
-    constexpr int WC_NUM = PIPELINE * MAX_PEERS * 2;
-    int64_t total = static_cast<int64_t>(n_bytes);
-    int num_peers = size_ - 1;
-
-    // Counters to maintain the state of transfers
-    int in_flight = 0;
-    int read_offset = 0;
-    int completed_send_count[PIPELINE] = {0};
-    int write_offset[MAX_PEERS] = {0};
-
-    // Prefill the pipeline
-    int buff = 0;
-    while (read_offset < total && buff < PIPELINE) {
-      post_recv_all(sz, buff);
-      std::copy(
-          our_data + read_offset,
-          our_data + std::min(read_offset + N, total),
-          send_buffer(sz, buff).begin<char>());
-      post_send_all(sz, buff);
-
-      buff++;
-      in_flight += 2 * num_peers;
-      read_offset += N;
-    }
-
-    // Main loop
-    //
-    // Keep going until we have no longer data in flight.
-    while (in_flight > 0) {
-      ibv_wc wc[WC_NUM];
-      int n = poll(connections_, WC_NUM, wc);
-      for (int i = 0; i < n; i++) {
-        int work_type = wc[i].wr_id >> 16;
-        int buff = (wc[i].wr_id >> 8) & 0xff;
-        int rank = wc[i].wr_id & 0xff;
-
-        in_flight--;
-
-        // Send completed. If all sends completed then send the next chunk.
-        if (work_type == SEND_WR && read_offset < total) {
-          completed_send_count[buff]++;
-          if (completed_send_count[buff] == num_peers) {
-            std::copy(
-                our_data + read_offset,
-                our_data + std::min(read_offset + N, total),
-                send_buffer(sz, buff).begin<char>());
-            post_send_all(sz, buff);
-
-            completed_send_count[buff] = 0;
-            in_flight += num_peers;
-            read_offset += N;
-          }
-        }
-
-        // Recv completed. If we have more chunks then post another recv.
-        else if (work_type == RECV_WR) {
-          std::copy(
-              recv_buffer(sz, buff, rank).begin<char>(),
-              recv_buffer(sz, buff, rank).begin<char>() +
-                  std::min(N, total - write_offset[rank]),
-              data + rank * n_bytes + write_offset[rank]);
-          write_offset[rank] += N;
-          if (write_offset[rank] + N * (PIPELINE - 1) < total) {
-            recv_from(sz, rank, buff);
-            in_flight++;
-          }
-        }
-      }
-    }
+    mesh_.all_gather(in_ptr, out_ptr, n_bytes);
   });
 }
 
@@ -224,55 +149,8 @@ void MeshGroup::send(const array& input, int dst, Stream stream) {
   int64_t n_bytes = input.nbytes();
   auto& encoder = cpu::get_command_encoder(stream);
   encoder.set_input_array(input);
-  encoder.dispatch([data, n_bytes, dst, this]() {
-    constexpr int PIPELINE = 2;
-    constexpr int WC_NUM = PIPELINE;
-    auto [sz, N] = buffer_size_from_message(n_bytes);
-
-    int in_flight = 0;
-    int64_t read_offset = 0;
-
-    // Prefill the pipeline
-    int buff = 0;
-    while (read_offset < n_bytes && buff < PIPELINE) {
-      std::copy(
-          data + read_offset,
-          data + std::min(read_offset + N, n_bytes),
-          send_buffer(sz, buff).begin<char>());
-      send_to(sz, dst, buff);
-
-      buff++;
-      read_offset += N;
-      in_flight++;
-    }
-
-    // Main loop
-    while (in_flight > 0) {
-      // Poll the hardware for completions.
-      //
-      // If a send was completed and we have more data to send then go ahead
-      // and send them.
-      ibv_wc wc[WC_NUM];
-      int n = connections_[dst].poll(WC_NUM, wc);
-      for (int i = 0; i < n; i++) {
-        int buff = (wc[i].wr_id >> 8) & 0xff;
-        int rank = wc[i].wr_id & 0xff;
-
-        in_flight--;
-
-        if (read_offset < n_bytes) {
-          std::copy(
-              data + read_offset,
-              data + std::min(read_offset + N, n_bytes),
-              send_buffer(sz, buff).begin<char>());
-          send_to(sz, dst, buff);
-
-          read_offset += N;
-          in_flight++;
-        }
-      }
-    }
-  });
+  encoder.dispatch(
+      [data, n_bytes, dst, this]() { mesh_.send(data, n_bytes, dst); });
 }
 
 void MeshGroup::recv(array& out, int src, Stream stream) {
@@ -280,52 +158,8 @@ void MeshGroup::recv(array& out, int src, Stream stream) {
   int64_t n_bytes = out.nbytes();
   auto& encoder = cpu::get_command_encoder(stream);
   encoder.set_output_array(out);
-  encoder.dispatch([data, n_bytes, src, this]() {
-    constexpr int PIPELINE = 2;
-    constexpr int WC_NUM = PIPELINE;
-    auto [sz, N] = buffer_size_from_message(n_bytes);
-
-    int in_flight = 0;
-    int64_t write_offset = 0;
-
-    // Prefill the pipeline
-    int buff = 0;
-    while (N * buff < n_bytes && buff < PIPELINE) {
-      recv_from(sz, src, buff);
-
-      in_flight++;
-      buff++;
-    }
-
-    // Main loop
-    while (in_flight > 0) {
-      // Poll the hardware for completions.
-      //
-      // If a recv was completed copy it to the output and if we have more
-      // data to fetch post another recv.
-      ibv_wc wc[WC_NUM];
-      int n = connections_[src].poll(WC_NUM, wc);
-      for (int i = 0; i < n; i++) {
-        int buff = (wc[i].wr_id >> 8) & 0xff;
-        int rank = wc[i].wr_id & 0xff;
-
-        in_flight--;
-
-        std::copy(
-            recv_buffer(sz, buff, src).begin<char>(),
-            recv_buffer(sz, buff, src).begin<char>() +
-                std::min(n_bytes - write_offset, static_cast<int64_t>(N)),
-            data + write_offset);
-        write_offset += N;
-
-        if (write_offset + (PIPELINE - 1) * N < n_bytes) {
-          recv_from(sz, src, buff);
-
-          in_flight++;
-        }
-      }
-    }
-  });
+  encoder.dispatch(
+      [data, n_bytes, src, this]() { mesh_.recv(data, n_bytes, src); });
 }
 
 template <typename T, typename ReduceOp>
@@ -336,115 +170,12 @@ void MeshGroup::all_reduce(
     ReduceOp reduce_op) {
   auto in_ptr = input.data<T>();
   auto out_ptr = output.data<T>();
+  int64_t size = input.size();
   auto& encoder = cpu::get_command_encoder(stream);
   encoder.set_input_array(input);
   encoder.set_output_array(output);
-  encoder.dispatch([in_ptr, out_ptr, size = input.size(), this, reduce_op]() {
-    // If not inplace all reduce then copy the input to the output first
-    if (in_ptr != out_ptr) {
-      std::memcpy(out_ptr, in_ptr, size * sizeof(T));
-    }
-
-    // Fully connected all reduce
-    T* data = out_ptr;
-    auto [sz, buffer_size] = buffer_size_from_message(size * sizeof(T));
-    int64_t N = buffer_size / sizeof(T);
-    constexpr int PIPELINE = 2;
-    constexpr int WC_NUM = PIPELINE * MAX_PEERS * 2;
-    int64_t total = static_cast<int64_t>(size);
-    int num_peers = size_ - 1;
-
-    // Counters to maintain the state of transfers
-    int in_flight = 0;
-    int64_t read_offset = 0;
-    int completed_send_count[PIPELINE] = {0};
-    int completed_recv_begin[MAX_PEERS] = {0};
-    int completed_recv_end[MAX_PEERS] = {0};
-
-    // Prefill the pipeline
-    int buff = 0;
-    while (read_offset < total && buff < PIPELINE) {
-      post_recv_all(sz, buff);
-      std::copy(
-          data + read_offset,
-          data + std::min(read_offset + N, total),
-          send_buffer(sz, buff).begin<T>());
-      post_send_all(sz, buff);
-
-      buff++;
-      in_flight += 2 * num_peers;
-      read_offset += N;
-    }
-
-    // Main loop
-    //
-    // Keep going until we have no longer data in flight.
-    while (in_flight > 0) {
-      // Poll the hardware for completions.
-      //
-      // If a send was completed mark how many completions we have received
-      // for that buffer. If we have sent the buffer to all peers we can
-      // reuse the buffer so copy the next chunk of data and send it to all.
-      //
-      // If a receive is completed then advance the pointer of completed
-      // receives.
-      ibv_wc wc[WC_NUM];
-      int n = poll(connections_, WC_NUM, wc);
-      for (int i = 0; i < n; i++) {
-        int work_type = wc[i].wr_id >> 16;
-        int buff = (wc[i].wr_id >> 8) & 0xff;
-        int rank = wc[i].wr_id & 0xff;
-
-        in_flight--;
-
-        if (work_type == SEND_WR && read_offset < total) {
-          completed_send_count[buff]++;
-          if (completed_send_count[buff] == num_peers) {
-            std::copy(
-                data + read_offset,
-                data + std::min(read_offset + N, total),
-                send_buffer(sz, buff).begin<T>());
-            post_send_all(sz, buff);
-
-            completed_send_count[buff] = 0;
-            in_flight += num_peers;
-            read_offset += N;
-          }
-        }
-
-        else if (work_type == RECV_WR) {
-          completed_recv_end[rank]++;
-        }
-      }
-
-      // Process the completed recv
-      //
-      // For each rank we have a range of completed recv defined by a begin
-      // and end inclusive and exlusive in standard C++ fashion.
-      //
-      // When there is an unprocessed receive we first check if we have
-      // finished sending the write location. If so then we reduce in-place
-      // and then check if there is more to be received and post a recv.
-      for (int r = 0; r < size_; r++) {
-        int s = completed_recv_begin[r];
-        int e = completed_recv_end[r];
-        int w = s * N;
-        while (w < read_offset && e - s > 0) {
-          int buff = s % PIPELINE;
-          reduce_op(
-              recv_buffer(sz, buff, r).begin<T>(),
-              data + w,
-              std::min(N, total - w));
-          w += N;
-          s++;
-          if (w + (PIPELINE - 1) * N < total) {
-            recv_from(sz, r, buff);
-            in_flight++;
-          }
-        }
-        completed_recv_begin[r] = s;
-      }
-    }
+  encoder.dispatch([in_ptr, out_ptr, size, this, reduce_op]() {
+    mesh_.all_reduce(in_ptr, out_ptr, size, reduce_op);
   });
 }
 
