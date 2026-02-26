5c4abd2f067f1e896264bd0b65447bd8ba00ac41
Angelos Katharopoulos <a_katharopoulos@apple.com>
2026-02-26 13:56:19 -0800

    JACCL refactor and small update (#3174)

---
+++ mlx/distributed/jaccl/mesh_impl.h
@@ -0,0 +1,358 @@
+// Copyright © 2026 Apple Inc.
+
+#pragma once
+
+#include <span>
+
+#include "mlx/distributed/jaccl/utils.h"
+
+constexpr int MESH_MAX_PEERS = 8;
+
+namespace mlx::core::distributed::jaccl {
+
+class MeshImpl {
+ public:
+  MeshImpl(
+      int rank,
+      int size,
+      std::vector<Connection>& conns,
+      std::vector<SharedBuffer>& buffers)
+      : rank_(rank), size_(size), connections_(conns), buffers_(buffers) {}
+
+  MeshImpl() : rank_(0), size_(1) {}
+
+  template <typename T, typename ReduceOp>
+  void
+  all_reduce(const T* in_ptr, T* out_ptr, int64_t size, ReduceOp reduce_op) {
+    // If not inplace all reduce then copy the input to the output first
+    if (in_ptr != out_ptr) {
+      std::memcpy(out_ptr, in_ptr, size * sizeof(T));
+    }
+
+    // Fully connected all reduce
+    T* data = out_ptr;
+    auto [sz, buffer_size] = buffer_size_from_message(size * sizeof(T));
+    int64_t N = buffer_size / sizeof(T);
+    constexpr int PIPELINE = 2;
+    constexpr int WC_NUM = PIPELINE * MESH_MAX_PEERS * 2;
+    int64_t total = static_cast<int64_t>(size);
+    int num_peers = size_ - 1;
+
+    // Counters to maintain the state of transfers
+    int in_flight = 0;
+    int64_t read_offset = 0;
+    int completed_send_count[PIPELINE] = {0};
+    int completed_recv_begin[MESH_MAX_PEERS] = {0};
+    int completed_recv_end[MESH_MAX_PEERS] = {0};
+
+    // Prefill the pipeline
+    int buff = 0;
+    while (read_offset < total && buff < PIPELINE) {
+      post_recv_all(sz, buff);
+      std::copy(
+          data + read_offset,
+          data + std::min(read_offset + N, total),
+          send_buffer(sz, buff).begin<T>());
+      post_send_all(sz, buff);
+
+      buff++;
+      in_flight += 2 * num_peers;
+      read_offset += N;
+    }
+
+    // Main loop
+    //
+    // Keep going until we have no longer data in flight.
+    while (in_flight > 0) {
+      // Poll the hardware for completions.
+      //
+      // If a send was completed mark how many completions we have received
+      // for that buffer. If we have sent the buffer to all peers we can
+      // reuse the buffer so copy the next chunk of data and send it to all.
+      //
+      // If a receive is completed then advance the pointer of completed
+      // receives.
+      ibv_wc wc[WC_NUM];
+      int n = poll(connections_, WC_NUM, wc);
+      for (int i = 0; i < n; i++) {
+        int work_type = wc[i].wr_id >> 16;
+        int buff = (wc[i].wr_id >> 8) & 0xff;
+        int rank = wc[i].wr_id & 0xff;
+
+        in_flight--;
+
+        if (work_type == SEND_WR && read_offset < total) {
+          completed_send_count[buff]++;
+          if (completed_send_count[buff] == num_peers) {
+            std::copy(
+                data + read_offset,
+                data + std::min(read_offset + N, total),
+                send_buffer(sz, buff).begin<T>());
+            post_send_all(sz, buff);
+
+            completed_send_count[buff] = 0;
+            in_flight += num_peers;
+            read_offset += N;
+          }
+        }
+
+        else if (work_type == RECV_WR) {
+          completed_recv_end[rank]++;
+        }
+      }
+
+      // Process the completed recv
+      //
+      // For each rank we have a range of completed recv defined by a begin
+      // and end inclusive and exlusive in standard C++ fashion.
+      //
+      // When there is an unprocessed receive we first check if we have
+      // finished sending the write location. If so then we reduce in-place
+      // and then check if there is more to be received and post a recv.
+      for (int r = 0; r < size_; r++) {
+        int s = completed_recv_begin[r];
+        int e = completed_recv_end[r];
+        int w = s * N;
+        while (w < read_offset && e - s > 0) {
+          int buff = s % PIPELINE;
+          reduce_op(
+              recv_buffer(sz, buff, r).begin<T>(),
+              data + w,
+              std::min(N, total - w));
+          w += N;
+          s++;
+          if (w + (PIPELINE - 1) * N < total) {
+            recv_from(sz, r, buff);
+            in_flight++;
+          }
+        }
+        completed_recv_begin[r] = s;
+      }
+    }
+  }
+
+  void all_gather(const char* in_ptr, char* out_ptr, int64_t n_bytes) {
+    // Copy our data to the appropriate place
+    std::memcpy(out_ptr + rank_ * n_bytes, in_ptr, n_bytes);
+
+    // Fully connected all gather
+    char* data = out_ptr;
+    char* our_data = out_ptr + rank_ * n_bytes;
+    auto [sz, N] = buffer_size_from_message(n_bytes);
+    constexpr int PIPELINE = 2;
+    constexpr int WC_NUM = PIPELINE * MESH_MAX_PEERS * 2;
+    int64_t total = static_cast<int64_t>(n_bytes);
+    int num_peers = size_ - 1;
+
+    // Counters to maintain the state of transfers
+    int in_flight = 0;
+    int read_offset = 0;
+    int completed_send_count[PIPELINE] = {0};
+    int write_offset[MESH_MAX_PEERS] = {0};
+
+    // Prefill the pipeline
+    int buff = 0;
+    while (read_offset < total && buff < PIPELINE) {
+      post_recv_all(sz, buff);
+      std::copy(
+          our_data + read_offset,
+          our_data + std::min(read_offset + N, total),
+          send_buffer(sz, buff).begin<char>());
+      post_send_all(sz, buff);
+
+      buff++;
+      in_flight += 2 * num_peers;
+      read_offset += N;
+    }
+
+    // Main loop
+    //
+    // Keep going until we have no longer data in flight.
+    while (in_flight > 0) {
+      ibv_wc wc[WC_NUM];
+      int n = poll(connections_, WC_NUM, wc);
+      for (int i = 0; i < n; i++) {
+        int work_type = wc[i].wr_id >> 16;
+        int buff = (wc[i].wr_id >> 8) & 0xff;
+        int rank = wc[i].wr_id & 0xff;
+
+        in_flight--;
+
+        // Send completed. If all sends completed then send the next chunk.
+        if (work_type == SEND_WR && read_offset < total) {
+          completed_send_count[buff]++;
+          if (completed_send_count[buff] == num_peers) {
+            std::copy(
+                our_data + read_offset,
+                our_data + std::min(read_offset + N, total),
+                send_buffer(sz, buff).begin<char>());
+            post_send_all(sz, buff);
+
+            completed_send_count[buff] = 0;
+            in_flight += num_peers;
+            read_offset += N;
+          }
+        }
+
+        // Recv completed. If we have more chunks then post another recv.
+        else if (work_type == RECV_WR) {
+          std::copy(
+              recv_buffer(sz, buff, rank).begin<char>(),
+              recv_buffer(sz, buff, rank).begin<char>() +
+                  std::min(N, total - write_offset[rank]),
+              data + rank * n_bytes + write_offset[rank]);
+          write_offset[rank] += N;
+          if (write_offset[rank] + N * (PIPELINE - 1) < total) {
+            recv_from(sz, rank, buff);
+            in_flight++;
+          }
+        }
+      }
+    }
+  }
+
+  void send(const char* in_ptr, int64_t n_bytes, int dst) {
+    constexpr int PIPELINE = 2;
+    constexpr int WC_NUM = PIPELINE;
+    auto [sz, N] = buffer_size_from_message(n_bytes);
+
+    int in_flight = 0;
+    int64_t read_offset = 0;
+
+    // Prefill the pipeline
+    int buff = 0;
+    while (read_offset < n_bytes && buff < PIPELINE) {
+      std::copy(
+          in_ptr + read_offset,
+          in_ptr + std::min(read_offset + N, n_bytes),
+          send_buffer(sz, buff).begin<char>());
+      send_to(sz, dst, buff);
+
+      buff++;
+      read_offset += N;
+      in_flight++;
+    }
+
+    // Main loop
+    while (in_flight > 0) {
+      // Poll the hardware for completions.
+      //
+      // If a send was completed and we have more data to send then go ahead
+      // and send them.
+      ibv_wc wc[WC_NUM];
+      int n = connections_[dst].poll(WC_NUM, wc);
+      for (int i = 0; i < n; i++) {
+        int buff = (wc[i].wr_id >> 8) & 0xff;
+        int rank = wc[i].wr_id & 0xff;
+
+        in_flight--;
+
+        if (read_offset < n_bytes) {
+          std::copy(
+              in_ptr + read_offset,
+              in_ptr + std::min(read_offset + N, n_bytes),
+              send_buffer(sz, buff).begin<char>());
+          send_to(sz, dst, buff);
+
+          read_offset += N;
+          in_flight++;
+        }
+      }
+    }
+  }
+
+  void recv(char* out_ptr, int64_t n_bytes, int src) {
+    constexpr int PIPELINE = 2;
+    constexpr int WC_NUM = PIPELINE;
+    auto [sz, N] = buffer_size_from_message(n_bytes);
+
+    int in_flight = 0;
+    int64_t write_offset = 0;
+
+    // Prefill the pipeline
+    int buff = 0;
+    while (N * buff < n_bytes && buff < PIPELINE) {
+      recv_from(sz, src, buff);
+
+      in_flight++;
+      buff++;
+    }
+
+    // Main loop
+    while (in_flight > 0) {
+      // Poll the hardware for completions.
+      //
+      // If a recv was completed copy it to the output and if we have more
+      // data to fetch post another recv.
+      ibv_wc wc[WC_NUM];
+      int n = connections_[src].poll(WC_NUM, wc);
+      for (int i = 0; i < n; i++) {
+        int buff = (wc[i].wr_id >> 8) & 0xff;
+        int rank = wc[i].wr_id & 0xff;
+
+        in_flight--;
+
+        std::copy(
+            recv_buffer(sz, buff, src).begin<char>(),
+            recv_buffer(sz, buff, src).begin<char>() +
+                std::min(n_bytes - write_offset, static_cast<int64_t>(N)),
+            out_ptr + write_offset);
+        write_offset += N;
+
+        if (write_offset + (PIPELINE - 1) * N < n_bytes) {
+          recv_from(sz, src, buff);
+
+          in_flight++;
+        }
+      }
+    }
+  }
+
+ private:
+  void send_to(int sz, int rank, int buff) {
+    connections_[rank].post_send(
+        send_buffer(sz, buff), SEND_WR << 16 | buff << 8 | rank);
+  }
+
+  void recv_from(int sz, int rank, int buff) {
+    connections_[rank].post_recv(
+        recv_buffer(sz, buff, rank), RECV_WR << 16 | buff << 8 | rank);
+  }
+
+  SharedBuffer& send_buffer(int sz, int buff) {
+    return buffers_[sz * NUM_BUFFERS * size_ + buff * size_ + rank_];
+  }
+
+  SharedBuffer& recv_buffer(int sz, int buff, int rank) {
+    return buffers_[sz * NUM_BUFFERS * size_ + buff * size_ + rank];
+  }
+
+  void post_send_all(int sz, int buff) {
+    auto& b = send_buffer(sz, buff);
+    int wr_id = SEND_WR << 16 | buff << 8;
+    for (int i = 0; i < size_; i++) {
+      if (i == rank_) {
+        continue;
+      }
+      connections_[i].post_send(b, wr_id | i);
+    }
+  }
+
+  void post_recv_all(int sz, int buff) {
+    int b = sz * NUM_BUFFERS * size_ + buff * size_;
+    int wr_id = RECV_WR << 16 | buff << 8;
+    for (int i = 0; i < size_; i++) {
+      if (i == rank_) {
+        continue;
+      }
+      connections_[i].post_recv(buffers_[b + i], wr_id | i);
+    }
+  }
+
+  int rank_;
+  int size_;
+  std::span<Connection> connections_;
+  std::span<SharedBuffer> buffers_;
+};
+
+} // namespace mlx::core::distributed::jaccl
--- mlx/distributed/jaccl/ring.h
+++ mlx/distributed/jaccl/ring.h
@@ -3,11 +3,10 @@
 #pragma once
 
 #include "mlx/distributed/distributed_impl.h"
+#include "mlx/distributed/jaccl/ring_impl.h"
 #include "mlx/distributed/jaccl/utils.h"
 
 using GroupImpl = mlx::core::distributed::detail::GroupImpl;
-
-constexpr int MAX_CONNS = 4;
 
 namespace mlx::core::distributed::jaccl {
 
@@ -64,14 +63,6 @@ class RingGroup : public GroupImpl {
       Stream stream,
       ReduceOp reduce_op);
 
-  template <int MAX_DIR, typename T, typename ReduceOp>
-  void all_reduce_impl(
-      const T* in_ptr,
-      T* out_ptr,
-      int64_t size,
-      int n_wires,
-      ReduceOp reduce_op);
-
   /**
    * Performs the connection initialization. Namely, after this call all
    * Connection objects should have a queue pair in RTS state and all buffers
@@ -84,95 +75,15 @@ class RingGroup : public GroupImpl {
    */
   void allocate_buffers();
 
-  void send_to(int sz, int buff, int left_right, int wire) {
-    if (left_right) {
-      left_[wire].post_send(
-          send_buffer_left(sz, buff, wire),
-          SEND_WR << 16 | buff << 8 | (MAX_CONNS + wire));
-    } else {
-      right_[wire].post_send(
-          send_buffer_right(sz, buff, wire), SEND_WR << 16 | buff << 8 | wire);
-    }
-  }
-
-  void recv_from(int sz, int buff, int left_right, int wire) {
-    if (left_right) {
-      right_[wire].post_recv(
-          recv_buffer_right(sz, buff, wire),
-          RECV_WR << 16 | buff << 8 | (MAX_CONNS + wire));
-    } else {
-      left_[wire].post_recv(
-          recv_buffer_left(sz, buff, wire), RECV_WR << 16 | buff << 8 | wire);
-    }
-  }
-
-  SharedBuffer& send_buffer_right(int sz, int buff, int wire) {
-    return send_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 + wire];
-  }
-
-  SharedBuffer& send_buffer_left(int sz, int buff, int wire) {
-    return send_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 + MAX_CONNS +
-         wire];
-  }
-
-  SharedBuffer& send_buffer(int sz, int buff, int left_right, int wire) {
-    return send_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 +
-         left_right * MAX_CONNS + wire];
-  }
-
-  SharedBuffer& recv_buffer_left(int sz, int buff, int wire) {
-    return recv_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 + wire];
-  }
-
-  SharedBuffer& recv_buffer_right(int sz, int buff, int wire) {
-    return recv_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 + MAX_CONNS +
-         wire];
-  }
-
-  SharedBuffer& recv_buffer(int sz, int buff, int left_right, int wire) {
-    return recv_buffers_
-        [sz * NUM_BUFFERS * MAX_CONNS * 2 + buff * MAX_CONNS * 2 +
-         left_right * MAX_CONNS + wire];
-  }
-
-  template <int MAX_DIR>
-  void post_recv_all(int sz, int buff, int n_wires) {
-    for (int lr = 0; lr < MAX_DIR; lr++) {
-      for (int lw = 0; lw < n_wires; lw++) {
-        recv_from(sz, buff, lr, lw);
-      }
-    }
-  }
-
-  void post_recv_all(int sz, int buff) {
-    post_recv_all<2>(sz, buff, left_.size());
-  }
-
-  template <int MAX_DIR>
-  void post_send_all(int sz, int buff, int n_wires) {
-    for (int lr = 0; lr < MAX_DIR; lr++) {
-      for (int lw = 0; lw < n_wires; lw++) {
-        send_to(sz, buff, lr, lw);
-      }
-    }
-  }
-
-  void post_send_all(int sz, int buff) {
-    post_send_all<2>(sz, buff, left_.size());
-  }
-
   int rank_;
   int size_;
+  int n_conns_;
   SideChannel side_channel_;
   std::vector<Connection> left_;
   std::vector<Connection> right_;
   std::vector<SharedBuffer> send_buffers_;
   std::vector<SharedBuffer> recv_buffers_;
+  RingImpl ring_;
 };
 
 } // namespace mlx::core::distributed::jaccl
