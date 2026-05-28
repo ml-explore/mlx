7af002f8d91b31d47cf666294c336374c0c28ec8
Angelos Katharopoulos <a_katharopoulos@apple.com>
2026-05-28 14:44:22 -0700

    Remove blank lines

--- mlx/primitives.h
+++ mlx/primitives.h
@@ -1701,9 +1701,7 @@ class GatherQMM : public UnaryPrimitive {
   DEFINE_GRADS()
   DEFINE_NAME(GatherQMM)
   bool is_equivalent(const Primitive& other) const override;
-
   std::vector<Shape> output_shapes(const std::vector<array>& inputs) override;
-
   auto state() const {
     return std::make_tuple(
         group_size_, bits_, mode_, transpose_, left_sorted_, right_sorted_);
