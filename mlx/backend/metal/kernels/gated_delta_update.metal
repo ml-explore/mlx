b6248b24d1c742db5a6aa394a2f4b3f796e43ff2
tpegolotti <tommaso.pegolotti@hotmail.com>
2026-06-11 12:10:46 +0200

    copied implementation over from mlx_lm as baseline

--- mlx/backend/metal/kernels/gated_delta_update.metal
+++ mlx/backend/metal/kernels/gated_delta_update.metal
@@ -1,19 +1,20 @@
-// mlx/backend/metal/kernels/gated_delta_update.metal
-
 #include "mlx/backend/metal/kernels/utils.h"
 #include "mlx/backend/metal/kernels/gated_delta_update_impl.h"
 
 using namespace metal;
 
-#define instantiate_gated_delta_update(type, dk, dv)          \
-  instantiate_kernel(                                          \
-      "gated_delta_update_fwd_" #type "_" #dk "_" #dv,        \
-      gated_delta_update_fwd,                                  \
-      type,                                                    \
-      dk,                                                      \
-      dv)
+#define instantiate_gated_delta_update(in_type, st_type, dk, dv, hk, hv)  \
+  instantiate_kernel(                                                        \
+      "gated_delta_step_" #in_type "_" #st_type "_" #dk "_" #dv "_" #hk "_" #hv, \
+      gated_delta_step,                                                      \
+      in_type,                                                               \
+      st_type,                                                               \
+      dk,                                                                    \
+      dv,                                                                    \
+      hk,                                                                    \
+      hv)
 
-#define instantiate_gated_delta_update_dims(type) \
-  instantiate_gated_delta_update(type, 64,  64)  
+#define instantiate_gated_delta_update_dims(in_type, st_type) \
+  instantiate_gated_delta_update(in_type, st_type, 64, 64, 4, 4)
 
-instantiate_gated_delta_update_dims(float)
+instantiate_gated_delta_update_dims(float, float)
--- mlx/backend/metal/kernels/gated_delta_update_impl.h
+++ mlx/backend/metal/kernels/gated_delta_update_impl.h
@@ -5,20 +5,84 @@
 
 using namespace metal;
 
-template <typename T, int Dk, int Dv>
-[[kernel]] void gated_delta_update_fwd(
-    const device T* q        [[buffer(0)]],
-    const device T* k        [[buffer(1)]],
-    const device T* v        [[buffer(2)]],
-    const device T* g        [[buffer(3)]],
-    const device T* beta     [[buffer(4)]],
-    const device T* h0       [[buffer(5)]],
-          device T* out      [[buffer(6)]],
-          device T* hf       [[buffer(7)]],
-    constant int& B          [[buffer(8)]],
-    constant int& H          [[buffer(9)]],
-    constant int& T_len      [[buffer(10)]],
-    uint3 tid                [[thread_position_in_grid]]
+template <typename InT, typename StT, int Dk, int Dv, int Hk, int Hv>
+[[kernel]] void gated_delta_step(
+    const device InT* q          					[[buffer(0)]],
+    const device InT* k          					[[buffer(1)]],
+    const device InT* v          					[[buffer(2)]],
+    const device InT* g          					[[buffer(3)]],   // [B, T, Hv] or [B, T, Hv, Dk]
+    const device InT* beta      					[[buffer(4)]],   // [B, T, Hv]
+    const device StT* state_in   					[[buffer(5)]],   // [B, Hv, Dv, Dk]
+    constant int& T              					[[buffer(6)]],
+    device InT* y                					[[buffer(7)]],   // [B, T, Hv, Dv]
+    device StT* state_out        					[[buffer(8)]],   // [B, Hv, Dv, Dk]
+    uint3 thread_position_in_grid        	[[thread_position_in_grid]],
+    uint3 thread_position_in_threadgroup 	[[thread_position_in_threadgroup]],
+    uint  thread_index_in_simdgroup      	[[thread_index_in_simdgroup]]
 ) {
     // kernel implementation
+    auto n = thread_position_in_grid.z;
+		auto b_idx = n / Hv;
+		auto hv_idx = n % Hv;
+		auto hk_idx = hv_idx / (Hv / Hk);
+		constexpr int n_per_t = Dk / 32;
+
+		// q, k: [B, T, Hk, Dk]
+		auto q_ = q + b_idx * T * Hk * Dk + hk_idx * Dk;
+		auto k_ = k + b_idx * T * Hk * Dk + hk_idx * Dk;
+
+		// v, y: [B, T, Hv, Dv]
+		auto v_ = v + b_idx * T * Hv * Dv + hv_idx * Dv;
+		y += b_idx * T * Hv * Dv + hv_idx * Dv;
+
+		auto dk_idx = thread_position_in_threadgroup.x;
+		auto dv_idx = thread_position_in_grid.y;
+
+		// state_in, state_out: [B, Hv, Dv, Dk]
+		auto i_state = state_in + (n * Dv + dv_idx) * Dk;
+		auto o_state = state_out + (n * Dv + dv_idx) * Dk;
+
+		float state[n_per_t];
+		for (int i = 0; i < n_per_t; ++i) {
+			auto s_idx = n_per_t * dk_idx + i;
+			state[i] = static_cast<float>(i_state[s_idx]);
+		}
+
+		// g: [B, T, Hv]
+		auto g_ = g + b_idx * T * Hv;
+		auto beta_ = beta + b_idx * T * Hv;
+
+		for (int t = 0; t < T; ++t) {
+			float kv_mem = 0.0f;
+			for (int i = 0; i < n_per_t; ++i) {
+				auto s_idx = n_per_t * dk_idx + i;
+				state[i] = state[i] * g_[hv_idx];
+				kv_mem += state[i] * k_[s_idx];
+			}
+			kv_mem = simd_sum(kv_mem);
+
+			auto delta = (v_[dv_idx] - kv_mem) * beta_[hv_idx];
+
+			float out = 0.0f;
+			for (int i = 0; i < n_per_t; ++i) {
+				auto s_idx = n_per_t * dk_idx + i;
+				state[i] = state[i] + k_[s_idx] * delta;
+				out += state[i] * q_[s_idx];
+			}
+			out = simd_sum(out);
+			if (thread_index_in_simdgroup == 0) {
+				y[dv_idx] = static_cast<InT>(out);
+			}
+			// Increment data pointers to next time step
+			q_ += Hk * Dk;
+			k_ += Hk * Dk;
+			v_ += Hv * Dv;
+			y += Hv * Dv;
+			g_ += Hv;
+			beta_ += Hv;
+		}
+		for (int i = 0; i < n_per_t; ++i) {
+			auto s_idx = n_per_t * dk_idx + i;
+			o_state[s_idx] = static_cast<StT>(state[i]);
+		}
 }
