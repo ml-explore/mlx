5cbd03f56f101410483d44f6f10e982c52b4e389
tpegolotti <tommaso.pegolotti@hotmail.com>
2026-06-11 12:10:46 +0200

    copied implementation over from mlx_lm as baseline

--- mlx/backend/metal/gated_delta_update.cpp
+++ mlx/backend/metal/gated_delta_update.cpp
@@ -12,27 +12,6 @@
 
 namespace mlx::core::fast {
 
-namespace {
-
-void gated_delta_update_forward_metal(
-  const Stream& s,
-  metal::Device& d){
-			
-	std::string base_name = "gated_delta_update_fwd_float_64_64";
-  std::string hash_name = base_name;
-  metal::MTLFCList func_consts = {};
-
-  auto kernel = get_steel_gated_delta_forward_kernel(
-        d,
-        base_name,
-        hash_name,
-        func_consts);
-}
-    
-
-
-}
-
 bool GatedDeltaUpdate::use_fallback(Stream s) {
     // TODO: finish implementation
     return false;
@@ -54,12 +33,54 @@ void GatedDeltaUpdate::eval_gpu(
 
     auto& out = outputs[0];
     auto& hf  = outputs[1];
+    
+    int B  = q.shape(0);
+    int Hk = q.shape(1);		// key heads
+    int T  = q.shape(2);
+    int Dk = q.shape(3);
+    int Hv = v.shape(1);   	// value heads
+    int Dv = v.shape(3);
 
 		out.set_data(allocator::malloc(out.nbytes()));
     hf.set_data(allocator::malloc(hf.nbytes()));
+    
+		std::string base_name = "gated_delta_step_"
+				+ get_type_string(q.dtype())          // "float"
+				+ "_" + get_type_string(h0.dtype()) // "float"
+				+ "_" + std::to_string(Dk)
+				+ "_" + std::to_string(Dv)
+				+ "_" + std::to_string(Hk)
+				+ "_" + std::to_string(Hv);
+		// e.g. "gated_delta_step_float_float_64_64_4_4"
+		std::string hash_name = base_name;
+		metal::MTLFCList func_consts = {};
 
-    gated_delta_update_forward_metal(s,d);
-    // throw std::runtime_error("NYI");
+		auto kernel = get_steel_gated_delta_forward_kernel(
+					d,
+					base_name,
+					hash_name,
+					func_consts);
+    
+    auto& compute_encoder = metal::get_command_encoder(s);
+
+    compute_encoder.set_compute_pipeline_state(kernel);
+
+    compute_encoder.set_input_array(q,     0);
+    compute_encoder.set_input_array(k,     1);
+    compute_encoder.set_input_array(v,     2);
+    compute_encoder.set_input_array(g,     3);
+    compute_encoder.set_input_array(beta,  4);
+    compute_encoder.set_input_array(h0,		 5);
+    compute_encoder.set_bytes(T,           6);
+    compute_encoder.set_output_array(out,  7);
+    compute_encoder.set_output_array(hf,   8);
+
+    // auto grid   = MTL::Size(1, 1, 1);
+    // auto threads = MTL::Size(1, 1, 1);
+    auto grid   = MTL::Size(32, Dv, B * Hv);
+    auto threads = MTL::Size(32, 4, 1);
+    compute_encoder.dispatch_threads(grid, threads);
+		// throw std::runtime_error("NYI");
 }
 
 bool GatedDeltaUpdate::is_equivalent(const Primitive& other) const {
