568338a98ff2a55772b3c1920775ff8a3b27b14d
tpegolotti <tommaso.pegolotti@hotmail.com>
2026-06-22 17:23:35 +0200

    fused wy into gated computation

--- mlx/backend/metal/kernels/gated_delta_update_impl.h
+++ mlx/backend/metal/kernels/gated_delta_update_impl.h
@@ -13,6 +13,193 @@
 
 using namespace metal;
 
+#define AT(TILE, IDX) TILE.thread_elements()[IDX]
+
+template <typename InT, typename StT, int Dk, int Dv, int Hk, int Hv, int C>
+[[kernel]] void gated_delta_fused_chunk(
+    const device InT* q [[buffer(0)]],
+    const device InT* k [[buffer(1)]],
+    const device InT* v [[buffer(2)]],
+    const device StT* state_in [[buffer(3)]],
+    const device InT* g [[buffer(4)]],
+    const device InT* beta [[buffer(5)]],
+    device InT* y [[buffer(6)]],
+    device StT* state_out [[buffer(7)]],
+    constant int& T [[buffer(8)]],
+    uint3 thread_position_in_grid [[thread_position_in_grid]],
+    uint thread_index_in_simdgroup [[thread_index_in_simdgroup]]) {
+  auto n = thread_position_in_grid.z;
+  auto b_idx = n / Hv;
+  auto hv_idx = n % Hv;
+  auto hk_idx = hv_idx / (Hv / Hk);
+
+  const short qid = thread_index_in_simdgroup / 4;
+  const short fm = (qid & 4) + ((thread_index_in_simdgroup / 2) % 4);
+  const short fn = (qid & 2) * 2 + (thread_index_in_simdgroup % 2) * 2;
+
+  auto dv_idx = thread_position_in_grid.y * 8;
+
+  // set up pointers
+  // g: [B, T, Hv]
+  auto g_ = g + b_idx * T * Hv;
+
+  // q, k: [B, T, Hk, Dk]
+  auto q_ = q + b_idx * T * Hk * Dk + hk_idx * Dk;
+  auto k_ = k + b_idx * T * Hk * Dk + hk_idx * Dk;
+
+  // v, y: [B, T, Hv, Dv]
+  y += b_idx * T * Hv * Dv + hv_idx * Dv;
+  auto v_ = v + b_idx * T * Hv * Dv + hv_idx * Dv;
+  auto beta_ = beta + b_idx * T * Hv;
+
+  // state_in, state_out: [B, Hv, Dv, Dk]
+  auto i_state = state_in + (n * Dv + dv_idx) * Dk;
+  auto o_state = state_out + (n * Dv + dv_idx) * Dk;
+
+  simdgroup_float8x8 S_tile[Dk / 8];
+
+  // simdgroup matrices
+  simdgroup_float8x8 V_tile, K_tile, KT_tile, Q_tile;
+  simdgroup_float8x8 W_tile, U_tile;
+  simdgroup_float8x8 WS_tile;
+  simdgroup_float8x8 delta_tile;
+  simdgroup_float8x8 tmp_tile;
+  simdgroup_float8x8 QKt_tile;
+  simdgroup_float8x8 out_tile;
+  simdgroup_float8x8 KD_tile;
+
+  // tiles for WY form computation
+  simdgroup_float8x8 KKtK_tile, KKtV_tile, X_tile, KKt_tile;
+
+  threadgroup float gamma[C];
+
+  // load initial state into threadgroup
+  for (int kk = 0; kk < Dk; kk += 8) {
+    simdgroup_load(S_tile[kk / 8], i_state + kk, Dk, ulong2(0, 0), true);
+  }
+  threadgroup_barrier(mem_flags::mem_threadgroup);
+
+  for (int t = 0; t < T; t += C) {
+    float g_val = (thread_index_in_simdgroup < C)
+        ? g_[thread_index_in_simdgroup * Hv + hv_idx]
+        : 1.0f;
+
+    float gamma_val = simd_prefix_inclusive_product(g_val);
+
+    if (thread_index_in_simdgroup < C) {
+      gamma[thread_index_in_simdgroup] = gamma_val;
+    }
+
+    float beta_fm = beta_[fm * Hv + hv_idx];
+
+    KKt_tile = make_filled_simdgroup_matrix<float, 8>(0.f);
+    for (int kk = 0; kk < Dk; kk += 8) {
+      simdgroup_load(K_tile, k_ + kk, Dk * Hk);
+      simdgroup_load(KT_tile, k_ + kk, Dk * Hk, ulong2(0, 0), true);
+      simdgroup_multiply_accumulate(KKt_tile, K_tile, KT_tile, KKt_tile);
+    }
+
+    KKtK_tile = KKt_tile;
+    KKtV_tile = KKt_tile;
+
+    AT(KKtK_tile, 0) = fn >= fm ? 0.f : AT(KKtK_tile, 0) * beta_fm;
+    AT(KKtV_tile, 0) =
+        fn >= fm ? 0.f : AT(KKtV_tile, 0) * beta_fm * (gamma[fm] / gamma[fn]);
+
+    AT(KKtK_tile, 1) = fn + 1 >= fm ? 0.f : AT(KKtK_tile, 1) * beta_fm;
+    AT(KKtV_tile, 1) = fn + 1 >= fm
+        ? 0.f
+        : AT(KKtV_tile, 1) * beta_fm * (gamma[fm] / gamma[fn + 1]);
+
+    WS_tile = make_filled_simdgroup_matrix<float, 8>(0.f);
+    // Use the Neumann series: (I - T)^-1 = sum T^k, instead of doing forward
+    // substitution to compute W (and U). For C=8 this is 8 matmuls, for generic
+    // C this probably does not work well? Also fuse the WS in the same loop ->
+    // less memory movements.
+    for (int kk = 0; kk < Dk; kk += 8) {
+      simdgroup_load(K_tile, k_ + kk, Dk * Hk);
+      AT(K_tile, 0) *= beta_fm;
+      AT(K_tile, 1) *= beta_fm;
+      W_tile = K_tile;
+      for (int iter = 0; iter < C - 1; iter++) {
+        simdgroup_multiply(X_tile, KKtK_tile, W_tile);
+        AT(W_tile, 0) = AT(K_tile, 0) - AT(X_tile, 0);
+        AT(W_tile, 1) = AT(K_tile, 1) - AT(X_tile, 1);
+      }
+
+      AT(W_tile, 0) *= gamma[fm];
+      AT(W_tile, 1) *= gamma[fm];
+
+      //  WS = W_left @ S^T
+      simdgroup_multiply_accumulate(WS_tile, W_tile, S_tile[kk / 8], WS_tile);
+    }
+
+    simdgroup_load(V_tile, v_ + dv_idx, Dv * Hv);
+    AT(V_tile, 0) *= beta_fm;
+    AT(V_tile, 1) *= beta_fm;
+    U_tile = V_tile;
+    for (int iter = 0; iter < C - 1; iter++) {
+      simdgroup_multiply(X_tile, KKtV_tile, U_tile);
+      AT(U_tile, 0) = AT(V_tile, 0) - AT(X_tile, 0);
+      AT(U_tile, 1) = AT(V_tile, 1) - AT(X_tile, 1);
+    }
+
+    // delta = U - WS
+    AT(delta_tile, 0) = AT(U_tile, 0) - AT(WS_tile, 0);
+    AT(delta_tile, 1) = AT(U_tile, 1) - AT(WS_tile, 1);
+
+    // Q_left @ S^T
+    tmp_tile = make_filled_simdgroup_matrix<float, 8>(0.f);
+    QKt_tile = make_filled_simdgroup_matrix<float, 8>(0.f);
+
+    for (int kk = 0; kk < Dk; kk += 8) {
+      simdgroup_load(Q_tile, q_ + kk, Hk * Dk);
+      simdgroup_load(K_tile, k_ + kk, Hk * Dk, ulong2(0, 0), true);
+
+      AT(Q_tile, 0) *= gamma[fm];
+      AT(Q_tile, 1) *= gamma[fm];
+
+      simdgroup_multiply_accumulate(tmp_tile, Q_tile, S_tile[kk / 8], tmp_tile);
+      simdgroup_multiply_accumulate(QKt_tile, Q_tile, K_tile, QKt_tile);
+    }
+
+    // element 0 at (fm, fn), element 1 at (fm, fn+1)
+    AT(QKt_tile, 0) *= fn > fm ? 0.f : (1.0f / gamma[fn]);
+    AT(QKt_tile, 1) *= fn + 1 > fm ? 0.f : (1.0f / gamma[fn + 1]);
+
+    simdgroup_multiply_accumulate(out_tile, QKt_tile, delta_tile, tmp_tile);
+
+    y[fm * Hv * Dv + dv_idx + fn] = static_cast<InT>(AT(out_tile, 0));
+    y[fm * Hv * Dv + dv_idx + fn + 1] = static_cast<InT>(AT(out_tile, 1));
+
+    for (int kk = 0; kk < Dk; kk += 8) {
+      simdgroup_load(K_tile, k_ + kk, Hk * Dk, ulong2(0, 0), true);
+
+      AT(K_tile, 0) *= (gamma[C - 1] / gamma[fn]);
+      AT(K_tile, 1) *= (gamma[C - 1] / gamma[fn + 1]);
+
+      simdgroup_multiply(KD_tile, K_tile, delta_tile);
+
+      AT(S_tile[kk / 8], 0) =
+          gamma[C - 1] * AT(S_tile[kk / 8], 0) + AT(KD_tile, 0);
+      AT(S_tile[kk / 8], 1) =
+          gamma[C - 1] * AT(S_tile[kk / 8], 1) + AT(KD_tile, 1);
+    }
+
+    // advance pointers
+    q_ += C * Hk * Dk;
+    k_ += C * Hk * Dk;
+    v_ += C * Hv * Dv;
+    beta_ += C * Hv;
+    y += C * Hv * Dv;
+    g_ += C * Hv;
+  }
+
+  for (int kk = 0; kk < Dk; kk += 8) {
+    simdgroup_store(S_tile[kk / 8], o_state + kk, Dk, ulong2(0, 0), true);
+  }
+}
+
 template <typename InT, int Dk, int Dv, int Hk, int Hv, int C>
 [[kernel]] void make_wy(
     const device InT* k [[buffer(0)]],
@@ -140,7 +327,6 @@ template <typename InT, typename StT, int Dk, int Dv, int Hk, int Hv, int C>
     device StT* state_out [[buffer(7)]], // [B, Hv, Dv, Dk]
     constant int& T [[buffer(8)]],
     uint3 thread_position_in_grid [[thread_position_in_grid]],
-    uint3 thread_position_in_threadgroup [[thread_position_in_threadgroup]],
     uint thread_index_in_simdgroup [[thread_index_in_simdgroup]]) {
   auto n = thread_position_in_grid.z;
   auto b_idx = n / Hv;
@@ -152,7 +338,6 @@ template <typename InT, typename StT, int Dk, int Dv, int Hk, int Hv, int C>
   const short fm = (qid & 4) + ((thread_index_in_simdgroup / 2) % 4);
   const short fn = (qid & 2) * 2 + (thread_index_in_simdgroup % 2) * 2;
 
-  auto dk_idx = thread_position_in_threadgroup.x; // simd group id
   auto dv_idx = thread_position_in_grid.y * 8;
 
   // set up pointers
@@ -209,7 +394,6 @@ template <typename InT, typename StT, int Dk, int Dv, int Hk, int Hv, int C>
 
     float gamma_val = simd_prefix_inclusive_product(g_val);
 
-    // Only write within bounds
     if (thread_index_in_simdgroup < C) {
       gamma[thread_index_in_simdgroup] = gamma_val;
     }
