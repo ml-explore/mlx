d81b2d1cc0522dcea0877035d525887ffa1cb844
tpegolotti <tommaso.pegolotti@hotmail.com>
2026-06-22 17:23:35 +0200

    fused wy into gated computation

--- mlx/backend/metal/gated_delta_update.cpp
+++ mlx/backend/metal/gated_delta_update.cpp
@@ -43,7 +43,10 @@ void GatedDeltaUpdate::eval_gpu(
   int C = chunk_size;
   int n_chunks = T / C; // TODO: make general
 
-  std::string kernel_name = C < 1 ? "seq_gated_delta_" : "chunk_gated_delta_";
+  //   std::string kernel_name = C < 1 ? "seq_gated_delta_" :
+  //   "chunk_gated_delta_";
+  std::string kernel_name =
+      C < 1 ? "seq_gated_delta_" : "gated_delta_fused_chunk_";
   std::string suffix = get_type_string(q.dtype()) // "float"
       + "_" + get_type_string(h0.dtype()) // "float"
       + "_" + std::to_string(Dk) + "_" + std::to_string(Dv) + "_" +
@@ -68,47 +71,66 @@ void GatedDeltaUpdate::eval_gpu(
 
   if (C > 1) {
     // allocate full W and U -- [B, T, H, D]
-    array W({B, T, Hk, Dk}, q.dtype(), nullptr, {});
-    array U({B, T, Hv, Dv}, q.dtype(), nullptr, {});
-    W.set_data(allocator::malloc(W.nbytes()));
-    U.set_data(allocator::malloc(U.nbytes()));
+    // array W({B, T, Hk, Dk}, q.dtype(), nullptr, {});
+    // array U({B, T, Hv, Dv}, q.dtype(), nullptr, {});
+    // W.set_data(allocator::malloc(W.nbytes()));
+    // U.set_data(allocator::malloc(U.nbytes()));
 
-    compute_encoder.add_temporary(W);
-    compute_encoder.add_temporary(U);
+    // compute_encoder.add_temporary(W);
+    // compute_encoder.add_temporary(U);
 
-    // kernel 1: compute full W and U
-    std::string make_wy_name = "make_wy_" +
-        get_type_string(q.dtype()) // "float"
-        + "_" + std::to_string(Dk) + "_" + std::to_string(Dv) + "_" +
-        std::to_string(Hk) + "_" + std::to_string(Hv) + "_" + std::to_string(C);
+    // // kernel 1: compute full W and U
+    // std::string make_wy_name = "make_wy_" +
+    //     get_type_string(q.dtype()) // "float"
+    //     + "_" + std::to_string(Dk) + "_" + std::to_string(Dv) + "_" +
+    //     std::to_string(Hk) + "_" + std::to_string(Hv) + "_" +
+    //     std::to_string(C);
 
-    auto wy_kernel = d.get_kernel(make_wy_name);
+    // auto wy_kernel = d.get_kernel(make_wy_name);
 
-    compute_encoder.set_compute_pipeline_state(wy_kernel);
-    compute_encoder.set_input_array(k, 0);
-    compute_encoder.set_input_array(v, 1);
-    compute_encoder.set_input_array(g, 2);
-    compute_encoder.set_input_array(beta, 3);
-    compute_encoder.set_output_array(W, 4);
-    compute_encoder.set_output_array(U, 5);
-    compute_encoder.set_bytes(T, 6);
+    // compute_encoder.set_compute_pipeline_state(wy_kernel);
+    // compute_encoder.set_input_array(k, 0);
+    // compute_encoder.set_input_array(v, 1);
+    // compute_encoder.set_input_array(g, 2);
+    // compute_encoder.set_input_array(beta, 3);
+    // compute_encoder.set_output_array(W, 4);
+    // compute_encoder.set_output_array(U, 5);
+    // compute_encoder.set_bytes(T, 6);
 
-    auto grid_wy = MTL::Size(32, 4, B * Hv * n_chunks);
-    auto threads_wy = MTL::Size(32, 4, 1);
-    compute_encoder.dispatch_threads(grid_wy, threads_wy);
+    // auto grid_wy = MTL::Size(32, 4, B * Hv * n_chunks);
+    // auto threads_wy = MTL::Size(32, 4, 1);
+    // compute_encoder.dispatch_threads(grid_wy, threads_wy);
+
+    // // kernel 2: gated delta
+    // compute_encoder.set_compute_pipeline_state(delta_kernel);
+    // compute_encoder.set_input_array(q, 0);
+    // compute_encoder.set_input_array(k, 1);
+    // compute_encoder.set_input_array(W, 2);
+    // compute_encoder.set_input_array(U, 3);
+    // compute_encoder.set_input_array(h0, 4); // initial state in
+    // compute_encoder.set_input_array(g, 5);
+    // compute_encoder.set_output_array(out, 6);
+    // compute_encoder.set_output_array(hf, 7); // final state out
+    // compute_encoder.set_bytes(T, 8);
+    // compute_encoder.set_bytes(n_chunks, 9);
+
+    // auto grid = MTL::Size(32, Dv / 8, B * Hv);
+    // auto threads = MTL::Size(32, 1, 1); // get one simdgroup to work
+    // // auto grid   = MTL::Size(1, 1, 1);
+    // // auto threads = MTL::Size(1, 1, 1);
+    // compute_encoder.dispatch_threads(grid, threads);
 
     // kernel 2: gated delta
     compute_encoder.set_compute_pipeline_state(delta_kernel);
     compute_encoder.set_input_array(q, 0);
     compute_encoder.set_input_array(k, 1);
-    compute_encoder.set_input_array(W, 2);
-    compute_encoder.set_input_array(U, 3);
-    compute_encoder.set_input_array(h0, 4); // initial state in
-    compute_encoder.set_input_array(g, 5);
+    compute_encoder.set_input_array(v, 2);
+    compute_encoder.set_input_array(h0, 3); // initial state in
+    compute_encoder.set_input_array(g, 4);
+    compute_encoder.set_input_array(beta, 5);
     compute_encoder.set_output_array(out, 6);
     compute_encoder.set_output_array(hf, 7); // final state out
     compute_encoder.set_bytes(T, 8);
-    compute_encoder.set_bytes(n_chunks, 9);
 
     auto grid = MTL::Size(32, Dv / 8, B * Hv);
     auto threads = MTL::Size(32, 1, 1); // get one simdgroup to work
--- mlx/backend/metal/kernels/gated_delta_update.metal
+++ mlx/backend/metal/kernels/gated_delta_update.metal
@@ -58,6 +58,31 @@ using namespace metal;
           instantiate_make_wy(in_type, 64, 64, 4, 4, 8) \
               instantiate_make_wy(in_type, 64, 64, 8, 8, 8)
 
+#define instantiate_gated_delta_update_fused_chunk(                            \
+    in_type, st_type, dk, dv, hk, hv, c)                                       \
+  instantiate_kernel(                                                          \
+      "gated_delta_fused_chunk_" #in_type "_" #st_type "_" #dk "_" #dv "_" #hk \
+      "_" #hv "_" #c,                                                          \
+      gated_delta_fused_chunk,                                                 \
+      in_type,                                                                 \
+      st_type,                                                                 \
+      dk,                                                                      \
+      dv,                                                                      \
+      hk,                                                                      \
+      hv,                                                                      \
+      c)
+
+#define instantiate_gated_delta_update_fused_chunk_dims(in_type, st_type) \
+  instantiate_gated_delta_update_fused_chunk(                             \
+      in_type, st_type, 64, 64, 4, 4, 32)                                 \
+      instantiate_gated_delta_update_fused_chunk(                         \
+          in_type, st_type, 64, 64, 4, 4, 16)                             \
+          instantiate_gated_delta_update_fused_chunk(                     \
+              in_type, st_type, 64, 64, 4, 4, 8)                          \
+              instantiate_gated_delta_update_fused_chunk(                 \
+                  in_type, st_type, 64, 64, 8, 8, 8)
+
 instantiate_gated_delta_update_seq_dims(float, float)
     instantiate_gated_delta_update_chunk_dims(float, float)
         instantiate_make_wy_dims(float)
+            instantiate_gated_delta_update_fused_chunk_dims(float, float)
