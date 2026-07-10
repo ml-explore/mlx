f50d146367c18f822d565c4d61ed053cf0179d44
Angelos Katharopoulos <a_katharopoulos@apple.com>
2026-07-10 16:20:03 -0700

    Remove is_random_state function declaration

--- python/src/random.h
+++ python/src/random.h
@@ -12,9 +12,6 @@ namespace nb = nanobind;
 // The process-global `mx.random.state` sentinel.
 nb::object random_state_sentinel();
 
-// True if `obj` is the RNG state sentinel.
-bool is_random_state(nb::handle obj);
-
 // Read/write the calling thread's current PRNG key.
 mx::array random_state_key();
 void set_random_state_key(const mx::array& key);
