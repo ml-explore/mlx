e4619617d57eb3fe9aa5270a4ab5c37841581e94
Scott Roy <scroy@meta.com>
2026-07-31 17:31:51 -0700

    Fix sorted gather_mm activation row stride

--- mlx/backend/metal/matmul.cpp
+++ mlx/backend/metal/matmul.cpp
@@ -2114,7 +2114,7 @@ void gather_mm_rhs(
   int K = a.shape(-1);
   int M = a.size() / K;
   int N = b.shape(-1);
-  int lda = a.strides()[a.ndim() - 2]; // should be K
+  int lda = K;
 
   // Define the dispatch blocks
   int bm = 16, bn = 64, bk = 16;
@@ -2247,7 +2247,7 @@ void gather_mm_rhs_nax(
   int K = a.shape(-1);
   int M = a.size() / K;
   int N = b.shape(-1);
-  int lda = a.strides()[a.ndim() - 2]; // should be K
+  int lda = K;
   int E = b.shape(0);
 
   // Define the dispatch blocks
