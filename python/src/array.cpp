57f5e471dac15b7262733ba1a072ac948dedac00
blanky <aaishwarymishra@gmail.com>
2026-08-03 02:14:00 +0530

    added __complex__ support

--- python/src/array.cpp
+++ python/src/array.cpp
@@ -1033,6 +1033,12 @@ void init_array(nb::module_& m) {
       .def("__int__", [](mx::array& a) { return nb::int_(to_scalar(a)); })
       .def("__float__", [](mx::array& a) { return nb::float_(to_scalar(a)); })
       .def(
+          "__complex__",
+          [](mx::array& a) {
+            auto value = PyComplex_AsCComplex(to_scalar(a).ptr());
+            return std::complex<double>(value.real, value.imag);
+          })
+      .def(
           "__format__",
           [](mx::array& a, nb::object format_spec) {
             if (nb::len(nb::str(format_spec)) > 0 && a.ndim() > 0) {
