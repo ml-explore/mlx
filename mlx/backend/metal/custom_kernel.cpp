75d994c8d8e4860a7b6a54aa069663fed634093f
Cheng <git@zcbenz.com>
2026-08-04 08:38:17 +0900

    Use fmt::format for string concat

--- mlx/backend/metal/custom_kernel.cpp
+++ mlx/backend/metal/custom_kernel.cpp
@@ -6,6 +6,8 @@
 #include "mlx/backend/metal/utils.h"
 #include "mlx/fast_primitives.h"
 
+#include <fmt/format.h>
+
 namespace mlx::core::fast {
 
 void CustomKernel::eval_gpu(
@@ -45,13 +47,8 @@ void CustomKernel::eval_gpu(
 
   auto& d = metal::device(s.device);
 
-  // Key the library by (name, source hash, compile options) so kernels that
-  // differ in any of those coexist instead of thrashing a single cache slot
-  // within one eval batch (fixes #3832). The source is hashed to keep the key
-  // short.
-  std::string lib_name = name_ + "_" +
-      std::to_string(std::hash<std::string>{}(source_)) + "_" +
-      std::to_string(compile_options_);
+  std::string lib_name = fmt::format(
+      "{}_{:x}_{}", name_, std::hash<std::string>{}(source_), compile_options_);
   auto lib = d.get_library(
       lib_name, compile_options_, [this] { return metal::utils() + source_; });
   auto kernel = d.get_kernel(name_, lib);
