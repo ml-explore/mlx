52504b4c4c1f89e199cb1dc5efc62e8333e6274b
John Elliott <54456354+PhysicistJohn@users.noreply.github.com>
2026-08-10 21:39:06 -0700

    Remove redundant bitwise switch

--- mlx/backend/metal/binary.cpp
+++ mlx/backend/metal/binary.cpp
@@ -235,15 +235,7 @@ BINARY_GPU(Power)
 BINARY_GPU(Subtract)
 
 void BitwiseBinary::eval_gpu(const std::vector<array>& inputs, array& out) {
-  switch (op_) {
-    case BitwiseBinary::And:
-    case BitwiseBinary::Or:
-    case BitwiseBinary::Xor:
-    case BitwiseBinary::LeftShift:
-    case BitwiseBinary::RightShift:
-      binary_op_gpu(inputs, out, name());
-      break;
-  }
+  return binary_op_gpu(inputs, out, name());
 }
 
 } // namespace mlx::core
