6e6ac0f753bba06404cf9cfbd47799e039f89e94
John Elliott <54456354+PhysicistJohn@users.noreply.github.com>
2026-08-12 07:59:30 -0700

    Disambiguate SliceUpdate test arguments

--- tests/autograd_tests.cpp
+++ tests/autograd_tests.cpp
@@ -857,15 +857,15 @@ TEST_CASE("test slice update jvp with one tangent") {
 
   for (bool add : {false, true}) {
     auto update_fn = [&src, add](array x) {
-      return add ? slice_update_add(src, x, {1}, {3})
-                 : slice_update(src, x, {1}, {3});
+      return add ? slice_update_add(src, x, Shape{1}, Shape{3})
+                 : slice_update(src, x, Shape{1}, Shape{3});
     };
     auto out = jvp(update_fn, update, update_tan).second;
     CHECK(array_equal(out, array({0.0f, 7.0f, 8.0f, 0.0f})).item<bool>());
 
     auto src_fn = [&update, add](array x) {
-      return add ? slice_update_add(x, update, {1}, {3})
-                 : slice_update(x, update, {1}, {3});
+      return add ? slice_update_add(x, update, Shape{1}, Shape{3})
+                 : slice_update(x, update, Shape{1}, Shape{3});
     };
     out = jvp(src_fn, src, src_tan).second;
     auto expected = add ? src_tan : array({1.0f, 0.0f, 0.0f, 4.0f});
