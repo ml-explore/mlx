53a825b3f70562c2b039b5def99216c67345e49c
blanky <aaishwarymishra@gmail.com>
2026-08-18 11:14:24 +0530

    implemented few methods

--- python/mlx/__array_api_info.py
+++ python/mlx/__array_api_info.py
@@ -3,16 +3,46 @@ class ArrayNamespaceInfo:
         pass
 
     def capabilities(self):
-        pass
+        return {
+            "boolean indexing": False,
+            "data-dependent shapes": False,
+            "max dimensions": None,
+        }
 
     def default_device(self):
-        pass
+        import mlx.core as mx
 
-    def default_dtype(self):
-        pass
+        return mx.default_device()
+
+    def default_dtypes(self, *, device=None):
+        import mlx.core as mx
+
+        if device is not None and not isinstance(device, mx.Device):
+            raise TypeError("Expected a mlx Device")
+        device = device if device is not None else self.default_device()
+        if device.type == mx.gpu:
+            return {
+                "real floating": mx.float32,
+                "complex floating": mx.complex64,
+                "integral": mx.int32,
+                "indexing": mx.uint32,
+            }
+        return {
+            "real floating": mx.float64,
+            "complex floating": mx.complex128,
+            "integral": mx.int64,
+            "indexing": mx.uint64,
+        }
 
     def devices(self):
-        pass
+        import mlx.core as mx
+
+        devices = [
+            mx.Device(dev_type, i)
+            for dev_type in (mx.cpu, mx.gpu)
+            for i in range(mx.device_count(dev_type))
+        ]
+        return tuple(devices)
 
     def dtypes(self, *, device=None, kind=None):
         pass
