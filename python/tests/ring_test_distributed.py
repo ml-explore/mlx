5fba80f27f1a920d7873e71075e5a66899300822
Ronald Mannak <ronaldmannak@me.com>
2026-09-24 09:27:43 -0700

    fix register_group caching

--- python/tests/ring_test_distributed.py
+++ python/tests/ring_test_distributed.py
@@ -23,6 +23,12 @@ class TestRingDistributed(mlx_distributed_tests.MLXDistributedCommonTestCase):
         with self.assertRaises(RuntimeError):
             sub = world.split(world.rank() % 2)
 
+    def test_strict_init_after_fallback(self):
+        # A failed non-strict init must not satisfy a later strict init
+        self.assertEqual(mx.distributed.init(backend="jaccl").size(), 1)
+        with self.assertRaises(RuntimeError):
+            mx.distributed.init(backend="jaccl", strict=True)
+
     def test_all_reduce_extra(self):
         world = mx.distributed.init()
         dtypes = [
